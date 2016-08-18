a3c69cb1296c405eb38242d07002233474524fb5
kaushik.pugazhendi <kaushik.pugazhendi@dvara.com>
2016-08-18 07:00:45 +0000

    Removed duplicate Individual Loan Model
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/branches/v8.1.x-kinara_dev/source@48 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- dev-www/process/models/v1/loans/IndividualLoan.js
+++
@@ -1,9 +0,0 @@
-irf.models.factory('IndividualLoan',function($resource,$httpParamSerializer,BASE_URL,searchResource){
-    var endpoint = BASE_URL + "/api/individualLoan";
-
-    return $resource(endpoint, null, {
-        'save': {
-            method: 'POST'
-        }
-    })
-})
