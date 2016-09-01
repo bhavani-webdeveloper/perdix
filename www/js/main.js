4ce8fca3981563165efcddd0569008c0989c334a
stalin.solomon <stalin.solomon@dvara.com>
2016-09-01 06:53:15 +0000

    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/branches/v8.1.x-kinara_dev/source@219 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- www/js/main.js
+++ www/js/main.js
@@ -5898,9 +5898,9 @@ $(document).ready(function(){
 });
 
 //kgfs-pilot irf.BASE_URL = 'http://uatperdix.kgfs.co.in:8080/kgfs-pilot';
-irf.BASE_URL = 'http://works2.sen-sei.in:8080/perdix-server';
+//irf.BASE_URL = 'http://works2.sen-sei.in:8080/perdix-server';
 //irf.BASE_URL = 'http://uatperdix.kgfs.co.in:8080/perdix-server';
-//irf.BASE_URL = 'http://52.4.230.141:8080/perdix-server';
+irf.BASE_URL = 'http://52.4.230.141:8080/perdix-server';
 //irf.BASE_URL = 'http://uatperdix.kgfs.co.in:8080/pilot-server';
 
 irf.MANAGEMENT_BASE_URL = 'http://uatperdix.kgfs.co.in:8081/perdixService/index.php';
