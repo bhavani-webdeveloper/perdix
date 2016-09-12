1c50256955fdccb5f1738c6e0679212eead3baac
rajkumar.v <rajkumar.v@dvara.com>
2016-09-12 13:57:25 +0000

    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/branches/v8.1.x-kinara_dev/source@383 15cf4ce8-1186-411e-815e-a2b7fc64482c

---
+++ dev-www/process/models/v1/Lead/lead.js
@@ -0,0 +1,53 @@
+irf.models.factory('lead', ["$resource", "$httpParamSerializer", "BASE_URL", "searchResource", "Upload", "$q",
+    function($resource, $httpParamSerializer, BASE_URL, searchResource, Upload, $q) {
+        var endpoint = BASE_URL + '/api/lead';
+        /*
+         * $get : /api/enrollments/{blank/withhistory/...}/{id}
+         *  eg: /enrollments/definitions -> $get({service:'definition'})
+         *      /enrollments/1           -> $get({id:1})
+         * $post will send data as form data, save will send it as request payload
+         */
+        var resource = $resource(endpoint, null, {
+            getSchema: {
+                method: 'GET',
+                url: 'process/schemas/Leadgeneration.json'
+            },
+            create: {
+                method: 'POST',
+                url: endpoint + '/create'
+            },
+            search: searchResource({
+                method: 'GET',
+                url: endpoint + '/search'
+            }),
+            searchHead: {
+                method: 'HEAD',
+                url: endpoint + '/search',
+                isArray: true
+            },
+            update: {
+                method: 'PUT',
+                url: endpoint + '/update'
+            }
+        });
+
+        resource.achMandateUpload = function(file, progress) {
+            var deferred = $q.defer();
+            Upload.upload({
+                url: BASE_URL + "/api/feed/achmandateupload",
+                data: {
+                    file: file
+                }
+            }).then(function(resp){
+                // TODO handle success
+                deferred.resolve(resp);
+            }, function(errResp){
+                // TODO handle error
+                deferred.reject(errResp);
+            }, progress);
+            return deferred.promise;
+        }
+
+        return resource;
+    }
+]);
