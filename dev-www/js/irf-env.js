ae1cfc44a3f625f06720093df42bb367bf83004c
shahal.tharique <shahal.tharique@dvara.com>
2016-09-16 03:57:13 +0000

    env changes
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/branches/v8.1.x-kinara_dev/source@494 15cf4ce8-1186-411e-815e-a2b7fc64482c

---
+++ dev-www/js/irf-env.js
@@ -0,0 +1,31 @@
+var irf = irf || {};
+
+//kgfs-pilot irf.BASE_URL = 'http://uatperdix.kgfs.co.in:8080/kgfs-pilot';
+//irf.BASE_URL = 'http://works2.sen-sei.in:8080/perdix-server';
+//irf.BASE_URL = 'http://uatperdix.kgfs.co.in:8080/perdix-server';
+irf.BASE_URL = 'http://52.4.230.141:8080/perdix-server';
+//irf.BASE_URL = 'http://uatperdix.kgfs.co.in:8080/pilot-server';
+
+irf.MANAGEMENT_BASE_URL = 'http://52.4.230.141:8081/management';
+
+//irf.FORM_DOWNLOAD_URL = 'http://uatperdix.kgfs.co.in:8081/saijaforms/DownloadForms.php';
+irf.FORM_DOWNLOAD_URL = 'http://115.113.193.49:8080/formsKinara/formPrint.jsp';
+
+irf.BI_BASE_URL = "http://52.4.230.141:8081/bi";
+
+
+
+//
+//
+////kgfs-pilot irf.BASE_URL = 'http://uatperdix.kgfs.co.in:8080/kgfs-pilot';
+////irf.BASE_URL = 'http://works2.sen-sei.in:8080/perdix-server';
+////irf.BASE_URL = 'http://uatperdix.kgfs.co.in:8080/perdix-server';
+//irf.BASE_URL = 'http://52.4.230.141:8080/kinara-server';
+////irf.BASE_URL = 'http://uatperdix.kgfs.co.in:8080/pilot-server';
+//
+//irf.MANAGEMENT_BASE_URL = 'http://52.4.230.141:8081/management-uat';
+//
+////irf.FORM_DOWNLOAD_URL = 'http://uatperdix.kgfs.co.in:8081/saijaforms/DownloadForms.php';
+//irf.FORM_DOWNLOAD_URL = 'http://115.113.193.49:8080/formsKinara/formPrint.jsp';
+//
+//irf.BI_BASE_URL = "http://52.4.230.141:8081/bi-uat";
