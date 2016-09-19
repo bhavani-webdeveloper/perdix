b3305703c4e33abd6e28b78efdc98cbe736217ea
stalin.solomon <stalin.solomon@dvara.com>
2016-09-19 16:18:26 +0000

    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/branches/v8.1.x-kinara_dev/source@549 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- dev-www/process/pages/definitions/loans/individual/booking/LoanInput.js
+++ dev-www/process/pages/definitions/loans/individual/booking/LoanInput.js
@@ -918,6 +918,7 @@ irf.pageCollection.factory(irf.page("loans.individual.booking.LoanInput"),
                     return deferred.promise;
                 },
                 submit: function(model, form, formName) {
+                    var self = this;
                     $log.info(model);
                     PageHelper.clearErrors();
 
@@ -1020,7 +1021,8 @@ irf.pageCollection.factory(irf.page("loans.individual.booking.LoanInput"),
                                 IndividualLoan.create(resp,function(resp,headers){
                                     $log.info(resp);
                                     PageHelper.showProgress("loan-create","Loan Created",5000);
-                                    $state.go('Page.Landing', null);
+                                    model = {};
+                                    self.initialize(model, form, null);
                                 },function(errresp){
                                     $log.info(errresp);
                                     PageHelper.showErrors(errresp);
