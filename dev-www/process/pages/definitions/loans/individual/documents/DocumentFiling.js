e72d002e4dfaaa9942dd2e452e8699a5ee197a16
rajkumar.v <rajkumar.v@dvara.com>
2016-10-05 06:23:54 +0000

    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/branches/v8.2.x-kinara_dev/source@855 15cf4ce8-1186-411e-815e-a2b7fc64482c

---
+++ dev-www/process/pages/definitions/loans/individual/documents/DocumentFiling.js
@@ -0,0 +1,87 @@
+irf.pageCollection.factory(irf.page("DocumentTracking.DocumentFilling"), ["$log", "$state", "Enrollment", "lead", "EnrollmentHelper", "SessionStore", "formHelper", "$q", "irfProgressMessage",
+    "PageHelper", "Utils", "BiometricService", "PagesDefinition", "Queries",
+
+
+   function($log, $state, document, SessionStore, formHelper, $q, irfProgressMessage,
+    PageHelper, Utils, PagesDefinition, Queries) {
+
+    var branch = SessionStore.getBranch();
+
+    return {
+        "type": "schema-form",
+        "title": "DISPATCH_INCOMPLETE_DOCUMENTS",
+        "subTitle": "",
+        initialize: function(model, form, formCtrl) {
+            model.doc = model.doc || {};
+            $log.info("Dispatch Incomplete documents is initiated ");
+        },
+
+        modelPromise: function(pageId, _model) {
+
+        },
+
+        offline: true,
+        getOfflineDisplayItem: function(item, index) {
+            return []
+        },
+
+         form: [{
+                "type": "box",
+                "title": "DISPATCH_DETAILS",
+                "items": [{
+                    key: "doc.Dispatch[].CourierSentDate",
+                    title: "COURIER_SENT_DATE",
+                }, {
+                    key: "doc.Dispatch[].CourierCompanyName",
+                    title: "CourierCompanyName",
+                }, {
+                    key: "doc.Dispatch[].PodNumber",
+                    title: "POD_NUMBER",
+                }, {
+                    key: "doc.Dispatch[].BatchNumber",
+                    title: "BATCHNUMBER",
+                    readonly: true
+                }, {
+                    key: "doc.Dispatch[].Remarks",
+                    title: "REMARKS",
+                }]
+
+
+            },
+
+
+            {
+                "type": "actionbox",
+                "items": [{
+                    "type": "submit",
+                    "title": "Submit"
+                }]
+            },
+        ],
+
+        schema: function() {
+
+        },
+
+        actions: {
+            preSave: function(model, form, formName) {
+                $log.info("Inside save()");
+                var deferred = $q.defer();
+                if (model.lead.Name) {
+                    deferred.resolve();
+                } else {
+                    irfProgressMessage.pop('LeadGeneration-save', 'Applicant Name is required', 3000);
+                    deferred.reject();
+                }
+                return deferred.promise;
+            },
+
+            submit: function(model, form, formName) {
+                $log.info("Inside submit()");
+                irfProgressMessage.pop('LeadGeneration-save', 'Lead is successfully created', 3000);
+                $log.warn(model);
+            }
+        }
+    };
+
+}]);
