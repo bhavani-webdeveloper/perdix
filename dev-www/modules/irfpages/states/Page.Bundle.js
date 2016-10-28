9a6f7de43f3ee89ceb9dd77153d41093764b8fd8
stalin.solomon <stalin.solomon@dvara.com>
2016-10-28 11:19:39 +0000

    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/branches/v8.2.x-kinara_dev/source@1158 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- dev-www/modules/irfpages/states/Page.Bundle.js
+++ dev-www/modules/irfpages/states/Page.Bundle.js
@@ -1,161 +1,156 @@
-irf.pages.controller("PageBundleCtrl", ["$log","$filter", "$scope", "$state", "$stateParams", "$injector", "$q", "entityManager", "formHelper", "$timeout",
-    function($log,$filter,$scope, $state, $stateParams, $injector, $q, entityManager, formHelper, $timeout) {
-        var self = this;
+irf.pages.controller("PageBundleCtrl", ["$log", "$filter", "$scope", "$state", "$stateParams", "$injector", "$q", "entityManager", "formHelper", "$timeout",
+function($log, $filter, $scope, $state, $stateParams, $injector, $q, entityManager, formHelper, $timeout) {
+    var self = this;
 
-        /*var bundle = {
-            bundleName: "loans.Screening",
-            pages: [{
-                pageName: 'lead.LeadGeneration',
-                title: 'LeadGeneration'
-            }]
-        };*/
-
-        $scope.pageNames = [{
-            pageName: 'lead.LeadGeneration',
-            title: 'Lead Generation',
-            repeat:3
-        }, {
-            pageName: 'CustomerSearch',
-            title: 'Customer Search',
-            repeat:2
-        }, {
-            pageName: 'lead.LeadBulkUpload',
-            title: 'Bulk Upload',
-            repeat:1
-        },
-        {
+    var bundle = {
+        bundleName: "loans.Screening",
+        pages: [{
             pageName: 'customer.IndividualEnrollment',
-            title: 'Applicant',
-            repeat:1
-        },
-        {
+            title: 'APPLICANT',
+            minimum: 1,
+            maximum: 1
+        }, {
+            pageName: 'customer.IndividualEnrollment',
+            title: 'CO_APPLICANT',
+            minimum: 0,
+            maximum: 2
+        }, {
             pageName: 'customer.EnterpriseEnrollment',
-            title: 'Enterprise Enrollment',
-            repeat:1
+            title: 'BUSINESS',
+            minimum: 1,
+            maximum: 1
+        }]
+    };
+
+    $scope.pages = [];
+    $scope.addTabMenu = [];
+
+    var initializePage = function(bundlePage) {
+        var pageObj = {};
+
+        pageObj.pageName = bundlePage.pageName;
+        pageObj.pageNameHtml = pageObj.pageName.split('.').join('<br/>');
+        pageObj.formName = irf.form(bundlePage.pageName);
+        pageObj.title = bundlePage.title;
+        pageObj.id = bundlePage.id;
+        pageObj.bundlePage = bundlePage;
+
+        pageObj.error = false;
+        try {
+            pageObj.page = $injector.get(irf.page(pageObj.pageName));
+        } catch (e) {
+            $log.error(e);
+            pageObj.error = true;
+            //$state.go('Page.EngineError', {pageName:$scope.pageName});
         }
-        ];
+        if (pageObj.page) {
+            if (pageObj.page.type == 'schema-form') {
+                pageObj.model = entityManager.getModel(pageObj.pageName);
+                if (angular.isFunction(pageObj.page.schema)) {
+                    var promise = pageObj.page.schema();
+                    promise.then((function(data) {
+                        var _pageObj = pageObj;
+                        return function(data) {
+                            _pageObj.page.schema = data;
 
-
-
-        $scope.pages = [];
-        $scope.tabs = [];
-        for (i in $scope.pageNames) {
-            var obj = {};
-            
-            obj.pageName = $scope.pageNames[i].pageName;
-            obj.formName = irf.form($scope.pageNames[i].pageName);
-            obj.pageNameHtml = $scope.pageNames[i].pageName.split('.').join('<br/>');
-            obj.title = $scope.pageNames[i].title;
-            obj.id=$scope.pageNames[i].id;
-            obj.repeat=$scope.pageNames[i].repeat;
-
-            obj.error = false;
-            try {
-                obj.page = $injector.get(irf.page(obj.pageName));
-            } catch (e) {
-                $log.error(e);
-                obj.error = true;
-                //$state.go('Page.EngineError', {pageName:$scope.pageName});
-            }
-
-            if (obj.page) {
-                if (obj.page.type == 'schema-form') {
-                    obj.model = entityManager.getModel(obj.pageName);
-                    if (angular.isFunction(obj.page.schema)) {
-                        var promise = obj.page.schema();
-                        promise.then((function(data) {
-                            var _obj = obj;
-                            return function(data) {
-                                _obj.page.schema = data;
-
-                                if (angular.isFunction(_obj.page.form)) {
-                                    var promise = _obj.page.form();
-                                    promise.then(function(data) {
-                                         var obj = _obj;
-                                        obj.page.form = data;
-                                        $timeout(function() {
-                                            obj.page.initialize(obj.page.model, $scope.page.form, $scope.formCtrl);
-                                        });
+                            if (angular.isFunction(_pageObj.page.form)) {
+                                var promise = _pageObj.page.form();
+                                promise.then(function(data) {
+                                    var pageObj = _pageObj;
+                                    pageObj.page.form = data;
+                                    $timeout(function() {
+                                        pageObj.page.initialize(pageObj.page.model, $scope.page.form, $scope.formCtrl);
                                     });
-                                }
-                            };
-                        })());
-                    }
-                    obj.formHelper = formHelper;
-
-                    $scope.$on('irf-sf-init', function(event) {
-                        $scope.formCtrl = event.targetScope[$scope.formName];
-                    });
-                    $scope.$on('sf-render-finished', function(event) {
-                        $log.warn("on sf-render-finished on page, rendering layout");
-                        setTimeout(renderLayout);
-                    });
-                } else if (obj.page.type == 'search-list') {
-                    obj.model = entityManager.getModel(obj.pageName);
-                    obj.page.definition.formName = obj.formName;
-                    if (obj.page.offline === true) {
-                        obj.page.definition.offline = true;
-                        var acts = obj.page.definition.actions = obj.page.definition.actions || {};
-                        acts.preSave = function(model, formCtrl, formName) {
-                            var deferred = $q.defer();
-                            $log.warn('on pageengine preSave');
-                            var offlinePromise = obj.page.getOfflinePromise(model);
-                            if (offlinePromise && _.isFunction(offlinePromise.then)) {
-                                offlinePromise.then(function(out) {
-                                    $log.warn('offline results:');
-                                    $log.warn(out.body.length);
-                                    /* Build results */
-                                    var items = obj.page.definition.listOptions.getItems(out.body, out.headers);
-                                    model._result = model._result || {};
-                                    model._result.items = items;
-
-                                    deferred.resolve();
-                                }).catch(function() {
-                                    deferred.reject();
                                 });
-                            } else {
-                                deferred.reject();
                             }
-                            return deferred.promise;
                         };
-                    }
+                    })());
+                }
+                pageObj.formHelper = formHelper;
+
+                $scope.$on('irf-sf-init', function(event) {
+                    $scope.formCtrl = event.targetScope[$scope.formName];
+                });
+                $scope.$on('sf-render-finished', function(event) {
+                    $log.warn("on sf-render-finished on page, rendering layout");
+                    //setTimeout(renderLayout);
+                });
+            } else if (pageObj.page.type == 'search-list') {
+                pageObj.model = entityManager.getModel(pageObj.pageName);
+                pageObj.page.definition.formName = pageObj.formName;
+                if (pageObj.page.offline === true) {
+                    pageObj.page.definition.offline = true;
+                    var acts = pageObj.page.definition.actions = pageObj.page.definition.actions || {};
+                    acts.preSave = function(model, formCtrl, formName) {
+                        var deferred = $q.defer();
+                        $log.warn('on pageengine preSave');
+                        var offlinePromise = pageObj.page.getOfflinePromise(model);
+                        if (offlinePromise && _.isFunction(offlinePromise.then)) {
+                            offlinePromise.then(function(out) {
+                                $log.warn('offline results:');
+                                $log.warn(out.body.length);
+                                /* Build results */
+                                var items = pageObj.page.definition.listOptions.getItems(out.body, out.headers);
+                                model._result = model._result || {};
+                                model._result.items = items;
+
+                                deferred.resolve();
+                            }).catch(function() {
+                                deferred.reject();
+                            });
+                        } else {
+                            deferred.reject();
+                        }
+                        return deferred.promise;
+                    };
                 }
             }
-            $scope.pages.push(obj);
-
-            $log.info(obj.repeat);
-
-            if(obj.repeat>1)
-            {
-            	$scope.tabs.push(obj);	
-            }
         }
+        return pageObj;
+    };
 
-        
-       
-
-        $scope.removeTab = function (item) {
-        var title=$scope.pages.splice(item, 1)[0].title;
-        var item=$filter('filter')($scope.tabs, title,true)[0];
-        var index=$scope.tabs.indexOf(item);
-        var temp=$scope.tabs.splice(index, 1)[0];
-        temp.repeat=temp.repeat+1;
-        $scope.tabs.push(temp);
-        };
-
-        $scope.addTab = function (item) {
-        var temp=$scope.tabs.splice(item, 1)[0];
-        $log.info($scope.pages);
-        $log.info(temp);
-        $scope.tabs.push(temp);
-
-        if(temp.repeat>1)
-        {
-            temp.repeatflag=true;
-            $scope.pages.push(temp);
-            --temp.repeat
+    for (i in bundle.pages) {
+        bundle.pages[i].minimum = bundle.pages[i].minimum || 0;
+        bundle.pages[i].maximum = bundle.pages[i].maximum || 1000;
+        var bundlePage = _.cloneDeep(bundle.pages[i]);
+        bundlePage.openPagesCount = 0;
+        if (bundlePage.minimum > 0) {
+            var openPage = initializePage(bundlePage);
+            $scope.pages.push(openPage);
+            bundlePage.openPagesCount = 1;
         }
-
-        };
+        if (bundlePage.minimum == 0 || bundlePage.maximum > 1) {
+            $scope.addTabMenu.push(bundlePage);
+        }
     }
-]);
+
+    $scope.isRemovable = function(bundlePage) {
+        return bundlePage.minimum < bundlePage.openPagesCount;
+    };
+
+    $scope.removeTab = function(index) {
+        var bundlePage = $scope.pages[index].bundlePage;
+        --bundlePage.openPagesCount;
+        $scope.pages.splice(index, 1);
+        if (!$filter('filter')($scope.addTabMenu, {"pageName":bundlePage.pageName}, true).length) {
+            $scope.addTabMenu.push(bundlePage);
+        }
+    };
+
+    $scope.addTab = function(index) {
+        var bundlePage = $scope.addTabMenu[index];
+        $log.info(bundlePage);
+        var openPage = initializePage(bundlePage);
+        $scope.pages.push(openPage);
+        ++bundlePage.openPagesCount;
+        if (bundlePage.maximum <= bundlePage.openPagesCount) {
+            $scope.addTabMenu.splice(index, 1);
+        }
+    };
+
+    $scope.$on('$viewContentLoaded', function(event) {
+        $('a[href^="#"]').click(function(e){
+            e.preventDefault();
+        });
+    });
+}]);
