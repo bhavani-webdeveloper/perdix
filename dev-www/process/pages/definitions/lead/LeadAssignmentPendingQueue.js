4e2f3d680e0264178e0ea441a29c5b117274ffbf
rajkumar.v <rajkumar.v@dvara.com>
2016-11-14 11:33:50 +0000

    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/branches/v8.2.x-kinara_dev/source@1283 15cf4ce8-1186-411e-815e-a2b7fc64482c

---
+++ dev-www/process/pages/definitions/lead/LeadAssignmentPendingQueue.js
@@ -0,0 +1,150 @@
+irf.pageCollection.factory(irf.page("lead.leadAssignmentPendingQueue"), ["$log", "formHelper", "Lead", "$state", "$q", "SessionStore", "Utils", "entityManager",
+	function($log, formHelper, Lead, $state, $q, SessionStore, Utils, entityManager) {
+		var branch = SessionStore.getBranch();
+		return {
+			"type": "search-list",
+			"title": "LEAD_ASSIGNMENT_PENDING",
+			"subTitle": "",
+			initialize: function(model, form, formCtrl) {
+				model.branch = branch;
+				$log.info("search-list sample got initialized");
+			},
+			definition: {
+				title: "SEARCH_LEAD",
+				searchForm: [
+					"*"
+				],
+				searchSchema: {
+					"type": 'object',
+					"title": 'searchOptions',
+					"properties": {
+						"branch": {
+							"title": "HUB_NAME",
+							"type": "string",
+							"enumCode": "branch",
+							"x-schema-form": {
+								"type": "select",
+								"screenFilter": true
+							}
+						},
+						"leadName": {
+							"title": "LEAD_NAME",
+							"type": "string"
+						},
+						"businessName": {
+							"title": "BUSINESS_NAME",
+							"type": "string"
+						},
+						"area": {
+							"title": "AREA",
+							"type": "string"
+						},
+						"cityTownVillage": {
+							"title": "CITY/_TOWN_VILLAGE",
+							"type": "string"
+						}
+					},
+					"required": []
+				},
+				getSearchFormHelper: function() {
+					return formHelper;
+				},
+				getResultsPromise: function(searchOptions, pageOpts) {
+					var promise = Lead.search({
+						'branchName': searchOptions.branch,
+						'currentStage': "Assignment Pending",
+						'leadName': searchOptions.leadName,
+						'area': searchOptions.area,
+						'cityTownVillage': searchOptions.cityTownVillage,
+						'businessName': searchOptions.businessName,
+						'page': pageOpts.pageNo,
+						'per_page': pageOpts.itemsPerPage,
+					}).$promise;
+					return promise;
+				},
+				paginationOptions: {
+					"getItemsPerPage": function(response, headers) {
+						return 100;
+					},
+					"getTotalItemsCount": function(response, headers) {
+						return headers['x-total-count']
+					}
+				},
+				listOptions: {
+					selectable: false,
+					expandable: true,
+					listStyle: "table",
+					itemCallback: function(item, index) {},
+					getItems: function(response, headers) {
+						if (response != null && response.length && response.length != 0) {
+							return response;
+						}
+						return [];
+					},
+					getListItem: function(item) {
+						return [
+							item.leadId,
+							item.leadName,
+							item.businessName,
+							item.branchName,
+							item.addressLine1,
+							item.cityTownVillage,
+							item.pincode,
+							item.mobileNo
+						]
+					},
+					getTableConfig: function() {
+						return {
+							"serverPaginate": true,
+							"paginate": true,
+							"pageLength": 10
+						};
+					},
+					getColumns: function() {
+						return [{
+							title: 'ID',
+							data: 'id'
+						}, {
+							title: 'Lead Name',
+							data: 'leadName'
+						}, {
+							title: 'Business Name',
+							data: 'businessName'
+						}, {
+							title: 'Address Line1',
+							data: 'addressLine1'
+						}, {
+							title: 'City/Town Village',
+							data: 'cityTownVillage'
+						}, {
+							title: 'Area',
+							data: 'area'
+						}, {
+							title: 'Pincode',
+							data: 'pincode'
+						}]
+					},
+					getActions: function() {
+						return [{
+							name: "Assign Lead",
+							desc: "",
+							icon: "fa fa-pencil-square-o",
+							fn: function(item, index) {
+								entityManager.setModel('lead.LeadReassign', {
+									_request: item
+								});
+								$state.go("Page.Engine", {
+									pageName: "lead.LeadReassign",
+									pageId: item.id
+								});
+							},
+							isApplicable: function(item, index) {
+								return true;
+							}
+						}];
+					}
+				}
+			}
+		};
+	}
+]);
