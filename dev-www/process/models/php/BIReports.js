45bc0912533b558322269b27b143b3282be11e43
balakrishnan.r <balakrishnan.r@dvara.com>
2016-12-02 13:12:19 +0000

    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/branches/v8.2.x-kinara_dev/source@1731 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- dev-www/process/models/php/BIReports.js
+++ dev-www/process/models/php/BIReports.js
@@ -6,6 +6,18 @@ irf.models.factory('BIReports', function($resource, $httpParamSerializer, search
             method: 'GET',
             url: endpoint + '/report_list.php',
             isArray: true
+        },
+        reportMenuList: {
+            method: 'GET',
+            url: endpoint + '/biportal/api/MenuDefinition.php'
+        },
+		reportDataList: {
+            method: 'GET',
+            url: endpoint + '/biportal/api/ReportDefinition.php?report_id=:report_id'
+        },
+		reportTabList: {
+            method: 'GET',
+            url: endpoint + '/biportal/api/TabDefinition.php?menu_id=:menu_id'
         }
     });
 
