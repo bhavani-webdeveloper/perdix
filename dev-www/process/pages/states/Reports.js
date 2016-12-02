5877dc60e3b08ec10efa0f0362b5c91ed7d00e2c
balakrishnan.r <balakrishnan.r@dvara.com>
2016-12-02 13:10:24 +0000

    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/branches/v8.2.x-kinara_dev/source@1727 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- dev-www/process/pages/states/Reports.js
+++ dev-www/process/pages/states/Reports.js
@@ -1,8 +1,61 @@
 irf.pages.controller("ReportsCtrl",
-["$log", "$scope", "SessionStore", "$stateParams",
-    function($log, $scope, SessionStore, $stateParams){
+["$log", "$scope", "SessionStore", "$stateParams", "$q", "BIReports", "PageHelper",
+    function($log, $scope, SessionStore, $stateParams, $q, BIReports, PageHelper){
     $log.info("ReportsCtrl loaded");
 
     var userName = SessionStore.getLoginname();
+
+	//sample
+    PageHelper.showLoader();
+	
+	BIReports.reportMenuList().$promise.then(function(response) {
+        $scope.dashboardDefinition = response;
+    });
+	
+	$scope.ResultDataSet = [];
+	
+	$scope.onTabLoad = function(tabIndex, activeindex){
+		
+		
+		
+		if(!$scope.ResultDataSet[activeindex])
+		{
+			PageHelper.showLoader();
+			BIReports.reportDataList({"report_id":tabIndex}).$promise.then(function(response) {
+			
+			$scope.dataset = [];
+			
+			angular.forEach(response.ReportData, function(value, key){
+				
+				if(value.ReportType == 'CHART')
+				{
+					$scope.dataset[key] = value;			
+		
+				//include chart js	
+				}			
+				else
+				$scope.dataset[key] = value;
+			});
+			$scope.ResultDataSet[activeindex] = $scope.dataset;
+			PageHelper.hideLoader();
+    	});
+		}
+		
+	};
+	
+	BIReports.reportTabList({"menu_id":$stateParams.pageId}).$promise.then(function(response) {
+        console.log(response);
+		$scope.TabDefinition = response;
+    });
+	
+	
+    BIReports.reportList({"sd":"sd"}).$promise.then(function(resp){
+        self.formSource[0].items[0].titleMap = resp;
+        self.form = self.formSource;
+    }, function(errResp){
+        PageHelper.showErrors(errResp);
+    }).finally(function(){
+        PageHelper.hideLoader();
+    });
 
 }]);
