91b54169e688652358187a723b583176cf0f7a99
balakrishnan.r <balakrishnan.r@dvara.com>
2016-12-02 13:05:20 +0000

    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/branches/v8.2.x-kinara_dev/source@1725 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- dev-www/js/irf-env.js
+++ dev-www/js/irf-env.js
@@ -3,4 +3,5 @@ irf.BASE_URL = 'http://52.202.57.36:8080/perdix-server';
 irf.MANAGEMENT_BASE_URL = 'http://52.202.57.36:8081/management';
 irf.FORM_DOWNLOAD_URL = 'http://115.113.193.49:8080/formsKinara/formPrint.jsp';
 irf.BI_BASE_URL = "http://52.202.57.36:8081/bi-dev";
+//irf.BI_BASE_URL = "http://localhost/scoringsvn/bi-reports";
 irf.PSYCHOMETRIC_BASE_URL = "http://52.202.57.36:8080/psychometric-8.2";
