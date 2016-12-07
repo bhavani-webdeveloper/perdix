54419d3d0732999bedf5d13c58fcd435034eb454
shahal.tharique <shahal.tharique@dvara.com>
2016-12-07 04:44:31 +0000

    offline base
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/branches/v8.2.x-kinara_dev/source@1803 15cf4ce8-1186-411e-815e-a2b7fc64482c

---
+++ dev-www/modules/irfcommons/offlinemanager.js
@@ -0,0 +1,49 @@
+/**
+ * ======================================================================
+ * OFFLINE MANAGER
+ * ======================================================================
+ *
+ * Offline Manager facilitate usage of any storage for Storing data offline.
+ *
+ * All items stored under offline storage will be assigned a unique id, which
+ * can be used to remove the item from storage.
+ *
+ * Bundle Page
+ * ----------
+ * Every Bundle Page will store a list of items in the offline. This will be
+ * a collection of all page information (containing the page model) plus the
+ * bundleModel object. Items can be added, retrieved and removed from offline storage
+ * using the `OfflineManager`. Bundle can inject offline manager
+ *
+ * */
+
+irf.commons.factory('OfflineManager', ["$log", function($log){
+
+    return {
+        /**
+         * Add a new item under an id.
+         *
+         * @param {string} collectionId  Usually the page id.
+         * @param {object} item  The item to be saved in offline storage.
+         */
+        storeItem: function(collectionId, item){
+
+        },
+        /**
+         * Retrieve a list of items stored in offline.
+         *
+         * @param {string} collectionId Usually the pageId.
+         */
+        retrieveItems: function(collectionId){
+
+        },
+        /**
+         * Remove an item from the offline storage.
+         *
+         * @param {string} id  Id of the item.
+         */
+        removeItem: function(id){
+
+        }
+    }
+}])
