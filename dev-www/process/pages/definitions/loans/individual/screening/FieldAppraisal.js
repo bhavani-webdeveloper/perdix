b6d22f1426ead425b8d7c11c16c97145ed75f476
karthik.anandan <karthik.anandan@dvara.com>
2016-12-14 12:24:52 +0000

    Ticket #02413 - Summary page required in Field Appraisal Stage
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/branches/v8.2.x-kinara_dev/source@1991 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- dev-www/process/pages/definitions/loans/individual/screening/FieldAppraisal.js
+++ dev-www/process/pages/definitions/loans/individual/screening/FieldAppraisal.js
@@ -88,6 +88,18 @@ irf.pageCollection.factory(irf.page('loans.individual.screening.FieldAppraisal')
                                                 coApplicants[i].customerId = customers.urns[coApplicants[i].urn].id;
                                             }
                                             applicant.customerId = customers.urns[applicant.urn].id;*/
+
+                                            $this.bundlePages.push({
+                                                pageName: 'loans.individual.screening.Summary',
+                                                title: 'SUMMARY',
+                                                pageClass: 'summary',
+                                                minimum: 1,
+                                                maximum: 1,
+                                                model: {
+                                                    cbModel: {customerId:res.customerId,loanId:bundleModel.loanId, scoreName:'RiskScore2'}
+                                                }
+                                            });
+                                            
                                             $this.bundlePages.push({
                                                 pageName: 'customer.IndividualEnrolment2',
                                                 title: 'APPLICANT',
