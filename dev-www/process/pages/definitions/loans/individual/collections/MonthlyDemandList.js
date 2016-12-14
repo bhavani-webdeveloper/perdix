c86fc76636f581ad021d2835c2be094419b4c98e
rajkumar.v <rajkumar.v@dvara.com>
2016-12-14 14:16:24 +0000

    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/branches/v8.2.x-kinara_dev/source@2002 15cf4ce8-1186-411e-815e-a2b7fc64482c

---
+++ dev-www/process/pages/definitions/loans/individual/collections/MonthlyDemandList.js
@@ -0,0 +1,92 @@
+
+irf.pageCollection.factory(irf.page("loans.individual.collections.MonthlyDemandList"), 
+    ["$log", "SessionStore", 'Utils', 'ACH', 'AuthTokenHelper', 'PageHelper', 'formHelper', '$filter', '$q',
+    function($log, SessionStore, Utils, ACH, AuthTokenHelper, PageHelper, formHelper, $filter, $q) {
+        var branchIDArray = [];
+        return {
+            "type": "schema-form",
+            "title": "MONTHLY_DEMAND_LIST",
+            "subTitle": Utils.getCurrentDate(),
+
+            initialize: function(model, form, formCtrl) {
+                model.authToken = AuthTokenHelper.getAuthData().access_token;
+                model.userLogin = SessionStore.getLoginname();
+                model.demandlist = model.demandlist || {};
+                model.demandlist.demandDate = model.demandlist.demandDate || Utils.getCurrentDate();
+                //model.achDemand.updateDemand = model.achDemand.updateDemand || [];
+                for (var i = 0; i < formHelper.enum('branch_id').data.length; i++) {
+                    branchIDArray.push(parseInt(formHelper.enum('branch_id').data[i].code));
+                }
+
+            },
+            form: [{
+                "type": "box",
+                "title": "ACH_INPUT",
+                "items": [{
+                    "type": "fieldset",
+                    "title": "DOWNLOAD_MONTHLY_DEMAND_LIST",
+                    "items": [{
+                        "key": "demandlist.demandDate",
+                        "title": "INSTALLMENT_MONTH",
+                        "type": "date"
+                    }, {
+                        "title": "DOWNLOAD",
+                        "htmlClass": "btn-block",
+                        "icon": "fa fa-download",
+                        "type": "button",
+                        "notitle": true,
+                        "readonly": false,
+                        "onClick": function(model, formCtrl, form, $event) {
+                            if (!model.demandlist.demandDate) {
+                                PageHelper.setError({
+                                    'message': 'Installment Date is mandatory.'
+                                });
+                                return false;
+                            }
+                            PageHelper.clearErrors();
+                            PageHelper.showLoader();
+                            ACH.demandDownloadStatus({
+                                "demandDate": model.achCollections.demandDate,
+                                "branchId": branchIDArray.join(",")
+                            }).$promise.then(
+                                function(response) {
+                                    window.open(irf.BI_BASE_URL + "/download.php?user_id=" + model.userLogin + "&auth_token=" + model.authToken + "&report_name=ach_demands&date=" + model.achCollections.demandDate);
+                                    PageHelper.showProgress("page-init", "Success", 5000);
+                                },
+                                function(error) {
+                                    PageHelper.showProgress("page-init", error, 5000);
+                                }).finally(function() {
+                                PageHelper.hideLoader();
+                            });
+                        }
+                    }]
+                }]
+            }, {
+                "type": "box",
+                "title": "UPLOAD_MONTHLY_REALIZATION_REPORT",
+                "items": [{
+                    "type": "fieldset",
+                    "title": "UPLOAD_STATUS",
+                    "items": [{
+                            "key": "ach.achDemandListFileId",
+                            "notitle": true,
+                            "type": "file",
+                            "category": "ACH",
+                            "subCategory": "cat2",
+                            "fileType": "application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                            customHandle: function(file, progress, modelValue, form, model) {
+                                ACH.achDemandListUpload(file, progress);
+                            }
+                        }
+                    ]
+                }]
+            }],
+            schema: function() {
+                return ACH.getSchema().$promise;
+            },
+            actions: {
+                submit: function(model, form, formName) {}
+            }
+        };
+    }
+]);
