726b2ddc537f6afb2fcfbea764685fbce23d705e
rajkumar.v <rajkumar.v@dvara.com>
2016-12-16 17:21:46 +0000

    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/branches/v8.2.x-kinara_dev/source@2061 15cf4ce8-1186-411e-815e-a2b7fc64482c

---
+++ dev-www/process/pages/definitions/loans/individual/achpdc/ACHClose.js
@@ -0,0 +1,126 @@
+irf.pageCollection.factory(irf.page("loans.individual.achpdc.ACHClose"), ["$log", "ACH", "IndividualLoan", "LoanAccount", "PageHelper", "irfProgressMessage", "SessionStore", "$state", "Utils", "$stateParams", "formHelper", "CustomerBankBranch", 'Queries', "$q",
+	function($log, ACH, IndividualLoan, LoanAccount, PageHelper, irfProgressMessage, SessionStore, $state, Utils, $stateParams, formHelper, CustomerBankBranch, Queries, $q) {
+
+		var branch = SessionStore.getBranch();
+		var achSearchPromise;
+		var loanAccountPromise;
+		var queriesPromise;
+
+		return {
+			"type": "schema-form",
+			"title": "ACH_CLOSE",
+			"subTitle": "",
+
+			initialize: function(model, form, formCtrl) {
+				//Create Model ach
+				model.ach = model.ach || {};
+				model.temp = model.temp || {};
+				model.achACHSearch = model.achACHSearch || {};
+
+				$log.info("ACH_CLOSE got initialized");
+			},
+			offline: false,
+
+			getOfflineDisplayItem: function(item, index) {},
+
+			form: [{
+				"type": "box",
+				"notitle": true,
+				"items": [{
+					"type": "fieldset",
+					"title": "ACH_DETAILS",
+					"items": [{
+						key: "ach.accountNumber",
+						type: "lov",
+						autolov: true,
+						title: "ACCOUNT_NUMBER",
+						bindMap: {},
+						inputMap: {
+
+						},
+						outputMap: {
+
+						},
+						searchHelper: formHelper,
+						search: function(inputModel, form, model) {
+							// console.log(model.ach.accountNumber);
+							return ACH.search({
+								'accountNumber': model.ach.accountNumber
+							}).$promise;
+						},
+						onSelect: function(result, model, arg1) {
+							model.ach = result;
+							$log.info(model.temp);
+						},
+						getListDisplayItem: function(item, index) {
+							return [
+								'Account Number : ' + item.accoundId,
+								'Branch : ' + item.bankName,
+								'Bank : ' + item.branchName,
+								'IFSC Code : ' + item.ifscCode
+							];
+						}
+					}, {
+						"key": "ach.accountType",
+						"title": "ACCOUNT_TYPE",
+					}, {
+						"key": "ach.applicantName",
+						"title": "APPLICATION_NAME",
+					}, {
+						"key": "ach.bankAccountNumber",
+						"title": "BANK_ACCOUNT_NUMBER",
+					}, {
+						"key": "ach.accountHolderName",
+						"title": "ACCOUNT_HOLDER_NAME",
+					}, {
+						"key": "ach.bankName",
+						"title": "BANK_NAME",
+					}, {
+						"key": "ach.ifscCode",
+						"title": "IFSC_CODE",
+					}, {
+						"key": "ach.registrationDate",
+						"title": "REGISTRATION_DATE",
+					}]
+				}]
+			}, {
+				"type": "actionbox",
+				"items": [{
+					"type": "submit",
+					"title": "SUBMIT"
+				}]
+			}],
+
+			schema: function() {
+				return ACH.getSchema().$promise;
+			},
+
+			actions: {
+				submit: function(model, form, formName) {
+					PageHelper.clearErrors();
+					PageHelper.showLoader();
+
+					ACH.ACHClose(model.ach).$promise.then(
+						function(response) {
+							PageHelper.hideLoader();
+							PageHelper.showProgress("page-init", "Done.", 2000);
+							/*$state.go("Page.Engine", {
+								pageName: 'loans.individual.achpdc.ACHPDCQueue',
+								pageId: null
+							});*/
+						},
+						function(errorResponse) {
+							PageHelper.hideLoader();
+							PageHelper.showErrors(errorResponse);
+						}
+					).finally(function() {
+						PageHelper.hideLoader();
+					});
+
+					//model.ach=response;
+				},
+			}
+
+		}
+	}
+]);
