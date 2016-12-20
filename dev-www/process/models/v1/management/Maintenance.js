dc46664d81b379da82707d83da8d09654b138ca5
rajkumar.v <rajkumar.v@dvara.com>
2016-12-20 12:29:33 +0000

    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/branches/v8.2.x-kinara_dev/source@2139 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- dev-www/process/models/v1/management/Maintenance.js
+++ dev-www/process/models/v1/management/Maintenance.js
@@ -1,6 +1,7 @@
 irf.models.factory('Maintenance', ["$resource", "$httpParamSerializer", "BASE_URL", "searchResource", "Upload", "$q", "PageHelper",
     function($resource, $httpParamSerializer, BASE_URL, searchResource, Upload, $q, PageHelper) {
         var endpoint = BASE_URL + '/api/maintenance';
+        var biEndPoint= irf.BI_BASE_URL;
 
         var res = $resource(endpoint, null, {
             updateSpoke: {
@@ -8,7 +9,38 @@ irf.models.factory('Maintenance', ["$resource", "$httpParamSerializer", "BASE_UR
                 url: endpoint + '/centreMerger'
             },
 
+            getMasterData1: {
+                method: 'GET',
+                url: biEndPoint + '/upload_list.php'
+            },
+
+             getMasterData: searchResource({
+                method: 'GET',
+                url: biEndPoint + '/upload_list.php'
+            }),
+
         });
+
+        res.masterDataUpload = function(file, progress, opts) {
+            var deferred = $q.defer();
+            reqData = {
+                "file": file
+            };
+            Upload.upload({
+                url: irf.BI_BASE_URL + "/upload.php?upload_name=" + opts.fileType,
+                data: reqData
+            }).then(function(resp){
+                // TODO handle success
+                PageHelper.showProgress("page-init", "Done.", 2000);
+                deferred.resolve(resp);
+            }, function(errResp){
+                // TODO handle error
+                PageHelper.showErrors(errResp);
+                deferred.reject(errResp);
+            }, progress);
+            return deferred.promise;
+        };
+
         return res;
     }
 ]);
--- dev-www/process/pages/definitions/management/MasterDataMaintenance.js
+++ dev-www/process/pages/definitions/management/MasterDataMaintenance.js
@@ -1,8 +1,8 @@
 
 irf.pageCollection.factory(irf.page("management.MasterDataMaintenance"), 
-    ["$log", "SessionStore", 'Utils', 'ACH', 'AuthTokenHelper', 'PageHelper', 'formHelper', '$filter', '$q',
-    function($log, SessionStore, Utils, ACH, AuthTokenHelper, PageHelper, formHelper, $filter, $q) {
-        var branchIDArray = [];
+    ["$log", "SessionStore", 'Utils', 'ACH','Maintenance','Files', 'AuthTokenHelper', 'PageHelper', 'formHelper', '$filter', '$q',
+    function($log, SessionStore, Utils, ACH,Maintenance, Files,AuthTokenHelper, PageHelper, formHelper, $filter, $q) {
+        var branchIDArray = [];  
         return {
             "type": "schema-form",
             "title": "MASTER_DATA_MAINTENANCE",
@@ -12,6 +12,13 @@ irf.pageCollection.factory(irf.page("management.MasterDataMaintenance"),
                 model.authToken = AuthTokenHelper.getAuthData().access_token;
                 model.userLogin = SessionStore.getLoginname();
                 model.master = model.master|| {};
+
+                var promise= Maintenance.getMasterData().$promise;
+                promise.then(function(res){
+                    $log.info("hi printing");
+                    $log.info(res);
+                })
+
                 model.master.demandDate = model.master.demandDate || Utils.getCurrentDate();
                 //model.achDemand.updateDemand = model.achDemand.updateDemand || [];
                 for (var i = 0; i < formHelper.enum('branch_id').data.length; i++) {
@@ -26,43 +33,47 @@ irf.pageCollection.factory(irf.page("management.MasterDataMaintenance"),
                     "type": "fieldset",
                     "title": "DOWNLOAD_MASTER_DATA_TEMPLATE",
                     "items": [{
-                        "key": "master.name",
+                        "key": "master.uploadName",
                         "title": "NAME",
-                        "type": "select"
+                        "type":"lov",
+                       autolov: true,
+                        bindMap: {
+                        },
+                        searchHelper: formHelper,
+                        search: function(inputModel, form, model) {
+                            return Maintenance.getMasterData().$promise;
+                        },
+                        getListDisplayItem: function(item, index) {
+                            return [
+                                item.group,
+                                item.name,
+                                item.value,
+                                item.template_file
+                            ];
+                        },
+                        onSelect: function(result, model, context) {
+                            $log.info(result);
+                            model.master.templateFile = result.template_file;
+                            model.master.uploadName = result.name;
+                            model.master.uploadNameValue= result.value;
+                        }
                     },{
-                        "key": "master.url",
+                        "key": "master.templateFile",
                         "title": "TEMPLATE_URL",
                         "type": "string"
-                    }, {
-                        "title": "DOWNLOAD",
-                        "htmlClass": "btn-block",
-                        "icon": "fa fa-download",
+                    },
+                    {
                         "type": "button",
+                        "title": "DOWNLOAD",
+                        "icon": "fa fa-download",
                         "notitle": true,
                         "readonly": false,
-                        "onClick": function(model, formCtrl, form, $event) {
-                            if (!model.demandlist.demandDate) {
-                                PageHelper.setError({
-                                    'message': 'Installment Date is mandatory.'
-                                });
-                                return false;
-                            }
-                            PageHelper.clearErrors();
-                            PageHelper.showLoader();
-                            ACH.demandDownloadStatus({
-                                "demandDate": model.achCollections.demandDate,
-                                "branchId": branchIDArray.join(",")
-                            }).$promise.then(
-                                function(response) {
-                                    window.open(irf.BI_BASE_URL + "/download.php?user_id=" + model.userLogin + "&auth_token=" + model.authToken + "&report_name=ach_demands&date=" + model.achCollections.demandDate);
-                                    PageHelper.showProgress("page-init", "Success", 5000);
-                                },
-                                function(error) {
-                                    PageHelper.showProgress("page-init", error, 5000);
-                                }).finally(function() {
-                                PageHelper.hideLoader();
-                            });
-                        }
+
+                        "onClick": function(model, form, schemaForm, event) {
+                            var fileId = irf.BI_BASE_URL + "/upload_template/" + model.master.templateFile ;
+                            Utils.downloadFile(fileId );
+                            //Utils.downloadFile(irf.MANAGEMENT_BASE_URL + "/forms/AllFormsDownload.php?record_id=" + model.loanAccount.id);
+                    },
                     }]
                 }]
             }, {
@@ -72,19 +83,15 @@ irf.pageCollection.factory(irf.page("management.MasterDataMaintenance"),
                     "type": "fieldset",
                     "title": "UPLOAD_STATUS",
                     "items": [
-                         {
-                               key: "ach.customerId",
-                               title: "CUSTOMER_ID",
-                            },
                     {
-                            "key": "ach.achDemandListFileId",
+                            "key": "master.achDemandListFileId",
                             "notitle": true,
                             "type": "file",
                             "category": "ACH",
                             "subCategory": "cat2",
                             "fileType": "application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
                             customHandle: function(file, progress, modelValue, form, model) {
-                                ACH.monthlyDemandUpload(file, progress,{customerId:model.ach.customerId});
+                                Maintenance.masterDataUpload(file, progress, {fileType: model.master.uploadNameValue});
                             }
                         }
                     ]
