f475e570c6fcd715d2e8f0fe6fcd2b58bd0fbea7
stalin.solomon <stalin.solomon@dvara.com>
2017-01-12 09:09:15 +0000

    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/branches/v8.2.x-kinara_dev/source@2488 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- dev-www/process/pages/states/Reports.js
+++ dev-www/process/pages/states/Reports.js
@@ -1,50 +1,50 @@
 irf.pages.controller("ReportsCtrl",
 ["$log", "$scope", "SessionStore", "$stateParams", "$q", "BIReports", "PageHelper",
-    function($log, $scope, SessionStore, $stateParams, $q, BIReports, PageHelper){
-    $log.info("ReportsCtrl loaded");
+	function($log, $scope, SessionStore, $stateParams, $q, BIReports, PageHelper){
+	$log.info("ReportsCtrl loaded");
 
-    var userName = SessionStore.getLoginname();
+	var userName = SessionStore.getLoginname();
 
-    $scope.currentUserRole = SessionStore.getUserRole();
+	$scope.currentUserRole = SessionStore.getUserRole();
 
 	//sample
-    PageHelper.showLoader();
+	PageHelper.showLoader();
 	
 	BIReports.reportMenuList().$promise.then(function(response) {
-        $scope.dashboardDefinition = response;
-    });
-	
+		$scope.dashboardDefinition = response;
+	});
+
 	//for hub dropdown
-	BIReports.reportFilterList({"DropDownType" : "region" , "selectedValue" :""}).$promise.then(function(response) {
-        $scope.regionName = response.items;
-    });
-	
+	BIReports.reportFilterList({"DropDownType":"region", "selectedValue":""}).$promise.then(function(response) {
+		$scope.regionName = response.items;
+	});
+
 	//for branch dropdown on select
-	$scope.listHubs = function(){
-		BIReports.reportFilterList({"DropDownType" : "Hub" , "selectedValue" : $scope.selectedRegion.id}).$promise.then(function(response) {
-        	$scope.HubNames = response.items;
-    	});
+	$scope.listHubs = function() {
+		$scope.HubNames = [];
+		$scope.loanOfficers = [];
+		BIReports.reportFilterList({"DropDownType":"Hub", "selectedValue":$scope.selectedRegion.id}).$promise.then(function(response) {
+			$scope.HubNames = response.items;
+		});
 	}
-	
+
 	//for users dropdown on select
-	$scope.listBranches = function(){
-		BIReports.reportFilterList({"DropDownType" : "branch" , "selectedValue" : $scope.selectedHub.id}).$promise.then(function(response) {
-        	$scope.loanOfficers = response.items;
-    	});
+	$scope.listBranches = function() {
+		BIReports.reportFilterList({"DropDownType":"branch", "selectedValue":$scope.selectedHub.id}).$promise.then(function(response) {
+			$scope.loanOfficers = response.items;
+		});
 	}
-	
+
 	//on pressing the button apply filter
-	$scope.processForm = function(){
-    	alert($scope.selectedRegion.id+'/'+$scope.selectedHub.id+'/'+$scope.selectedHub.id);
+	$scope.processFilter = function() {
+		alert($scope.selectedRegion.id+'/'+$scope.selectedHub.id+'/'+$scope.selectedHub.id);
 	}
-	
-	
+
 	$scope.ResultDataSet = [];
-	
+
 	$scope.onTabLoad = function(tabIndex, activeindex){	
 		
-		if(!$scope.ResultDataSet[activeindex])
-		{
+		if(!$scope.ResultDataSet[activeindex]) {
 			PageHelper.showLoader();
 			BIReports.reportDataList({"report_id":tabIndex}).$promise.then(function(response) {
 			
@@ -52,44 +52,36 @@ irf.pages.controller("ReportsCtrl",
 			
 			angular.forEach(response.ReportData, function(value, key){
 				
-				if(value.ReportType == 'CHART')
-				{
-					$scope.dataset[key] = value;			
-		
-				//include chart js	
-				}			
-				else
-				$scope.dataset[key] = value;
+				if(value.ReportType == 'CHART') {
+					$scope.dataset[key] = value;
+				} else {
+					$scope.dataset[key] = value;
+				}
 			});
 			$scope.ResultDataSet[activeindex] = $scope.dataset;
 			PageHelper.hideLoader();
-    	});
+		});
 		}
 		
-		$scope.onheaderClick = function(key, report_id){
-			
+		$scope.onheaderClick = function(key, report_id) {
 			var drillDownUrl = irf.REPORT_BASE_URL + '/ReportDrilldown.php?report_id='+report_id+'&key='+key;
-                    $log.info(drillDownUrl);
-                    window.open(drillDownUrl);
+			$log.info(drillDownUrl);
+			window.open(drillDownUrl);
 		};
-		
 	};
-	
-	BIReports.reportTabList({"menu_id":$stateParams.pageId}).$promise.then(function(response) {
-        console.log(response);
-		$scope.TabDefinition = response;
-    });
-	
-	
-	
-	
-    BIReports.reportList({"sd":"sd"}).$promise.then(function(resp){
-        self.formSource[0].items[0].titleMap = resp;
-        self.form = self.formSource;
-    }, function(errResp){
-        PageHelper.showErrors(errResp);
-    }).finally(function(){
-        PageHelper.hideLoader();
-    });
 
+	BIReports.reportTabList({"menu_id":$stateParams.pageId}).$promise.then(function(response) {
+		console.log(response);
+		$scope.TabDefinition = response;
+	});
+/*
+	BIReports.reportList({"sd":"sd"}).$promise.then(function(resp){
+		self.formSource[0].items[0].titleMap = resp;
+		self.form = self.formSource;
+	}, function(errResp){
+		PageHelper.showErrors(errResp);
+	}).finally(function(){
+		PageHelper.hideLoader();
+	});
+*/
 }]);
