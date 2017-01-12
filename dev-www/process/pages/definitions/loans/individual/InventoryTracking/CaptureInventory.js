5fd553e040070bec8b4df4245fe40beb8c0e28fa
rajkumar.v <rajkumar.v@dvara.com>
2017-01-12 07:14:44 +0000

    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/branches/v8.2.x-kinara_dev/source@2478 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- dev-www/process/pages/definitions/loans/individual/InventoryTracking/CaptureInventory.js
+++ dev-www/process/pages/definitions/loans/individual/InventoryTracking/CaptureInventory.js
@@ -1,148 +1,123 @@
-irf.pageCollection.factory(irf.page("loans.individual.InventoryTracking.CaptureInventory"), 
-    ["$log", "$state", "document", "SessionStore", "formHelper", "$q", "irfProgressMessage",
-"PageHelper", "Utils", "PagesDefinition", "Queries",
+irf.pageCollection.factory(irf.page("loans.individual.InventoryTracking.CaptureInventory"), ["$log", "$state", "document", "SessionStore", "formHelper", "$q", "irfProgressMessage",
+    "PageHelper", "Utils", "PagesDefinition", "Queries",
 
 
-function($log,$state,document, SessionStore, formHelper, $q, irfProgressMessage,
-    PageHelper, Utils, PagesDefinition, Queries) {
+    function($log, $state, document, SessionStore, formHelper, $q, irfProgressMessage,
+        PageHelper, Utils, PagesDefinition, Queries) {
 
-    var branch = SessionStore.getBranch();
+        var branch = SessionStore.getBranch();
 
-    return {
-        "type": "schema-form",
-        "title": "",
-        "subTitle": "",
-        initialize: function(model, form, formCtrl) {
-            model.doc = model.doc || {};
-            $log.info("Dispatch Incomplete documents is initiated ");
-        },
+        return {
+            "type": "schema-form",
+            "title": "CAPTURE_INVENTORY",
+            "subTitle": "",
+            initialize: function(model, form, formCtrl) {
+                model.inventory = model.inventory || {};
+                $log.info("Capture Inventory page  is initiated ");
+            },
 
-        modelPromise: function(pageId, _model) {
-
-        },
-
-        offline: true,
-        getOfflineDisplayItem: function(item, index) {
-            return []
-        },
-
-         form: [{
-                "type": "box",
-                "title": "DISPATCH_DETAILS",
-                "items": [{
-                    key: "doc.Dispatch[].CourierSentDate",
-                    title: "COURIER_SENT_DATE",
-                    type:"date"
-                }, {
-                    key: "doc.Dispatch[].CourierCompanyName",
-                    title: "CourierCompanyName",
-                }, {
-                    key: "doc.Dispatch[].PodNumber",
-                    title: "POD_NUMBER",
-                }, {
-                    key: "doc.Dispatch[].BatchNumber",
-                    title: "BATCHNUMBER",
-                    readonly: true
-                }, {
-                    key: "doc.Dispatch[].Remarks",
-                    title: "REMARKS",
-                }]
-
+            modelPromise: function(pageId, _model) {
 
             },
 
-
-            {
-                "type": "actionbox",
-                "items": [{
-                    "type": "submit",
-                    "title": "Submit"
-                }]
+            offline: true,
+            getOfflineDisplayItem: function(item, index) {
+                return []
             },
-        ],
 
-        /*schema: function() {
-            return document.getSchema().$promise;
-        },*/
+            form: [{
+                    type: "box",
+                    title: "CAPTURE_INVENTORY",
+                    items: [{
+                        key: "inventory.inventoryTrackerDto",
+                        type: "array",
+                        startEmpty: true,
+                        view: "fixed",
+                        title: "INVENTORY",
+                        items: [{
+                            key: "inventory.inventoryTrackerDto[].branchId",
+                            type: "date",
+                            readonly: true,
+                        }, {
+                            key: "inventory.inventoryTrackerDto[].inventoryName",
+                            readonly: true,
+                        }, {
+                            key: "inventory.inventoryTrackerDto[].numberOfInventories",
 
-         schema: {
+                        }]
+                    }]
+                },
+
+
+
+                {
+                    "type": "actionbox",
+                    "items": [{
+                        "type": "submit",
+                        "title": "Submit"
+                    }]
+                },
+            ],
+
+
+            schema: {
                 "$schema": "http://json-schema.org/draft-04/schema#",
                 "type": "object",
                 "properties": {
-                    "address": {
+                    "inventory": {
                         "type": "object",
-                        "title":"Address",
-                        "properties": {
-                            "streetAddress": {
-                                "type": "string",
-                                "title":"Street Address"
-                            },
-                            "city": {
-                                "type": "string",
-                                "title":"City"
-                            }
-                        },
                         "required": [
-                            "streetAddress",
-                            "city"
-                        ]
-                    },
-                    "phoneNumber": {
-                        "type": "array",
-                        "title":"Phone Numbers",
-                        "items": {
-                            "type": "object",
-                            "title":"Phone#",
-                            "properties": {
-                                "location": {
-                                    "type": "string",
-                                    "title":"Location"
-                                },
-                                "code": {
-                                    "type": "integer",
-                                    "title":"Code"
-                                },
-                                "number":{
-                                    "type":"integer",
-                                    "title":"Number"
-                                },
-                                "boo": {
-                                    "type": "boolean",
-                                    "title": "Check Done?"
+                        ],
+                        "properties": {
+                            "inventoryTrackerDto": {
+                                "type": "array",
+                                "items": {
+                                    "type": "object",
+                                    "required": [
+                                        "branchId",
+                                        "inventoryName",
+                                        "numberOfInventories"
+                                    ],
+                                    "properties": {
+                                        "branchId": {
+                                            "title": "MAKE",
+                                            "type": "string"
+                                        },
+                                        "inventoryName": {
+                                            "title": "TYPE",
+                                            "type": "string"
+                                        },
+                                        "numberOfInventories": {
+                                            "title": "YEAR",
+                                            "type": "string"
+                                        }
+                                    }
                                 }
-                            },
-                            "required": [
-                                "code",
-                                "number"
-                            ]
+                            }
                         }
                     }
-                },
-                "required": [
-                    "address",
-                    "phoneNumber"
-                ]
-            },
-
-        actions: {
-            preSave: function(model, form, formName) {
-                $log.info("Inside save()");
-                var deferred = $q.defer();
-                if (model.doc) {
-                    deferred.resolve();
-                } else {
-                    irfProgressMessage.pop('Dispatch-save', ' PodNumber is required', 3000);
-                    deferred.reject();
                 }
-                return deferred.promise;
             },
+            actions: {
+                preSave: function(model, form, formName) {
+                    $log.info("Inside save()");
+                    var deferred = $q.defer();
+                    if (model.doc) {
+                        deferred.resolve();
+                    } else {
+                        irfProgressMessage.pop('Dispatch-save', ' PodNumber is required', 3000);
+                        deferred.reject();
+                    }
+                    return deferred.promise;
+                },
 
-            submit: function(model, form, formName) {
-                $log.info("Inside submit()");
-                irfProgressMessage.pop('Dispatch-save', 'Dispatch is successfully created', 3000);
-                $log.warn(model);
+                submit: function(model, form, formName) {
+                    $log.info("Inside submit()");
+                    irfProgressMessage.pop('Dispatch-save', 'Dispatch is successfully created', 3000);
+                    $log.warn(model);
+                }
             }
-        }
-    };
+        };
 
-}]);
+    }
+]);
