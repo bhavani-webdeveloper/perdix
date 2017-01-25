8a2b342b282baf8f9376d98e6b9057d24972efd2
rajkumar.v <rajkumar.v@dvara.com>
2017-01-25 05:40:07 +0000

    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/branches/v8.2.x-kinara_dev/source@2594 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- dev-www/process/pages/definitions/management/SpokeMerger.js
+++ dev-www/process/pages/definitions/management/SpokeMerger.js
@@ -1,6 +1,6 @@
-irf.pageCollection.factory(irf.page("management.SpokeMerger"), ["$log", "Maintenance","Enrollment" ,"$state", "$stateParams", "Lead", "SessionStore",
+irf.pageCollection.factory(irf.page("management.SpokeMerger"), ["$log", "Maintenance", "Enrollment", "$state", "$stateParams", "Lead", "SessionStore",
     "formHelper", "$q", "irfProgressMessage", "PageHelper", "Utils", "PagesDefinition", "Queries",
-    function($log, Maintenance,Enrollment, $state, $stateParams, Lead, SessionStore, formHelper, $q, irfProgressMessage,
+    function($log, Maintenance, Enrollment, $state, $stateParams, Lead, SessionStore, formHelper, $q, irfProgressMessage,
         PageHelper, Utils, PagesDefinition, Queries) {
 
         var branch = SessionStore.getBranch();
@@ -52,17 +52,74 @@ irf.pageCollection.factory(irf.page("management.SpokeMerger"), ["$log", "Mainten
                                 1: "YES",
                                 0: "NO"
                             }
-                        }, 
-                       
-                        {
-                            key: "customer.customerIds",
-                            title: "Customer Id",
                         },
 
+                        {
+                            type: "fieldset",
+                            condition:"model.customer.allMembers== 0",
+                            title: "Customer Ids",
+                            items: [{
+                                key: "customer.customerid",
+                                type: "array",
+                                title: "Add CustomerIds",
+                                items: [{
+                                    "key": "customer.customerid[].id",
+                                    "title": "Customer Id",
+                                    "type": "lov",
+                                    "inputMap": {
+                                        "firstName": {
+                                            "key": "customer.firstName",
+                                            "title": "CUSTOMER_NAME"
+                                        },
+                                        "kgfsName": {
+                                            "key": "customer.kgfsName",
+                                            "type": "select",
+                                            "enumCode": "branch_id"
+                                        },
+                                        "centreId": {
+                                            "key": "customer.centreId",
+                                            "type": "select",
+                                            "enumCode": "centre",
+                                            "parentEnumCode": "branch_id"
+                                        }
+                                    },
+                                    "outputMap": {
+                                        "firstName": "customer.customerid[arrayIndex].firstName",
+                                        "id": "customer.customerid[arrayIndex].id",
+                                    },
+                                    "searchHelper": formHelper,
+                                    "search": function(inputModel, form) {
+                                        $log.info("SessionStore.getBranch: " + SessionStore.getBranch());
+                                        var branches = formHelper.enum('branch_id').data;
+                                        var branchId;
+                                        for (var i = 0; i < branches.length; i++) {
+                                            if (branches[i].id == inputModel.firstName)
+                                                branchId = branches[i].id;
+                                        }
+                                        var promise = Enrollment.search({
+                                            'branchName': branchId || SessionStore.getBranch(),
+                                            'firstName': inputModel.firstName,
+                                            'centreId': inputModel.centreId,
+                                            'customerType': ""
+                                        }).$promise;
+                                        return promise;
+                                    },
+                                    getListDisplayItem: function(data, index) {
+                                        return [
+                                            [data.firstName, data.fatherFirstName].join(' | '),
+                                            data.id,
+                                            data.urnNo
+                                        ];
+                                    },
+                                    onSelect: function(valueObj, model, context) {}
+                                }, {
+                                    key: "customer.customerid[].firstName",
+                                }]
+                            }]
+                        },
 
                         {
                             type: "actionbox",
-                            // condition: "model.spoke.config",
                             items: [{
                                 type: "submit",
                                 title: "submit"
@@ -83,7 +140,11 @@ irf.pageCollection.factory(irf.page("management.SpokeMerger"), ["$log", "Mainten
                 "properties": {
                     "customer": {
                         "type": "object",
-                        "required": [],
+                        "required": [
+                        "fromCentreId",
+                        "toCentreId",
+                        "allMembers"
+                        ],
                         "properties": {
                             "branchId": {
                                 "type": "number",
@@ -98,7 +159,41 @@ irf.pageCollection.factory(irf.page("management.SpokeMerger"), ["$log", "Mainten
                                 "type": "number",
                                 "title": "SPOKE_NAME",
                                 "enumCode": "TO_CENTRE"
-                            }
+                            },
+                            "firstName": {
+                                "type": "string",
+                                "title": "CUSTOMER_NAME"
+                            },
+                            "kgfsName": {
+                                "type": ["number", "null"],
+                                "title": "HUB_NAME"
+                            },
+                            "allMembers": {
+                                "type": ["string", "null"],
+                            },
+                            "centreId": {
+                                "type": ["number", "null"],
+                                "title": "SPOKE_NAME"
+                            },
+                            "customerid": {
+                                "type": "array",
+                                "title": "CUSTOMER_ID",
+                                "items": {
+                                    "type": "object",
+                                    "required": ["id"],
+                                    "properties": {
+                                        "firstName": {
+                                            "type": ["string", "null"],
+                                            "title": "CUSTOMER_NAME"
+                                        },
+                                        "id": {
+                                            "type": ["number", "null"],
+                                            "title": "CUSTOMER_ID"
+                                        }
+                                    }
+                                }
+                            },
+
                         }
                     }
                 }
@@ -106,7 +201,21 @@ irf.pageCollection.factory(irf.page("management.SpokeMerger"), ["$log", "Mainten
             actions: {
                 submit: function(model, form, formName) {
                     $log.info('on submit action ....');
+
+
+                    var idtemp = [];
+                    if (model.customer.customerid && model.customer.customerid.length) {
+                        for (i = 0; i < model.customer.customerid.length; i++) {
+                            idtemp.push(model.customer.customerid[i].id);
+                        }
+                    }
+
+                    if (idtemp && idtemp.length) {
+                        model.customer.customerIds = idtemp;
+                    }
+
                     $log.warn(model);
+
                     var sortFn = function(unordered) {
                         var out = {};
                         Object.keys(unordered).sort().forEach(function(key) {
