ecd46b9c30a49d50783f70f8705bdd8e1735ca34
rajkumar.v <rajkumar.v@dvara.com>
2017-02-08 07:23:31 +0000

    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/branches/v8.2.x-kinara_dev/source@2783 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- dev-www/process/pages/definitions/loans/LoanAmend.js
+++ dev-www/process/pages/definitions/loans/LoanAmend.js
@@ -10,7 +10,7 @@ irf.pageCollection.factory(irf.page('loans.LoanAmend'), ["$log", "$q", "$timeout
                     pageId: urnNo
                 });
             } catch (err) {
-                console.log(err);
+                console.log(err); 
             }
         }
 
