f57870d3bb48742a397bb72420551b1f92af1c40
stalin.solomon <stalin.solomon@dvara.com>
2017-04-20 12:16:00 +0000

    Adhoc page support added
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/trunk@3558 15cf4ce8-1186-411e-815e-a2b7fc64482c

---
+++ source/dev-www/modules/irfpages/states/Page.Adhoc.js
@@ -0,0 +1,21 @@
+irf.pages.controller("PageAdhocCtrl", ["$scope", "$stateParams", function($scope, $stateParams) {
+	$scope.pageNameHtml = $stateParams.pageName.split('.').join('<br/>');
+}]);
+irf.pages.directive("irfPageAdhoc", ["$stateParams", "$templateRequest", "$compile", function($stateParams, $templateRequest, $compile) {
+	return {
+		link: function (scope, element, attrs) {
+			$templateRequest(irf.templateUrl($stateParams.pageName)).then(function(html) {
+				element.html(html);
+				$compile(element.contents())(scope);
+			});
+		},
+		controller: ["$controller", "$scope", "$stateParams", function($controller, $scope, $stateParams) {
+			try {
+				$scope.$parent.error = null;
+				return $controller(irf.controller($stateParams.pageName), {$scope: $scope});
+			} catch (e) {
+				$scope.$parent.error = $stateParams.pageName;
+			}
+		}]
+	};
+}]);
