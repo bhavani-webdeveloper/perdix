8a4245f1a43eb00b6b46a19b7e2ec8dbe69583ba
rajkumar.v <rajkumar.v@dvara.com>
2017-05-04 07:11:37 +0000

    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/trunk@3655 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- source/dev-www/process/pages/definitions/loans/group/ApplicationPendingQueue.js
+++ source/dev-www/process/pages/definitions/loans/group/ApplicationPendingQueue.js
@@ -1,55 +1,125 @@
-irf.pageCollection.factory("Pages__ApplicationPendingQueue", ["$log", "formHelper", "Groups","$state",
-    "SessionStore","groupCommons",
-    function($log, formHelper, Groups,$state,SessionStore,groupCommons){
-        var listOptions= {
-            itemCallback: function(item, index) {
-                // This will not be called in case of selectable = true in definition
-                $log.info(item);
-                $state.go("Page.Engine",{
-                    pageName:"GroupCRUD",
-                    pageId:item.id,
-                    pageData:{
-                        intent:"APP_DWNLD"
-                    }
-                },{
-                    reload: true,
-                    inherit: false,
-                    notify: true
-                });
-            },
-            getItems: function(response, headers){
-                if (response!=null && response.length && response.length!=0){
-                    return response;
-                }
-                return [];
-            },
-            getListItem: function(item){
-                return [
+define({
+	pageUID: "loans.group.ApplicationPendingQueue2",
+	pageType: "Engine",
+	dependencies: ["$log", "$state", "Groups","entityManager", "Enrollment", "CreditBureau", "Journal", "$stateParams", "SessionStore", "formHelper", "$q", "irfProgressMessage",
+		"PageHelper", "Utils", "PagesDefinition", "Queries", "irfNavigator"
+	],
+	$pageFn: function($log, $state, Groups,entityManager, Enrollment, CreditBureau, Journal, $stateParams, SessionStore, formHelper, $q, irfProgressMessage,
+		PageHelper, Utils, PagesDefinition, Queries, irfNavigator) {
 
-                    'Group ID : ' + item.id,
-                    'Group Name : '+item.groupName,
-                    null
-                ]
-            },
-            getActions: function(){
+		var branchId = SessionStore.getBranchId();
+		var branchName = SessionStore.getBranch();
 
-                return [
+		return {
+			"type": "search-list",
+			"title": "Application Pending Queue",
+			"subTitle": "",
+			initialize: function(model, form, formCtrl) {
+				$log.info("Application Pending Queue got initialized");
+			},
+			definition: {
+				title: "Application Pending Queue",
+				searchForm: [
+					"*"
+				],
+				//autoSearch: true,
+				searchSchema: {
+					"type": 'object',
+					"title": 'SearchOptions',
+					"properties": {
+						"partner": {
+							"type": "string",
+							"title": "PARTNER",
+							"x-schema-form": {
+								"type": "select",
+								"enumCode": "partner"
+							}
+						}
+					},
+					"required": []
+				},
 
-                ];
-            }
-        };
+				getSearchFormHelper: function() {
+					return formHelper;
+				},
+				getResultsPromise: function(searchOptions, pageOpts) {
 
-        var definition = groupCommons.getSearchDefinition('StageAP',listOptions);
-        return {
-            "id": "applicationpendingqueue",
-            "type": "search-list",
-            "name": "ApplicationPendingQueue",
-            "title": "Application Pending Queue",
-            "subTitle": "",
-            "uri":"Groups/Application Pending Queue",
-            initialize: function (model, form, formCtrl) {
-                $log.info("AP Q got initialized");
-            },
-            definition: definition
-        };
-    }]);
+					var params = {
+						'branchId': branchId,
+						'partner': searchOptions.partner,
+						'groupStatus': true,
+						'page': pageOpts.pageNo,
+						//'currentStage': "StageAP",
+						'per_page': pageOpts.itemsPerPage
+					};
+
+					var promise = Groups.search(params).$promise;
+					return promise;
+				},
+				paginationOptions: {
+					"getItemsPerPage": function(response, headers) {
+						return 100;
+					},
+					"getTotalItemsCount": function(response, headers) {
+						return headers['x-total-count']
+					}
+				},
+				listOptions: {
+					selectable: false,
+					expandable: true,
+					listStyle: "table",
+					itemCallback: function(item, index) {},
+					getItems: function(response, headers) {
+						if (response != null && response.length && response.length != 0) {
+							return response;
+						}
+						return [];
+					},
+					getListItem: function(item) {
+						return []
+					},
+					getTableConfig: function() {
+						return {
+							"serverPaginate": true,
+							"paginate": true,
+							"pageLength": 10
+						};
+					},
+					getColumns: function() {
+						return [{
+							title: 'GROUP_ID',
+							data: 'id'
+						}, {
+							title: 'PARTNER_CODE',
+							data: 'partnerCode'
+						}, {
+							title: 'GROUP_NAME',
+							data: 'groupName'
+						}]
+					},
+					getActions: function() {
+						return [{
+							name: "GRT",
+							desc: "",
+							icon: "fa fa-pencil-square-o",
+							fn: function(item, index) {
+								irfNavigator.go({
+									state: "Page.Engine",
+									pageName: "loans.group.GRT",
+									pageId:item.id
+								}, {
+									state: "Page.Engine",
+									pageName: "loans.group.GrtQueue",
+								});
+							},
+							isApplicable: function(item, index) {
+
+								return true;
+							}
+						}];
+					}
+				}
+			}
+		};
+	}
+})
