d218544056089f6bc48e78bcae0f30602a6af022
rajkumar.v <rajkumar.v@dvara.com>
2017-05-26 10:41:49 +0000

    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/trunk@3870 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- source/dev-www/process/pages/definitions/loans/individual/screening/LoanRequest.js
+++ source/dev-www/process/pages/definitions/loans/individual/screening/LoanRequest.js
@@ -1,8 +1,8 @@
 irf.pageCollection.factory(irf.page("loans.individual.screening.LoanRequest"),
-["$log", "$q","LoanAccount","LoanProcess", 'Scoring', 'Enrollment', 'AuthTokenHelper', 'SchemaResource', 'PageHelper','formHelper',"elementsUtils",
+["$log", "$q","LoanAccount","LoanProcess", 'Scoring', 'Enrollment','EnrollmentHelper', 'AuthTokenHelper', 'SchemaResource', 'PageHelper','formHelper',"elementsUtils",
 'irfProgressMessage','SessionStore',"$state", "$stateParams", "Queries", "Utils", "CustomerBankBranch", "IndividualLoan",
 "BundleManager", "PsychometricTestService", "LeadHelper", "Message", "$filter",
-function($log, $q, LoanAccount,LoanProcess, Scoring, Enrollment, AuthTokenHelper, SchemaResource, PageHelper,formHelper,elementsUtils,
+function($log, $q, LoanAccount,LoanProcess, Scoring, Enrollment,EnrollmentHelper, AuthTokenHelper, SchemaResource, PageHelper,formHelper,elementsUtils,
     irfProgressMessage,SessionStore,$state,$stateParams, Queries, Utils, CustomerBankBranch, IndividualLoan,
     BundleManager, PsychometricTestService, LeadHelper, Message, $filter){
 
@@ -285,6 +285,7 @@ function($log, $q, LoanAccount,LoanProcess, Scoring, Enrollment, AuthTokenHelper
         "subTitle": "BUSINESS",
         initialize: function (model, form, formCtrl, bundlePageObj, bundleModel) {
             model.currentStage = bundleModel.currentStage;
+            model.customer=model.customer || {};
             model.review = model.review|| {};
             if (_.hasIn(model, 'loanAccount')){
                 $log.info('Printing Loan Account');
@@ -350,6 +351,38 @@ function($log, $q, LoanAccount,LoanProcess, Scoring, Enrollment, AuthTokenHelper
                 });
             }
 
+            if (_.hasIn(model, 'loanAccount')){
+                Enrollment.getCustomerById({id:model.loanAccount.customerId})
+                    .$promise
+                    .then(function(res){
+                        model.customer = res;
+                        $log.info("Printing raj customer");
+                        $log.info(model.customer);
+                        if (model.currentStage =="Rejected") {
+                            model.customer.properAndMatchingSignboard= null;
+                            model.customer.bribeOffered=null;
+                            model.customer.shopOrganized=null;
+                            model.customer.isIndustrialArea=null;
+                            model.customer.customerAttitudeToKinara=null;
+                            model.customer.bookKeepingQuality=null;
+                            model.customer.challengingChequeBounce=null;
+                            model.customer.allMachinesAreOperational=null;
+                            model.customer.employeeSatisfaction=null;
+                            model.customer.politicalOrPoliceConnections=null;
+                            model.customer.multipleProducts=null;
+                            model.customer.multipleBuyers=null;
+                            model.customer.seasonalBusiness=null;
+                            model.customer.incomeStability=null;
+                            model.customer.utilisationOfBusinessPremises=null;
+                            model.customer.approachForTheBusinessPremises=null;
+                            model.customer.safetyMeasuresForEmployees=null;
+                            model.customer.childLabours=null;
+                            model.customer.isBusinessEffectingTheEnvironment=null;
+                            model.customer.stockMaterialManagement=null;
+                            model.customer.customerWalkinToBusiness=null;
+                        }
+                    });
+            }
 
             BundleManager.broadcastEvent('loan-account-loaded', {loanAccount: model.loanAccount});
         },
@@ -2406,6 +2439,14 @@ function($log, $q, LoanAccount,LoanProcess, Scoring, Enrollment, AuthTokenHelper
                     reqData.remarks = model.review.remarks;
                     PageHelper.showLoader();
                     PageHelper.showProgress("update-loan", "Working...");
+                    var cusData = {customer: _.cloneDeep(model.customer)};
+                    if (model.loanAccount.currentStage == "Rejected") {
+                        EnrollmentHelper.proceedData(cusData).then(function(resp) {
+                            formHelper.resetFormValidityState(form);
+                        }, function(httpRes) {
+                            PageHelper.showErrors(httpRes);
+                        });
+                    }
                     IndividualLoan.update(reqData)
                         .$promise
                         .then(function(res){
