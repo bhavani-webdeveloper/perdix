d0709bbc29d996d7f094ed9be79173603bd5485e
anchit.raj <anchit.raj@dvara.com>
2017-06-21 13:46:20 +0000

    Fix Required for tagging maintenance page
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/trunk@4264 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- source/dev-www/process/pages/definitions/management/TaggingMaintenance.js
+++ source/dev-www/process/pages/definitions/management/TaggingMaintenance.js
@@ -27,7 +27,7 @@ irf.pageCollection.factory(irf.page("management.TaggingMaintenance"), ["$log", "
                             },
                             {
                                 "type": "button",
-                                "title": "DOWNLOAD",
+                                "title": "Download Template",
                                 "icon": "fa fa-download",
                                 "condition": "model.master.uploadName",
                                 "notitle": true,
@@ -37,45 +37,41 @@ irf.pageCollection.factory(irf.page("management.TaggingMaintenance"), ["$log", "
                                     
                                    var fileId = "";
                                    if(model.master.uploadName == "PAR"){
-                                     fileId = irf.MANAGEMENT_BASE_URL + '/server-ext/uploads/'+ "PAR_ddmmyyyy.xlsx";
+                                     fileId = irf.MANAGEMENT_BASE_URL + '/server-ext/template/'+ "PAR_ddmmyyyy.xlsx";
                                      model.master.uploadNameValue = "PAR";
                                    }
                                    if(model.master.uploadName == "SECURITIZATION"){
-                                     fileId = irf.MANAGEMENT_BASE_URL + '/server-ext/uploads/'+ "scuritisation_tagging.xlsx";
+                                     fileId = irf.MANAGEMENT_BASE_URL + '/server-ext/template/'+ "scuritisation_tagging.xlsx";
                                      model.master.uploadNameValue = "SECURITIZATION";
                                    }
-
                                    Utils.downloadFile(fileId);
                                 },
+                            },
+                            {
+                                "type": "fieldset",
+                                "title": "UPLOAD_STATUS",
+                                "items": [{
+                                    "key": "master.achDemandListFileId",
+                                    "notitle": true,
+                                    "type": "file",
+                                    "category": "ACH",
+                                    "condition": "model.master.uploadName",
+                                    "subCategory": "cat2",
+                                    "fileType": "application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                                    customHandle: function(file, progress, modelValue, form, model) {
+                                        Maintenance.taggingMasterDataUpload(file, progress, {
+                                            fileType: model.master.uploadName
+                                        }).then(function(resp) {
+                                            $log.info(resp.data.stats);
+                                            model.master.value = true;
+                                            model.uploadres = resp.data.stats;
+                                        });
+                                    }
+                                }]
                             }
                         ]
                     }]
-                }, {
-                    "type": "box",
-                    "title": "UPLOAD_MASTER_DATA",
-                    "items": [{
-                        "type": "fieldset",
-                        "title": "UPLOAD_STATUS",
-                        "items": [{
-                            "key": "master.achDemandListFileId",
-                            "notitle": true,
-                            "type": "file",
-                            "category": "ACH",
-                            "condition": "model.master.uploadName",
-                            "subCategory": "cat2",
-                            "fileType": "application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-                            customHandle: function(file, progress, modelValue, form, model) {
-                                Maintenance.taggingMasterDataUpload(file, progress, {
-                                    fileType: model.master.uploadName
-                                }).then(function(resp) {
-                                    $log.info(resp.data.stats);
-                                    model.master.value = true;
-                                    model.uploadres = resp.data.stats;
-                                });
-                            }
-                        }]
-                    }]
-                }, {
+                },{
                     type: "box",
                     colClass: "col-sm-12",
                     condition: "model.master.value",
@@ -88,7 +84,6 @@ irf.pageCollection.factory(irf.page("management.TaggingMaintenance"), ["$log", "
                             htmlClass: "col-sm-12 col-xs-12 col-md-12",
                             html: '<div  ng-repeat="(key, value) in model.uploadres" class="col-sm-12 col-xs-12 col-md-12">' +
                                 "<label class = 'col-sm-3 col-xs-3 col-md-3' style = 'width: 30%;float: left;'>{{key}}</label><div style = 'font-weight: bold;width: 70%;'  class = 'col-sm-9 col-xs-9 col-md-9'>{{value}}</div></div>"
-
                         }]
                     }]
                 }
