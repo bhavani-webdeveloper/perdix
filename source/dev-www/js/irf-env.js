3d3fb5d387904c041eca1596b5f400c3e6d775b5
kesavan.madheswaran <kesavan.madheswaran@dvara.com>
2017-07-27 11:41:21 +0000

    reverted the previous checkin done by Raj mistakenly
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/trunk@4763 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- source/dev-www/js/irf-env.js
+++ source/dev-www/js/irf-env.js
@@ -7,10 +7,3 @@ irf.PSYCHOMETRIC_BASE_URL = "http://devkinara.perdix.in:8080/psychometric-8.2";
 irf.AUDIT_API_URL = 'http://devkinara.perdix.in:84';
 irf.ANALYTICS_WEB_URL = 'http://34.202.113.237:12345';
 irf.ANALYTICS_API_URL = 'http://34.202.113.237:1337';
-
-
-/*var irf = irf || {};
-irf.BASE_URL = 'http://34.195.76.73:8080/perdix-server';
-irf.MANAGEMENT_BASE_URL = 'http://34.195.76.73:8081/management/';
-irf.FORM_DOWNLOAD_URL = 'http://34.195.76.73:8080/forms/formsPrint.php';
-irf.BI_BASE_URL = "http://34.195.76.73:8081/bi";*/
