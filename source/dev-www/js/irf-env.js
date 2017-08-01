f22f0d33a130c981b12d852c4037d106a47bcb0c
rajkumar.v <rajkumar.v@dvara.com>
2017-08-01 10:40:02 +0000

    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/trunk@4814 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- source/dev-www/js/irf-env.js
+++ source/dev-www/js/irf-env.js
@@ -1,4 +1,4 @@
-/*var irf = irf || {};
+var irf = irf || {};
 irf.BASE_URL = 'http://devkinara.perdix.in:8080/perdix-server';
 irf.MANAGEMENT_BASE_URL = 'http://devkinara.perdix.in:8081/management';
 irf.FORM_DOWNLOAD_URL = 'http://115.113.193.49:8080/formsKinaraDev/formPrint.jsp';
@@ -6,10 +6,5 @@ irf.BI_BASE_URL = "http://devkinara.perdix.in:8081/bi-dev";
 irf.PSYCHOMETRIC_BASE_URL = "http://devkinara.perdix.in:8080/psychometric-8.2";
 irf.AUDIT_API_URL = 'http://devkinara.perdix.in:84';
 irf.ANALYTICS_WEB_URL = 'http://34.202.113.237:12345';
-irf.ANALYTICS_API_URL = 'http://34.202.113.237:1337';*/
+irf.ANALYTICS_API_URL = 'http://34.202.113.237:1337';
 
-var irf = irf || {};
-irf.BASE_URL = 'http://34.195.76.73:8080/perdix-server';
-irf.MANAGEMENT_BASE_URL = 'http://34.195.76.73:8081/management/';
-irf.FORM_DOWNLOAD_URL = 'http://34.195.76.73:8080/forms/formsPrint.php';
-irf.BI_BASE_URL = "http://34.195.76.73:8081/bi";
