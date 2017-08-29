8de91c7fddadb98a232f04499c637c4dc559b20f
kesavan.madheswaran <kesavan.madheswaran@dvara.com>
2017-08-29 09:44:22 +0000

    added for saija new stage
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/trunk@5100 15cf4ce8-1186-411e-815e-a2b7fc64482c

---
+++ source/dev-www/process/pages/definitions/loans/group/Grt2Queue.js
@@ -0,0 +1,130 @@
+define({
+	pageUID: "loans.group.Grt2Queue",
+	pageType: "Engine",
+	dependencies: ["$log", "$state", "GroupProcess","entityManager", "Enrollment", "CreditBureau", "Journal", "$stateParams", "SessionStore", "formHelper", "$q", "irfProgressMessage",
+		"PageHelper", "Utils", "PagesDefinition", "Queries", "irfNavigator"
+	],
+	$pageFn: function($log, $state, GroupProcess,entityManager, Enrollment, CreditBureau, Journal, $stateParams, SessionStore, formHelper, $q, irfProgressMessage,
+		PageHelper, Utils, PagesDefinition, Queries, irfNavigator) {
+
+		var branchId = SessionStore.getBranchId();
+		var branchName = SessionStore.getBranch();
+
+		return {
+			"type": "search-list",
+			"title": "GRT Queue",
+			"subTitle": "",
+			initialize: function(model, form, formCtrl) {
+				model.partner = SessionStore.session.partnerCode;
+				model.isPartnerChangeAllowed = GroupProcess.hasPartnerCodeAccess(model.partner);
+			},
+			definition: {
+				title: "GRT QUEUE",
+				searchForm: [{
+					"key": "partner",
+					"readonly": true,
+					"condition": "!model.isPartnerChangeAllowed"
+				}, {
+					"key": "partner",
+					"condition": "model.isPartnerChangeAllowed"
+				}],
+				autoSearch: true,
+				searchSchema: {
+					"type": 'object',
+					"title": 'SearchOptions',
+					"properties": {
+						"partner": {
+							"type": "string",
+							"title": "PARTNER",
+							"x-schema-form": {
+								"type": "select",
+								"enumCode": "partner"
+							}
+						}
+					},
+					"required": []
+				},
+
+				getSearchFormHelper: function() {
+					return formHelper;
+				},
+				getResultsPromise: function(searchOptions, pageOpts) {
+					return GroupProcess.search({
+						'branchId': branchId,
+						'partner': searchOptions.partner,
+						'groupStatus': true,
+						'currentStage': "GRT2",
+						'page': pageOpts.pageNo,
+						'per_page': pageOpts.itemsPerPage
+					}).$promise;
+				},
+				paginationOptions: {
+					"getItemsPerPage": function(response, headers) {
+						return 100;
+					},
+					"getTotalItemsCount": function(response, headers) {
+						return headers['x-total-count']
+					}
+				},
+				listOptions: {
+					selectable: false,
+					expandable: true,
+					listStyle: "table",
+					itemCallback: function(item, index) {},
+					getItems: function(response, headers) {
+						if (response != null && response.length && response.length != 0) {
+							return response;
+						}
+						return [];
+					},
+					getListItem: function(item) {
+						return []
+					},
+					getTableConfig: function() {
+						return {
+							"serverPaginate": true,
+							"paginate": true,
+							"pageLength": 10
+						};
+					},
+					getColumns: function() {
+						return [{
+							title: 'GROUP_ID',
+							data: 'id'
+						}, {
+							title: 'PARTNER_CODE',
+							data: 'partnerCode'
+						}, {
+							title: 'GROUP_NAME',
+							data: 'groupName'
+						},{
+							title: 'GROUP_CODE',
+							data: 'groupCode'
+						}]
+					},
+					getActions: function() {
+						return [{
+							name: "GRT",
+							desc: "",
+							icon: "fa fa-pencil-square-o",
+							fn: function(item, index) {
+								irfNavigator.go({
+									state: "Page.Engine",
+									pageName: "loans.group.GRT",
+									pageId:item.id
+								}, {
+									state: "Page.Engine",
+									pageName: "loans.group.GrtQueue",
+								});
+							},
+							isApplicable: function(item, index) {
+
+								return true;
+							}
+						}];
+					}
+				}
+			}
+		};
+	}
+})
