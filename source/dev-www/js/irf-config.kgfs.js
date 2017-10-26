9b172b1216b02e66ccdc352de47719605c403db1
stalin.solomon <stalin.solomon@dvara.com>
2017-10-26 17:23:05 +0000

    Kgfs colouring
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/trunk@5765 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- source/dev-www/js/irf-config.kgfs.js
+++ source/dev-www/js/irf-config.kgfs.js
@@ -1,4 +1,4 @@
 irf.appConfig.CONNECT_PERDIX7 = false;
-irf.appConfig.CORPORATE_LOGO = "img/corporate_logo.kgfs.gif";
-irf.appConfig.FORCE_COLOR = "";
+irf.appConfig.CORPORATE_LOGO = "img/corporate_logo.kgfs.png";
+irf.appConfig.FORCE_COLOR = "green";
 irf.appConfig.AMS_ENABLED = true;
