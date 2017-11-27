853c1f314e7046e16742b3239b5a8f447eaad4b3
shahal.tharique <shahal.tharique@dvara.com>
2017-11-27 08:04:49 +0000

    # Changed
    1. Config changes.
    
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/trunk@6205 15cf4ce8-1186-411e-815e-a2b7fc64482c

---
+++ source/dev-www/process/config/LeadProcessConfig.ts
@@ -0,0 +1,59 @@
+let config = {
+    'policies': {
+        'default' : {
+            onLoad: {
+                "defaults": [
+                    {
+                        "name": "Dumm1",
+                        "arguments": {}
+                    },
+                    {
+                        "name": "Dumm2",
+                        "arguments": {}
+                    },
+                    {
+                        "name": "PopulateLeadInteractionPolicy",
+                        "arguments": {
+
+                        }
+                    }
+
+                ],
+                "overrides": [
+                    {
+                        "type": "expr",
+                        "expr": "this.lead.currentStage=='InProcess' || this.lead.currentStage=='Incomplete'",
+                        "add": [
+                            {
+                                "name": "PopulateLeadInteractionPolicy",
+                                "arguments": {
+
+                                }
+                            }
+                        ],
+                        "remove": [
+
+                        ]
+                    }
+                ]
+            },
+            onNew: {
+                "defaults": [
+                    {
+                        "name": "PopulateLeadInteractionPolicy",
+                        "arguments": {
+
+                        }
+                    }
+                ]
+            }
+        },
+        'command': {
+            'OverlayLeadData': {
+
+            }
+        }
+    }
+}
+
+export = config;
