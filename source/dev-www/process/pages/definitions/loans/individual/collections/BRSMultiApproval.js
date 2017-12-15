cefd5de0b6b4d98bd165569dde625ce957d813bd
shahal.tharique <shahal.tharique@dvara.com>
2017-12-15 06:54:03 +0000

    # Changed
    1. BRS changes released.
    
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/trunk@6468 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- source/dev-www/process/pages/definitions/loans/individual/collections/BRSMultiApproval.js
+++ source/dev-www/process/pages/definitions/loans/individual/collections/BRSMultiApproval.js
@@ -1,24 +1,19 @@
 define({
     pageUID: "loans.individual.collections.BRSMultiApproval",
     pageType: "Engine",
-    dependencies: ["$log", "LoanCollection", "SessionStore", "PageHelper", "formHelper", "RolesPages", "Utils", "translateFilter", "$state"],
-    $pageFn: function($log, LoanCollection, SessionStore, PageHelper, formHelper, RolesPages, Utils, translateFilter, $state) {
+    dependencies: ["$log", "LoanCollection", "SessionStore", "PageHelper", "formHelper", "RolesPages", "Utils", "translateFilter", "$state", "Queries"],
+    $pageFn: function($log, LoanCollection, SessionStore, PageHelper, formHelper, RolesPages, Utils, translateFilter, $state, Queries) {
         var branch = SessionStore.getBranch();
 
-        var loadBRSRecords = function(model){
+        var loadBRSRecords = function(model) {
             PageHelper.showBlockingLoader("Loading...");
             model.loanCollectionSummaryDTOs = {};
             LoanCollection.findDepositSummaries({
                 'currentStage': "BRSValidation",
-            }).$promise.then(function(result) {
-                $log.info(result)
-                $log.info("result")
-                if (result && result.length) {
-                    model.loanCollectionSummaryDTOs = [];
-                    for (var i = 0; i < result.length; i++) {
-                        model.loanCollectionSummaryDTOs.push(result[i]);
-                    };
-                }
+                'bankAccountNumber': model.bankAccountNumber
+            }).$promise.then(function(results) {
+                $log.info(results)
+                model.loanCollectionSummaryDTOs = results;
             }).finally(function() {
                 PageHelper.hideBlockingLoader();
             });
@@ -27,71 +22,102 @@ define({
             "type": "schema-form",
             "title": "BRS_MULTI_APPROVAL",
             initialize: function(model, form, formCtrl) {
-                loadBRSRecords(model);
+                // loadBRSRecords(model);
             },
             form: [{
-                "type": "box",
-                colClass: "col-sm-12",
-                "title": "BRS_MULTI_APPROVAL",
-                "items": [{
-                    key: "loanCollectionSummaryDTOs",
-                    condition: "model.loanCollectionSummaryDTOs.length",
-                    type: "tableview",
-                    "notitle": true,
-                    "selectable": true,
-                    "editable": true,
-                    "tableConfig": {
-                        "searching": true,
-                        "paginate": true,
-                        "pageLength": 10
-                    },
-                    getColumns: function() {
-                        return [{
-                            "title": "Account Number",
-                            "data": "depositId_loanAccountNumber",
-                            render: function(data, type, full, meta) {
-                                return '<i class="' + full.icon_class + '">&nbsp;</i>' + translateFilter(data);
+                    "type": "box",
+                    colClass: "col-sm-8",
+                    "title": "SEARCH",
+                    "items": [
+                        {
+                            key: "bankAccountNumber",
+                            type: "lov",
+                            autolov: true,
+                            title: "ACCOUNT_NUMBER",
+                            required: true,
+                            bindMap: {
+
                             },
-                            editable: false
-                        }, {
-                            "title": "Amount",
-                            "data": "amount",
-                            editable: false
-                        }, {
-                            "title": "Instrument Type",
-                            "data": "instrumentType",
-                            editable: false
-                        }, {
-                            "title": "Bank Account Number",
-                            "data": "bankAccountNumber",
-                            editable: false
-                        }, {
-                            "title": "Deposited BY",
-                            "data": "depositedby",
-                            editable: false
-                        }, {
-                            "title": "Deposited On",
-                            "data": "depositedOn",
-                            editable: false
-                        }, {
-                            "title": "Reference",
-                            "data": "reference",
-                            editable: false
-                        }, {
-                            "title": "Loan Collection Id",
-                            "data": "loanCollectionId",
-                            editable: false
-                        }]
-                    }
-                }]
-            }, {
-                type: "actionbox",
-                condition: "model.loanCollectionSummaryDTOs.length",
-                items: [{
-                    type: "submit",
-                    title: "Update"
-                }]
-            }],
+                            outputMap: {
+                                "account_number": "bankAccountNumber"
+                            },
+                            searchHelper: formHelper,
+                            search: function(inputModel, form, model) {
+                                return Queries.getBankAccountsByPartnerForLoanRepay("Kinara");
+                            },
+                            getListDisplayItem: function(item, index) {
+                                return [
+                                    item.account_number,
+                                    item.ifsc_code + ', ' + item.bank_name,
+                                    item.branch_name
+                                ];
+                            },
+                            onSelect: function(valueObj, model, context) {
+                                loadBRSRecords(model);
+                                model.bankAccountGLCode = valueObj.account_code;
+                            }
+                        },
+                        {
+                            "key": "bankAccountGLCode",
+                            "type": "string",
+                            "title": "GL_CODE"
+                        }
+                    ]
+                },
+                {
+                    "type": "box",
+                    colClass: "col-sm-12",
+                    "title": "LIST",
+                    "items": [{
+                        key: "loanCollectionSummaryDTOs",
+                        condition: "model.loanCollectionSummaryDTOs.length",
+                        type: "tableview",
+                        "notitle": true,
+                        "selectable": true,
+                        "editable": true,
+                        "tableConfig": {
+                            "searching": true,
+                            "paginate": true,
+                            "pageLength": 10
+                        },
+                        getColumns: function() {
+                            return [
+                            {
+                                "title": "Date",
+                                "data": "depositedOn"
+                            },
+                            {
+                                "title": "Reference",
+                                "data": "reference",
+                                editable: false
+                            },
+                            {
+                                "title": "Amount",
+                                "data": "amount",
+                                editable: false
+                            },
+                            {
+                                "title": "Instrument Type",
+                                "data": "instrumentType",
+                                editable: false
+                            },
+                            {
+                                "title": "Deposited BY",
+                                "data": "depositedby",
+                                editable: false
+                            }
+                            ]
+                        }
+                    }]
+                }, {
+                    type: "actionbox",
+                    condition: "model.loanCollectionSummaryDTOs.length",
+                    items: [{
+                        type: "submit",
+                        title: "Update"
+                    }]
+                }
+            ],
             schema: {
                 "$schema": "http://json-schema.org/draft-04/schema#",
                 "type": "object",
@@ -114,7 +140,7 @@ define({
                         }
                     };
 
-                    var reqData = {"loanCollectionSummaryDTOs": temp};
+                    var reqData = { "loanCollectionSummaryDTOs": temp };
                     if (model.loanCollectionSummaryDTOs.length >= 1) {
                         Utils.confirm("Are You Sure?")
                             .then(function() {
