d4f6ab2e4f97d9e718b2c559ec95050619a7cb0b
prakash.d <prakash.d@dvara.com>
2017-12-23 14:13:43 +0000

    Validation error message is changed
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/trunk@6599 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- configuration/management/server-ext/messaging/closeConversation.php
+++ configuration/management/server-ext/messaging/closeConversation.php
@@ -1,6 +1,5 @@
 <?php
 date_default_timezone_set("Asia/Kolkata");
-// http://devkinara.perdix.in:8081/management/server-ext/closeConversation.php?process_id=12&sub_process_id=34
 include_once("../bootload.php");
 use Illuminate\Database\Capsule\Manager as DB;
 use EllipseSynergie\ApiResponse\Contracts\Response;
@@ -14,7 +13,6 @@ $queryString = $_SERVER['QUERY_STRING'];
 //$authHeader = $_SERVER['HTTP_AUTHORIZATION'];
 $query = [];
 parse_str($queryString, $query);
-//print_r($query);
 $response = get_response_obj();
 
 // Validation rules
@@ -29,13 +27,11 @@ $customeMessage = [
 
 $validator = get_validator()->make($query, $rule,$customeMessage);
 if ($validator->fails()) {
-	$response->setStatusCode(500);
+	$response->setStatusCode(400);
 	exit();
 }
 
 try {
-	// Create model
-	// $conversation = Conversation::where([['process_id', '=', $query['process_id']], ['sub_process_id', '=', $query['sub_process_id']]])->first();
 	$conversation = Conversation::where('process_id', '=', $query['process_id'])->whereNull("closed_at")->count();
 	if($conversation) {
 		DB::table('ms_conversation')->where('process_id', $query['process_id'])->whereNull("closed_at")->update(['closed_at' => date('Y-m-d H:i:s')]);
@@ -43,7 +39,9 @@ try {
 		exit();
 	}
 } catch(Exception $e) {
-	echo $e->getMessage();
+	// echo $e->getMessage();
+	$response->setStatusCode(500);
+	exit();
 }
-$response->setStatusCode(500);
+$response->setStatusCode(200);
 ?>
