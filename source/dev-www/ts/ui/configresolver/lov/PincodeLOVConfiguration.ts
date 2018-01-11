2dd97c76292101d28ff7e5b8e37dd16939f2bf3b
shahal.tharique <shahal.tharique@dvara.com>
2018-01-11 09:59:46 +0000

    # Changed
    1. Fix for form builder
    2. New method to build form created, which does all resolutions.
    
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/trunk@6959 15cf4ce8-1186-411e-815e-a2b7fc64482c

---
+++ source/dev-www/ts/ui/configresolver/lov/PincodeLOVConfiguration.ts
@@ -0,0 +1,51 @@
+import {LOVElementConfiguration} from "./LOVElementConfiguration";
+import {NGHelper} from "../../../infra/helpers/NGHelper";
+import AngularResourceService = require("../../../infra/api/AngularResourceService");
+export class PincodeLOVConfiguration extends LOVElementConfiguration {
+    inputMap: Object = {
+        "pincode": "customer.pincode",
+        "district": {
+            key: "customer.district"
+        },
+        "state": {
+            key: "customer.state"
+        }
+    };
+    outputMap: Object = {
+        "division": "customer.locality",
+        "region": "customer.villageName",
+        "pincode": "customer.pincode",
+        "district": "customer.district",
+        "state": "customer.state",
+    };
+    initialize: Function = function(inputModel) {
+        let $log = AngularResourceService.getInstance().getNGService("$log");
+        $log.warn('in pincode initialize');
+        $log.info(inputModel);
+    };
+    search: Function = function(inputModel, form, model) {
+        let $q = AngularResourceService.getInstance().getNGService("$q");
+        let Queries = AngularResourceService.getInstance().getNGService("Queries");
+        if (!inputModel.pincode) {
+            return $q.reject();
+        }
+        return Queries.searchPincodes(
+            inputModel.pincode,
+            inputModel.district,
+            inputModel.state
+        );
+    };
+
+    getListDisplayItem: Function = function(item, index) {
+        return [
+            item.division + ', ' + item.region,
+            item.pincode,
+            item.district + ', ' + item.state
+        ];
+    };
+    onSelect: Function = function(result, model, context) {
+        NGHelper.refreshUI();
+    };
+    autolov: boolean = true;
+    lovonly: boolean = false;
+}
