b20a2256132ab89ca9ee08af978277123b423585
kesavan.madheswaran <kesavan.madheswaran@dvara.com>
2018-03-02 09:36:53 +0000

    For Vizard login integration (analytics)
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/trunk@7567 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- source/dev-www/process/models/v1/bi/BIAnalytics.js
+++ source/dev-www/process/models/v1/bi/BIAnalytics.js
@@ -30,7 +30,70 @@ function($resource, $log, SessionStore, $httpParamSerializer, irfStorageService,
 				"$no_auth": true
 			},
 			//"withCredentials": true
+		},
+		applications: {
+			"method": "GET",
+			"url": endpoint + "/teams/:teamId/current-user/applications",
+			"headers": {
+				"Content-Type": "application/json",
+					"Authorization": function(config) {
+						var token = config.params.authToken;
+						delete config.params.authToken;
+						return "Bearer " + token;
+					},
+				"$no_auth": true
+			},
 		}
 	});
+	ret.doAnalyticsLogin = function(authToken) {
+		var deferred = $q.defer();
+		var authToken;
+		Account.getAnalyticsToken().$promise.then(function(data) {
+			authToken = data.analyticsToken.substring(12)
+			return ret.setCookie({
+				"authToken": authToken
+			}).$promise;
+		}).then(function(resp) {
+			$log.info(resp);
+			ret.currentUser({
+				"authToken": authToken
+			}).$promise.then(function(data) {
+				var team = '';
+				if(data && data.user && data.user.teams && data.user.teams.length > 0 ) {
+					for (var i in data.user.teams) {
+						if (data.user.teams[i].active) {
+							team = data.user.teams[i].name;
+							ret.applications({"authToken": authToken, "teamId": data.user.teams[i]._id}).$promise.then(
+								function(resp) {
+									if(resp.viewerApplications) {
+										for (var j in resp.viewerApplications) {
+											if (resp.viewerApplications[j].active) {
+												deferred.resolve({"team": team, "application": resp.viewerApplications[j].name })
+												return;
+											}
+										}
+										deferred.reject({data: {error: "Analytics Login Failed. There are no active applications assigned to the user."}});
+									} else {
+										deferred.reject({data: {error: "Analytics Login Failed. There are no applications assigned to the user."}});	
+									}
+								}, function(err) {
+									deferred.reject({data: {error: JSON.stringify(err)}});
+								}
+							);
+							return;
+						}
+					}
+					deferred.reject({data: {error: "Analytics Login Failed. There are no active teams assigned to the user."}});
+				} else {
+					deferred.reject({data: {error: "Analytics Login Failed. There are no teams assigned to the user."}});
+				}
+			}, function(err) {
+				deferred.reject({data: {error: JSON.stringify(err)}});
+			});
+		}, function(err) {
+			deferred.reject({data: {error: JSON.stringify(err)}});
+		});
+		return deferred.promise;	
+	}
 	return ret;
 }]);
--- source/dev-www/process/pages/definitions/bi/AnalyticsDashboard.js
+++ source/dev-www/process/pages/definitions/bi/AnalyticsDashboard.js
@@ -1,41 +1,24 @@
 irf.pageCollection.controller(irf.controller("bi.AnalyticsDashboard"),
-["$scope", "BIAnalytics", "irfNavigator", "$log", "$sce", "$window", "PageHelper", "irfProgressMessage", "Account",
-function($scope, BIAnalytics, irfNavigator, $log, $sce, $window, PageHelper, irfProgressMessage, Account) {
+["$scope", "BIAnalytics", "irfNavigator", "$log", "$sce", "$window", "PageHelper", "irfProgressMessage",
+function($scope, BIAnalytics, irfNavigator, $log, $sce, $window, PageHelper, irfProgressMessage) {
 	var authToken;
 	PageHelper.showLoader();
-	irfProgressMessage.pop('Analytics', 'Analytics Page is loading. Please wait...', 10000);
-	Account.getAnalyticsToken().$promise.then(function(data) {
-		authToken = data.analyticsToken.substring(12)
-		return BIAnalytics.setCookie({
-			"authToken": authToken
-		}).$promise;
-	}).then(function(resp) {
-		$log.info(resp);
-		BIAnalytics.currentUser({
-			"authToken": authToken
-		}).$promise.then(function(data) {
-			PageHelper.hideLoader();
-			var role = '';
-			if(data && data.user && data.user.teams && data.user.teams.length > 0) {
-				role = data.user.teams[0].role || role;
-			}
+	irfProgressMessage.pop('Analytics', 'Analytics Page is loading. Please wait...', 50000);
+	BIAnalytics.doAnalyticsLogin().then(function(data) {
 			irfProgressMessage.clear();
-			$scope.analyticsDashboardURL = $sce.trustAsResourceUrl(irf.ANALYTICS_WEB_URL + "/embedded-mode/" + role + "?show-navigator=true");///TeamA/ProjectA/home
+			$scope.analyticsDashboardURL = $sce.trustAsResourceUrl(irf.ANALYTICS_WEB_URL + "/embedded-mode/" + data.team + "/" + 
+			data.application + "/home?show-navigator=true");///TeamA/ProjectA/home
 			function setAnalyticsFrameHeight() {
 				$(".analytics-iframe, .analytics-root").css('height', ($(window).height()-139)+'px');
 			}
 			$($window).resize(setAnalyticsFrameHeight);
 			setAnalyticsFrameHeight();
+			PageHelper.hideLoader();
 		}, function(err) {
 			irfProgressMessage.clear();
 			PageHelper.hideLoader();
 			PageHelper.showProgress('Analytics', 'Analytics login failed.', 5000);
             PageHelper.showErrors(err);
 		});
-	}, function(err) {
-		irfProgressMessage.clear();
-		PageHelper.hideLoader();
-		PageHelper.showProgress('Analytics', 'Analytics login failed.', 5000);
-        PageHelper.showErrors(err);
-	});
+	
 }]);
