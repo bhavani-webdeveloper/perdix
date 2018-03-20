2976040141476a18b972e42f20c195e7092ff622
preethi.magesh <preethi.magesh@dvara.com>
2018-03-20 06:26:51 +0000

    #added method
    New method createNewProcess() which creates a new process
    
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/trunk@7859 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- source/dev-www/ts/domain/model/lender/LoanBooking/LiabilityLoanAccountBookingProcess.ts
+++ source/dev-www/ts/domain/model/lender/LoanBooking/LiabilityLoanAccountBookingProcess.ts
@@ -1,12 +1,19 @@
-import {RepositoryIdentifiers} from '../../shared/RepositoryIdentifiers';
-import RepositoryFactory = require('../../shared/RepositoryFactory');
-import {IEnrolmentRepository} from "./IEnrolmentRepository";
+///<amd-dependency path="perdixConfig/LiabilityLoanAccountBookingProcessConfig" name="liabilityLoanAccountBookingProcessConfig"/>
+
+import {RepositoryIdentifiers} from '../../../shared/RepositoryIdentifiers';
+import RepositoryFactory = require('../../../shared/RepositoryFactory');
+import {ILiabilityLoanAccountBookingRepository} from "./ILiabilityLoanAccountBookingRepository";
 import {Observable} from "@reactivex/rxjs";
 import {plainToClass} from "class-transformer";
-import {PolicyManager} from "../../shared/PolicyManager";
-import {LiabilityAccounts} from "./lender";
+import {PolicyManager} from "../../../shared/PolicyManager";
+import {LiabilityLoanAccountBookingPolicyFactory}  from "./policy/LiabilityLoanAccountBookingPolicyFactory";
+import {LiabilityAccounts} from "./LiabilityAccount";
+
+declare var liabilityLoanAccountBookingProcessConfig: Object;
 
 export class LiabilityLoanAccountBookingProcess {
+	LiabilityLoanAccountBookingProcessRepo:ILiabilityLoanAccountBookingRepository;
+	liabilityProcessAction: string;
     
     liabilityAccounts: LiabilityAccounts;
     constructor() {
@@ -18,4 +25,18 @@ export class LiabilityLoanAccountBookingProcess {
         let obs1 = this.LiabilityLoanAccountBookingProcessRepo.saveLiabilityLoanAccount(this)
         return Observable.concat(obs1).last();
     }
+
+    static createNewProcess(): Observable<LiabilityLoanAccountBookingProcess> {
+        let ep = new LiabilityLoanAccountBookingProcess();
+        ep.liabilityAccounts = new LiabilityAccounts();
+       //  ep.customer.customerType = customerType;
+        let pm: PolicyManager<LiabilityLoanAccountBookingProcess> = new PolicyManager<LiabilityLoanAccountBookingProcess>(ep, LiabilityLoanAccountBookingPolicyFactory.liabilityLoanFactory, 'onNew', LiabilityLoanAccountBookingProcess.getProcessConfig());
+        return pm.applyPolicies();
+    }
+
+     static getProcessConfig() {
+        return liabilityLoanAccountBookingProcessConfig;
+    }
+
+
 }
