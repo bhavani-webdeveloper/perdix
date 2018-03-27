9fa85dfed6abbced237151793ccbc39a768ed117
stalin.solomon <stalin.solomon@dvara.com>
2018-03-27 15:14:03 +0000

    AMS - enable feature
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/trunk@7980 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- source/dev-www/js/irf-config.kinara.js
+++ source/dev-www/js/irf-config.kinara.js
@@ -1,5 +1,5 @@
 irf.appConfig.CONNECT_PERDIX7 = false;
 irf.appConfig.CORPORATE_LOGO = "img/corporate_logo.png";
 irf.appConfig.FORCE_COLOR = "gray";
-irf.appConfig.AMS_ENABLED = false;
+irf.appConfig.AMS_ENABLED = true;
 irf.appConfig.OFFLINE_USERS = "all"; // ["all"|"mobile"|false]
