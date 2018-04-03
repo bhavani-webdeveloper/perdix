f3df6bfd684de061aef13f6798487f1ccbaadc87
preethi.magesh <preethi.magesh@dvara.com>
2018-04-03 07:28:14 +0000

    #ADDED
    VIEW LOAN pages/definitions/lender/enrolment/View.js
    
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/trunk@8029 15cf4ce8-1186-411e-815e-a2b7fc64482c

---
+++ source/dev-www/process/pages/definitions/lender/enrolment/View.js
@@ -0,0 +1,108 @@
+irf.pageCollection.factory(irf.page('lender.enrolment.View'),
+    ["$log", "formHelper", "LoanAccount", "$state", "SessionStore", "LoanAccount", "$stateParams","LiabilityAccountProcess",
+        function($log, formHelper, LoanAccount, $state, SessionStore, LoanAccount, $stateParams,LiabilityAccountProcess){
+            return {
+                "type": "search-list",
+                "title": "VIEW_LOANS",
+                "subTitle": "VIEW_LOANS_SUB",
+                initialize: function (model, form, formCtrl) {
+                    $log.info("ViewLoans initialiized");
+                },
+                offline: false,
+                definition: {
+                    title: "Loans",
+                    autoSearch:true,
+                    searchForm: [
+                        //"*"
+                    ],
+                    searchSchema: {
+                        "type": 'object',
+                        "title": 'SearchOptions',
+                        "properties": {
+                            "show_closed": {
+                                "title": "SHOW_CLOSED_LOANS",
+                                "type": "boolean",
+                                "default": false
+                            }
+                        },
+                        "required":["branch"]
+                    },
+                    getSearchFormHelper: function() {
+                        return formHelper;
+                    },
+                    getResultsPromise: function(searchOptions, pageOpts){      /* Should return the Promise */
+                        var promise = LiabilityAccountProcess.getLiabilityAccountSearch({lenderId: $stateParams.pageId}).$promise;
+                        return promise;
+                    },
+                    paginationOptions: {
+                        //"viewMode": "page",
+                        "getItemsPerPage": function(response, headers){
+                            return 20;
+                        },
+                        "getTotalItemsCount": function(response, headers){
+                            return response.length;
+                        }
+                    },
+                    listOptions: {
+                        itemCallback: function(item, index) {
+
+                        },
+                        getItems: function(response, headers){
+                            if (response!=null && response.length && response.length!=0){
+                                //var arrLength = response.length;
+                                //for (var i=0; i<arrLength; i++){
+                                //    if (!_.isNull(response[i]) && _.isObject(response[i])){
+                                //        _.remove()
+                                //    }
+                                //}
+                                _.pullAll(response, [null]);
+                                return response;
+                            }
+                            return [];
+                        },
+                        getListItem: function(item){
+                            return [
+                                item.lenderAccountNumber,
+                                'Lender Name: ' + item.lenderName
+                            ]
+                        },
+                        getActions: function(){
+                            return [
+                                {
+                                    name: "View Details",
+                                    desc: "",
+                                    fn: function(item, index){
+                                        $state.go('Page.Engine', {
+                                            pageName: 'lender.liabilities.LiabilityLoanAccountBooking',
+                                            pageId: item.id
+                                        })
+                                    },
+                                    isApplicable: function(item, index){
+                                        return true;
+                                    }
+                                },
+                                {
+                                    name: "Repay",
+                                    desc: "",
+                                    fn: function(item, index){
+                                        $state.go('Page.Engine', {
+                                            pageName: 'loans.LoanRepay',
+                                            pageId: [item.accountNumber,item.urnNo].join(".")
+                                        })
+                                    },
+                                    isApplicable: function(item, index){
+                                        var siteCode = SessionStore.getGlobalSetting('siteCode');
+                                        if(siteCode == 'KGFS' || siteCode == 'sambandh' || siteCode == 'saija')
+                                        {
+                                            return false;
+                                        } else {
+                                            return true;
+                                        }
+                                    }
+                                },
+                            ];
+                        }
+                    }
+                }
+            };
+        }]);
