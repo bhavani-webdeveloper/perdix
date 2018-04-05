a07919f79363b0f673323095c4b60feec6afd508
preethi.magesh <preethi.magesh@dvara.com>
2018-04-05 09:12:57 +0000

    #added Schedule.js
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/trunk@8079 15cf4ce8-1186-411e-815e-a2b7fc64482c

---
+++ source/dev-www/process/models/v1/lender/Schedule.js
@@ -0,0 +1,43 @@
+irf.models.factory('Schedule', 
+["$resource", "$httpParamSerializer", "BASE_URL", "searchResource", "Upload", "$q", "PageHelper",
+function($resource, $httpParamSerializer, BASE_URL, searchResource, Upload, $q, PageHelper) {
+        var endpoint = BASE_URL + '/api/liablitySchedule';
+        var lenderAccountNumb = '/find?lenderAccountNumber='
+        /*
+         * $get : /api/enrollments/{blank/withhistory/...}/{id}
+         *  eg: /enrollments/definitions -> $get({service:'definition'})
+         *      /enrollments/1           -> $get({id:1})
+         * $post will send data as form data, save will send it as request payload
+         */
+        var resource = $resource(endpoint, null, {
+            scheduleUploads: {
+                method: 'POST',
+                //url: 'endpoint' + '/uploadLiablityScheduleDetails'
+            },
+            getLiabilitySchedule :{
+                method:'GET',
+                url: endpoint + lenderAccountNumb + ':id',
+                isArray:true
+            }
+        });
+        resource.scheduleUpload = function(file, progress) {
+            var deferred = $q.defer();
+            Upload.upload({
+                url: endpoint + '/uploadLiablityScheduleDetails',
+                data: {
+                    file: file
+                }
+            }).then(function(resp){
+                // TODO handle success
+                PageHelper.showProgress("page-init", "Done.", 2000);
+                deferred.resolve(resp);
+            }, function(errResp){
+                // TODO handle error
+                PageHelper.showErrors(errResp);
+                deferred.reject(errResp);
+            }, progress);
+            return deferred.promise;
+        };
+    return resource;
+    }
+]);
