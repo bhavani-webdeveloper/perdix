0fa81d1c11e031551e686b142164a03f6c793ed3
preethi.magesh <preethi.magesh@dvara.com>
2018-04-06 08:43:19 +0000

    #added ILiabilityRepaymentRepository.ts
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/trunk@8105 15cf4ce8-1186-411e-815e-a2b7fc64482c

---
+++ source/dev-www/ts/domain/model/lender/LoanBooking/ILiabilityRepaymentRepository.ts
@@ -0,0 +1,10 @@
+
+import {Observable} from "@reactivex/rxjs";
+
+export interface ILiabilityRepaymentRepository {
+
+	repay(reqData: Object): Observable<any>;
+	// proceedLiabilityLoanAccount(reqData: Object): Observable<any>;
+	// getLenderLoan(id: number): Observable<any>;
+    
+}
