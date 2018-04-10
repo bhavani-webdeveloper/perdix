16b27b73c70f094167741dff89363be66ce2ac7c
preethi.magesh <preethi.magesh@dvara.com>
2018-04-10 05:05:04 +0000

    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/trunk@8145 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- source/dev-www/process/pages/definitions/management/product/ProductBranch.js
+++ source/dev-www/process/pages/definitions/management/product/ProductBranch.js
@@ -16,37 +16,27 @@ define({
                         PageHelper.hideLoader();
                         model.branchProductMapping = response.branchProductMappings;
                         model.branchProductMappings = [];
-                            for (i = 0; i < model.productTypeMasterData.length; i++) {
-                                for (j = 0; j <= model.branchProductMapping.length; j++) {
-                                    if (model.branchProductMapping.length != 0 && j < model.branchProductMapping.length ) {
-                                        if (model.branchProductMapping[j].productCode == model.productTypeMasterData[i].productName) {
-                                            model.branchProductMappings.push({
-                                                "productCode": model.productTypeMasterData[i].productName,
-                                                "agentAccess": model.branchProductMapping[j].agentAccess,
-                                                "wmAccess": model.branchProductMapping[j].wmAccess,
-                                                "checked": true
-                                            })
-                                            break;
-                                        } else {
-                                            if (j == model.branchProductMapping.length) {
-                                                model.branchProductMappings.push({
-                                                    "productCode": model.productTypeMasterData[i].productName,
-                                                    "agentAccess": false,
-                                                    "wmAccess": false,
-                                                    "checked": false
-                                                })
-                                            }
-                                        }
-                                    } else {
+                        for (i = 0; i < model.productTypeMasterData.length; i++) {
+                            for (j = 0; j <= model.branchProductMapping.length; j++) {
+                                if (model.branchProductMapping.length != 0 && j < model.branchProductMapping.length) {
+                                    if (model.branchProductMapping[j].productCode == model.productTypeMasterData[i].productName) {
                                         model.branchProductMappings.push({
                                             "productCode": model.productTypeMasterData[i].productName,
-                                            "agentAccess": false,
-                                            "wmAccess": false,
-                                            "checked": false
+                                            "agentAccess": model.branchProductMapping[j].agentAccess,
+                                            "wmAccess": model.branchProductMapping[j].wmAccess,
+                                            "checked": true
                                         })
+                                        break;
+                                    } else {
+                                        if (j == model.branchProductMapping.length) {
+                                            branchMapping(model, i);
+                                        }
                                     }
+                                } else {
+                                    branchMapping(model, i);
                                 }
                             }
+                        }
                     },
                     function(err) {
                         console.log("error")
@@ -56,6 +46,15 @@ define({
                     message: ' Branch name is required'
                 })
             }
+        }
+
+        var branchMapping = function(model,i){
+            model.branchProductMappings.push({
+                "productCode": model.productTypeMasterData[i].productName,
+                "agentAccess": false,
+                "wmAccess": false,
+                "checked": false
+            })
         }
         return {
             "type": "schema-form",
@@ -86,7 +85,12 @@ define({
                     PageHelper.hideLoader();
                 }, function(err) {
                     irfProgressMessage.pop("productTypeMasterData-save", "An Error Occurred. Failed to fetch Data", 5000);
-                })
+                });
+                model.selectAl = function(value){
+                    if(value == false){
+                        model.selectAll = false
+                    }
+                }
             },
             form: [{
                 "type": "box",
@@ -96,11 +100,10 @@ define({
                     "type": "select",
                     "enumCode": "branch_id",
                     "required": true,
-                }, {
-                    "type": "button",
-                    "title": "SUBMIT",
-                    "style": "btn-theme",
-                    "onClick": "actions.submit(model, formCtrl, form)"
+                    "onChange": function(modelValue, form, model) {
+                        branchId = model.branch;
+                        getProductMappingData(model, branchId);
+                    }
                 }]
             }, {
                 "type": "box",
@@ -110,7 +113,7 @@ define({
                     type: "section",
                     htmlClass: "col-sm-12",
                     html: '<div class="table-responsive">' +
-                        '<div>'+'<input type="checkbox" ng-model="data1.selectAll"  ng-change="model.myClick(data1.selectAll,model.branchProductMappings)">'+'<span>SelectAll</span>'+'</div>'+
+                        '<div>'+'<input type="checkbox" ng-model="model.selectAll"  ng-change="model.myClick(model.selectAll,model.branchProductMappings)">'+'<span>SelectAll</span>'+'</div>'+
                         '<table class="table table-bordered">' + '<thead class="thead-default">' +
                         '<tr>' +
                         '<th style="text-align: center">Select</th>' +
@@ -121,7 +124,7 @@ define({
                         '</thead>' +
                         '<tbody>' +
                         '<tr ng-repeat="data1 in model.branchProductMappings">' +
-                        '<td class="col-sm-3" style="text-align: center"><input type="checkbox"  ng-checked="data1.checked" ng-model="data1.checked" ng-change="data1.checked" /> </td>' +
+                        '<td class="col-sm-3" style="text-align: center"><input type="checkbox"  ng-checked="data1.checked" ng-model="data1.checked" ng-change="model.selectAl(data1.checked)" /> </td>' +
                         '<td class="col-sm-3" style="text-align: center">{{data1.productCode}}</td>' +
                         '<td class="col-sm-3" style="text-align: center"><input type="checkbox" ng-checked="data1.wmAccess" ng-model="data1.wmAccess" ng-change="data1.wmAccess" class="center" /> </td>' +
                         '<td class="col-sm-3" style="text-align: center"><input type="checkbox" ng-checked="data1.agentAccess" ng-model="data1.agentAccess" ng-change="data1.agentAccess" class="center" /></td>' + '</tr>' +
