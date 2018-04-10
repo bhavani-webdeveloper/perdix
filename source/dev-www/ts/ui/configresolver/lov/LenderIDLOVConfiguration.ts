37e7daf7cacc91f55abdf3061e7d90c542b00801
prakash.d <prakash.d@dvara.com>
2018-04-10 14:37:48 +0000

    # Added 1. LOV is added
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/trunk@8176 15cf4ce8-1186-411e-815e-a2b7fc64482c

---
+++ source/dev-www/ts/ui/configresolver/lov/LenderIDLOVConfiguration.ts
@@ -0,0 +1,40 @@
+import {LOVElementConfiguration} from "./LOVElementConfiguration";
+import {NGHelper} from "../../../infra/helpers/NGHelper";
+import AngularResourceService = require("../../../infra/api/AngularResourceService");
+import {EnrolmentProcess} from '../../../domain/model/customer/EnrolmentProcess';
+import * as _ from 'lodash';
+export class LenderIDLOVConfiguration extends LOVElementConfiguration {
+	inputMap: Object = {
+		 "firstName": {
+            "key": "customer.firstName",
+            "title": "LENDER_NAME"
+        }
+	};
+	outputMap: Object = {
+		"lenderId": "customer.id"
+	};
+	onSelect: Function = function(result, model, context) {
+		model.liabilityAccount.lenderId = result.id;
+		NGHelper.refreshUI();
+	};
+	getListDisplayItem: Function = function(item, index) {
+	 	return [
+            [item.firstName, item.fatherFirstName].join(' '),
+            item.id
+        ];
+	};
+	search: Function = function(inputModel, form, model) {
+		let SessionStore = AngularResourceService.getInstance().getNGService("SessionStore");
+        let Enrollment = AngularResourceService.getInstance().getNGService("Enrollment");
+        if (!inputModel.branchName)
+            inputModel.branchName = SessionStore.getBranch();
+       	let promise = Enrollment.lenderSearch({
+            'branchName': inputModel.branchName,
+            'firstName': inputModel.firstName,
+            'customerType': 'lender'
+        }).$promise;
+        return promise;
+	};
+	autolov: boolean = true;
+	lovonly: boolean = false;
+}
