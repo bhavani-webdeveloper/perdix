d4e8d36c72d7ba5cbbba9cf4ccbe08afa5cc4752
kesavan.madheswaran <kesavan.madheswaran@dvara.com>
2018-04-12 10:56:37 +0000

    Report parameters filter enhancement
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/trunk@8214 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- source/dev-www/process/pages/definitions/bi/BIReports.js
+++ source/dev-www/process/pages/definitions/bi/BIReports.js
@@ -1,6 +1,44 @@
-irf.pageCollection.factory(irf.page("bi.BIReports"), ["$log", "RolesPages", "BIReports", "SessionStore", "PageHelper", "$httpParamSerializer", "AuthTokenHelper", "$filter", "$q",
-    function($log, RolesPages, BIReports, SessionStore, PageHelper, $httpParamSerializer, AuthTokenHelper, $filter, $q) {
+irf.pageCollection.factory(irf.page("bi.BIReports"), ["$log", "RolesPages", "BIReports", "SessionStore", "PageHelper", "$httpParamSerializer", "AuthTokenHelper", "$filter", "$q", "$http", "irfProgressMessage",
+    function($log, RolesPages, BIReports, SessionStore, PageHelper, $httpParamSerializer, AuthTokenHelper, $filter, $q, $http, irfProgressMessage) {
+        var allReportParametersCache = [];
+        var getReportsConfiguration = function (reportName) {
+            var defered = $q.defer();
+            var result = [];
+            if(allReportParametersCache.length != 0)
+            {   
+                if(reportName) {
+                    result = $filter('filter')(allReportParametersCache, {report_name: reportName}, true);
+                } else {
+                    result = allReportParametersCache
+                }
 
+                defered.resolve(result);
+            } else {
+                BIReports.allReportParameters().$promise.then(function(result) {
+                    var item;
+                    for(var i=0; i < result.length ; i++) {
+                        if(result[i].parameter == 'from_date' || result[i].parameter == 'to_date'){
+                            result[i].required = true;
+                            // result[i].operators.push("IN", "between")
+                        }
+                        item = result[i];
+                        allReportParametersCache.push(result[i]);  
+                    }
+                    if(reportName) {
+                        result = $filter('filter')(allReportParametersCache, {report_name: reportName}, true);
+                    } else {
+                        result = allReportParametersCache
+                    }
+
+                    defered.resolve(result);
+                }, function(err){
+                    PageHelper.showErrors(err);
+                });
+            }
+
+            return defered.promise;
+        };
+        getReportsConfiguration();
         return {
             "type": "schema-form",
             "title": "REPORTS",
@@ -47,45 +85,59 @@ irf.pageCollection.factory(irf.page("bi.BIReports"), ["$log", "RolesPages", "BIR
                 var self = this;
                 var defered = $q.defer();
                 PageHelper.showLoader();
-                var p1 = BIReports.allReportParameters().$promise.then(function(result){
-                    var item;
-                    self.formSource[0].items.splice(3, self.formSource[0].items.length -3);
-                    for(var i=0; i < result.length ; i++) {
-                        if(result[i].parameter == 'from_date' || result[i].parameter == 'to_date'){
-                            continue;
-                        }
-                        item = {};
-                        BIReports.Utils.setOptionsForReportParameter(item, result[i]);
-                        console.log (item['key']);
-                        item['condition'] = "model.selectedReport && model.selectedReport.parameters.indexOf('" + result[i].parameter + "') != -1";
-                        self.formSource[0].items.push(item);    
-                    }
-                    console.log(JSON.stringify(self.formSource[0].items));
-                    defered.resolve();
-                }, function(err){
-                    PageHelper.showErrors(err);
-                });
+                
                 return defered;
             },
             form: [],
             formSource: [{
                 "type": "box",
                 "title": "CHOOSE_A_REPORT",
+                colClass: "col-sm-9",
                 "items": [{
                     "key": "bi.report_name",
                     "type": "select",
                     onChange: function(modelValue, form, model, formCtrl, event){
                         var res = $filter('filter')( model.report.masterData, {'value': model.bi.report_name}, true);
                         model.selectedReport = (res && res.length > 0) ? res[0] : undefined; 
+                        model.bi._filterCollection = [];
+                        if (model.selectedReport.parameters) {
+                            for (var i = 0; i < model.selectedReport.parameters.length; i++) {
+                                model.bi._filterCollection[i] = model.bi._filterCollection[i] || {};
+                                if (model.bi._filterCollection[i] ) {
+                                    model.bi._filterCollection[i]['filterParameter'] = model.selectedReport.parameters[i];
+                                } else {
+                                    model.bi._filterCollection[i] = {filterParameter : model.selectedReport.parameters[i]};
+                                } 
+                            }
+                        }
                     }
-                }, {
-                    "key": "bi.from_date",
-                    condition: "model.done('true')",
-                    "type": "date"
-                }, {
-                    "key": "bi.to_date",
-                    condition: "model.done('true')",
-                    "type": "date"
+                }, 
+                // {
+                //     "key": "bi.from_date",
+                //     condition: "model.done('true')",
+                //     "type": "date"
+                // }, {
+                //     "key": "bi.to_date",
+                //     condition: "model.done('true')",
+                //     "type": "date"
+                // }, 
+                {
+                    "key": "bi._filterCollection",
+                    "type": "array",
+                    "titleExpr": "",
+                    condition: "model.selectedReport && model.selectedReport.parameters.length",
+                    "view": "fixed",
+                    "add": null,
+                    "remove": null,
+                    "startEmpty": false,
+                    "items": [{
+                        "key": "bi._filterCollection[]",
+                        "type": "structeredFilter",
+                        "filterParamPreset": true,
+                        "getConfiguration": function(modelValue, form, model, formCtrl, event) {
+                           return getReportsConfiguration(model.bi.report_name);
+                        }
+                    }]
                 }]
             }, {
                 type: "actionbox",
@@ -139,9 +191,47 @@ irf.pageCollection.factory(irf.page("bi.BIReports"), ["$log", "RolesPages", "BIR
             },
             actions: {
                 submit: function(model, form, formName) {
-                    var biDownloadUrl = irf.BI_BASE_URL + '/download.php?auth_token=' + AuthTokenHelper.getAuthData().access_token + '&' + $httpParamSerializer(model.bi);
-                    $log.info(biDownloadUrl);
-                    window.open(biDownloadUrl);
+                    if (model.selectedReport.parameterized) {
+                        var reqData = {}
+                        reqData.auth_data = {
+                            'auth_token' : AuthTokenHelper.getAuthData().access_token,
+                        }
+                        reqData.report_name = model.bi.report_name;
+                        //reqData.query_mode = 1;
+
+                        reqData.filters = [];
+                        for (var i = 0; i < model.bi._filterCollection.length; i++) {
+                            if (model.bi._filterCollection[i].filterQuery)
+                            reqData.filters.push(model.bi._filterCollection[i].filterQuery)
+                        }
+                        // BIReports.downLoadReport(reqData).$promise.then(function(data){
+                        //     console.log(data);
+                        // });
+                        PageHelper.showLoader();
+                        irfProgressMessage.pop("Reports","Downloading Report. Please wait...");
+                        $http.post(
+                          irf.BI_BASE_URL + '/newdownload.php',
+                          reqData,
+                          {responseType: 'arraybuffer'}
+                        ).then(function (response) {
+                          var headers = response.headers();
+                          var blob = new Blob([response.data],{type:headers['content-type']});
+                          var link = document.createElement('a');
+                          link.href = window.URL.createObjectURL(blob);
+                          link.download = SessionStore.getLoginname() + '_' + model.selectedReport.name + '_' + moment().format('YYYYMMDDhhmmss');
+                          link.click();
+                          irfProgressMessage.pop("Reports","Report downloaded.", 5000);
+                        }, function(err) {
+                            PageHelper.showErrors(err);
+                        }).finally(function(){
+                            PageHelper.hideLoader();
+                        });
+
+                    } else {
+                        var biDownloadUrl = irf.BI_BASE_URL + '/download.php?auth_token=' + AuthTokenHelper.getAuthData().access_token + '&' + $httpParamSerializer(model.bi);
+                        $log.info(biDownloadUrl);
+                        window.open(biDownloadUrl);
+                    }
                 }
             }
         };
