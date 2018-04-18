3460a53caa2d251cc5589047ee85aaf7c2b2b0e9
arpit.agarwal <arpit.agarwal@dvara.com>
2018-04-18 12:00:42 +0000

    #Changed: Witfin CR- policy/VerifyBankAccountNumberPolicy.ts
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/trunk@8318 15cf4ce8-1186-411e-815e-a2b7fc64482c

---
+++ source/dev-www/ts/domain/model/customer/policy/VerifyBankAccountNumberPolicy.ts
@@ -0,0 +1,44 @@
+import {IPolicy} from "../../../shared/IPolicy";
+import {Observable} from "@reactivex/rxjs";
+import {IEnrolmentRepository} from "../../customer/IEnrolmentRepository";
+import RepositoryFactory = require("../../../shared/RepositoryFactory");
+import {RepositoryIdentifiers} from "../../../shared/RepositoryIdentifiers";
+import {UserSession, ISession} from "../../../shared/Session";
+import {FormHelper, IFormHelper} from "../../../shared/FormHelper";
+import {ObjectFactory} from "../../../shared/ObjectFactory";
+import Customer = require("../Customer");
+import {ValidationError} from "../../../../ui/errors/ValidationError";
+import FamilyMember = require("../FamilyMember");
+import Expenditure = require("../Expenditure");
+import {EnrolmentProcess} from "../EnrolmentProcess";
+import AngularResourceService = require("../../../../infra/api/AngularResourceService");
+import * as _ from 'lodash';
+
+
+declare var moment: Function;
+
+export class VerifyBankAccountNumberPolicy extends IPolicy<EnrolmentProcess> {
+
+    enrolmentRepo: IEnrolmentRepository;
+
+    constructor(){
+        super();
+        this.enrolmentRepo = RepositoryFactory.createRepositoryObject(RepositoryIdentifiers.Enrolment);
+    }
+
+    setArguments(args) {
+    }
+
+    run(enrolmentProcess: EnrolmentProcess): Observable<EnrolmentProcess> {
+
+        if(_.hasIn(enrolmentProcess.customer, "customerBankAccounts") && _.isArray(enrolmentProcess.customer.customerBankAccounts) && enrolmentProcess.customer.customerBankAccounts.length > 0) {
+            for (let i=0;i<enrolmentProcess.customer.customerBankAccounts.length;i++) {
+                 var banckAccount = enrolmentProcess.customer.customerBankAccounts[i];
+                 if (banckAccount.accountNumber != banckAccount.confirmedAccountNumber) {
+                    return Observable.throw(new ValidationError("Account Number and Confirmed Account Number must be same"));
+                }
+            }
+        }
+        return Observable.of(enrolmentProcess);
+    }
+}
