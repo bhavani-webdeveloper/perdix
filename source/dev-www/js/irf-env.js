c234a026e388307521c048050560d16107d0e0ee
stalin.solomon <stalin.solomon@dvara.com>
2018-04-18 13:46:57 +0000

    AMS config
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/branches/v8.x-kgfs-uat@8334 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- source/dev-www/js/irf-env.js
+++ source/dev-www/js/irf-env.js
@@ -1,6 +1,6 @@
 var irf = irf || {};
-irf.BASE_URL = 'http://34.195.76.73:8080/perdix-server';
-irf.MANAGEMENT_BASE_URL = 'http://34.195.76.73:8081/management/';
-irf.FORM_DOWNLOAD_URL = 'http://34.195.76.73:8080/forms/formPrint.jsp';
-irf.BI_BASE_URL = "http://34.195.76.73:8081/bi";
-irf.AUDIT_API_URL = 'http://34.195.76.73:70';
+irf.BASE_URL = 'http://uatperdix.kgfs.co.in:8080/perdix-server';
+irf.MANAGEMENT_BASE_URL = 'http://uatperdix.kgfs.co.in:8081/management/';
+irf.FORM_DOWNLOAD_URL = 'http://uatperdix.kgfs.co.in:8080/forms/formPrint.jsp';
+irf.BI_BASE_URL = "http://uatperdix.kgfs.co.in:8081/bi";
+irf.AUDIT_API_URL = 'http://uatperdix.kgfs.co.in:86';
