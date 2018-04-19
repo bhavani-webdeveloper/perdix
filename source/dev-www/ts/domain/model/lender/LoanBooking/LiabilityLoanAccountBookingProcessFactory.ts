d29dda24d56aa74cf78a3f29b47b13e6069f6977
preethi.magesh <preethi.magesh@dvara.com>
2018-04-19 14:29:02 +0000

    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/trunk@8380 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- source/dev-www/ts/domain/model/lender/LoanBooking/LiabilityLoanAccountBookingProcessFactory.ts
+++ source/dev-www/ts/domain/model/lender/LoanBooking/LiabilityLoanAccountBookingProcessFactory.ts
@@ -1,5 +1,7 @@
 
-import { LiabilityAccount } from "./LiabilityAccount";
+import {plainToClass} from "class-transformer";
+import {Utils} from "../../../shared/Utils";
+import  LiabilityAccount = require("./LiabilityAccount") ;
 import {PolicyManager} from "../../../shared/PolicyManager";
 import {LiabilityLoanAccountBookingPolicyFactory} from "./policy/LiabilityLoanAccountBookingPolicyFactory";
 import {Observable} from "@reactivex/rxjs";
@@ -7,21 +9,27 @@ import {ILiabilityLoanAccountBookingRepository} from "./ILiabilityLoanAccountBoo
 import RepositoryFactory = require("../../../shared/RepositoryFactory");
 import {RepositoryIdentifiers} from "../../../shared/RepositoryIdentifiers";
 import {LiabilityLoanAccountBookingProcess} from "./LiabilityLoanAccountBookingProcess";
-import {plainToClass} from "class-transformer";
-import {Utils} from "../../../shared/Utils";
+
+
 export class LiabilityLoanAccountBookingProcessFactory {
 
     static liabilityRepo:ILiabilityLoanAccountBookingRepository = RepositoryFactory.createRepositoryObject(RepositoryIdentifiers.LiabilityLoanAccountBookingProcess);
-
+    
     static createFromLoanId(id: number): Observable<LiabilityLoanAccountBookingProcess> {
+        var liabilityAccount:LiabilityAccount ;
         let lp: LiabilityLoanAccountBookingProcess = new LiabilityLoanAccountBookingProcess();
         return LiabilityLoanAccountBookingProcessFactory.liabilityRepo.getLenderLoan(id)
             .map(
-                (liabilityAccount:Object) => {
-                    console.log(liabilityAccount);
-                    lp.liabilityAccount = <LiabilityAccount>plainToClass<LiabilityAccount, Object>(LiabilityAccount, Utils.toJSObj(liabilityAccount));
-                    // lp.loanAccount = plainToClass(LoanAccount, Utils.toJSObj(loanAccount));
-                    return lp;
+                (value:Object) => {
+                    let obj:Object = Utils.toJSObj(value);
+                    liabilityAccount = <LiabilityAccount>plainToClass<LiabilityAccount, Object>(LiabilityAccount, obj);
+                     let liabilityLoanAccountBookingProcess = new LiabilityLoanAccountBookingProcess();
+                     liabilityLoanAccountBookingProcess.liabilityAccount = liabilityAccount;
+                     return liabilityLoanAccountBookingProcess
+                    // console.log(liabilityAccount);
+                    // lp.liabilityAccount = <LiabilityAccount>plainToClass<LiabilityAccount,Object>(LiabilityAccount, Utils.toJSObj(liabilityAccount));
+                    // // lp.loanAccount = plainToClass(LoanAccount, Utils.toJSObj(loanAccount));
+                    // return lp;
                 }
             )
 
