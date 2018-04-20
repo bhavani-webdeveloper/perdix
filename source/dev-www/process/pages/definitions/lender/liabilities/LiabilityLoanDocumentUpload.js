d4ed0d728abec698e2a807e0e73e874f206e4ab8
preethi.magesh <preethi.magesh@dvara.com>
2018-04-20 10:34:13 +0000

    changed getFormDefination to buildFormDefination in initialization
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/trunk@8404 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- source/dev-www/process/pages/definitions/lender/liabilities/LiabilityLoanDocumentUpload.js
+++ source/dev-www/process/pages/definitions/lender/liabilities/LiabilityLoanDocumentUpload.js
@@ -123,10 +123,14 @@ define(['perdix/domain/model/lender/LoanBooking/LiabilityLoanAccountBookingProce
                             ]
                         }
                     };
-                    var p1 = UIRepository.getLenderLiabilitiesLoanAccountBookingProcess().$promise;
-                    p1.then(function(repo){
-                        self.form = IrfFormRequestProcessor.getFormDefinition(repo, formRequest, configFile(), model);
-                    });
+                
+                    UIRepository.getLenderLiabilitiesLoanAccountBookingProcess().$promise
+                        .then(function(repo){
+                            return IrfFormRequestProcessor.buildFormDefinition(repo, formRequest, configFile(), model)
+                        })
+                        .then(function(form){
+                            self.form = form;
+                        });
 
                     if(_.hasIn($stateParams, 'pageId')) {
                         PageHelper.showLoader();
