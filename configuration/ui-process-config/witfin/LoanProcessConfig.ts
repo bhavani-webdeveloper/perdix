fe2b6dff7eca0f2c9fa3ddf3acd19ccacb9a0004
arpit.agarwal <arpit.agarwal@dvara.com>
2018-05-22 17:51:55 +0000

    Loan Process Config
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/trunk@9115 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- configuration/ui-process-config/witfin/LoanProcessConfig.ts
+++ configuration/ui-process-config/witfin/LoanProcessConfig.ts
@@ -29,7 +29,7 @@ let config = {
                         "name": "DefaultVehicleDocumnentsPolicy",
                         "arguments": {}
                     }
-                ],
+                    ],
                 "overrides": [
                     {
                         "type": "expr",
