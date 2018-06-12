7bf66e57060af236cc3b6f80ac40f1fbd5f7a9b4
stalin.solomon <stalin.solomon@dvara.com>
2018-06-12 14:02:04 +0000

    ragul checkin & bootload bietl
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/branches/v8.x-kgfs-uat@9476 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- configuration/management/server-ext/bootload.php
+++ configuration/management/server-ext/bootload.php
@@ -45,6 +45,26 @@ $settings = [
         'collation' => 'utf8_unicode_ci',
         'prefix'    => '',
     ],
+    'bietl_db' => [
+        'driver' => 'mysql',
+        'host' => getenv('BIETL_DB_HOST'),
+        'database' => getenv('BIETL_DB_NAME'),
+        'username' => getenv('BIETL_DB_USER'),
+        'password' => getenv('BIETL_DB_PASSWORD'),
+        'charset'   => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix'    => '',
+    ],
+    'encore_db' => [
+        'driver' => 'mysql',
+        'host' => getenv('DB_HOST'),
+        'database' => getenv('ENCORE_DB_NAME'),
+        'username' => getenv('DB_USER'),
+        'password' => getenv('DB_PASSWORD'),
+        'charset'   => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix'    => '',
+    ],
     'perdix' => [
         'v8_url' => getenv('PERDIX_V8_BASE_URL'),
         'username' => getenv('PERDIX_USERNAME'),
@@ -54,6 +74,11 @@ $settings = [
         'par_upload_path' => getenv('PAR_UPLOAD_PATH')
     ]
 ];
+
+$bi_etl = getenv('BIETL_DB_NAME', 'bietl');
+$bi = getenv('BI_DB_NAME', 'bi');
+$encore = getenv('ENCORE_DB_NAME', 'encore');
+$perdix = getenv('DB_NAME', 'financialForms');
 
 Settings::getInstance()->setSettings($settings);
 
