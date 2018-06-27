7b8896f2317f2cbe729d1e006a4909b94dc49394
rajkumar.v <rajkumar.v@dvara.com>
2018-06-27 10:55:38 +0000

    Grouploanrepay - Print Issue Fix
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/branches/v8.x-kgfs-uat@9873 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- source/dev-www/process/pages/definitions/loans/group/GroupApplication.js
+++ source/dev-www/process/pages/definitions/loans/group/GroupApplication.js
@@ -395,7 +395,7 @@ define({
                         {
                             "type": "button",
                             "key": "group.jlgGroupMembers[].insurenceForm",
-                            "title": "Download Insurence Form",
+                            "title": "Download Insurance Form",
                             "onClick": function(model, form, schemaForm, event) {
                                 Utils.downloadFile(irf.FORM_DOWNLOAD_URL + "?form_name=bajaj_insurance&record_id=" + model.group.jlgGroupMembers[event.arrayIndex].loanAccount.id);
                             }
