1ad6f773c33bc358ec5d8ef8a245a29e10cbf9c9
vasanthi.shanmugam <vasanthi.shanmugam@dvara.com>
2018-06-27 10:26:05 +0000

    Mutual Fund ICICI integration
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/trunk@9870 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- source/dev-www/process/pages/definitions/MutualFund/MutualFundEKYC.js
+++ source/dev-www/process/pages/definitions/MutualFund/MutualFundEKYC.js
@@ -1,22 +1,31 @@
-irf.pageCollection.controller(irf.controller("MutualFund.MutualFundEKYC"), ["$log", "Enrollment", "$q", "Utils", "$stateParams", "$scope", "PagesDefinition", "PageHelper", "irfNavigator", "Audit", "formHelper", "SessionStore", "elementsUtils",
-    function($log, Enrollment, $q, Utils, $stateParams, $scope, PagesDefinition, PageHelper, irfNavigator, Audit, formHelper, SessionStore, elementsUtils) {
-        $scope.$templateUrl = "process/pages/templates/mutualFund/MutualFundEKYC.html";
+irf.pageCollection.controller(irf.controller("MutualFund.MutualFundEKYC"), ["$log", "irfProgressMessage", "Enrollment", "$q", "Utils", "$stateParams", "$scope", "PagesDefinition", "PageHelper", "irfNavigator", "Audit", "formHelper", "SessionStore", "elementsUtils",
+    function($log, irfProgressMessage, Enrollment, $q, Utils, $stateParams, $scope, PagesDefinition, PageHelper, irfNavigator, Audit, formHelper, SessionStore, elementsUtils) {
+        // $scope.$templateUrl = "process/pages/templates/mutualFund/MutualFundEKYC.html";
 
-
-        
-        Enrollment.getCustomerById ({
+        if (!$stateParams.pageId) {
+            irfNavigator.goBack();
+        }
+        PageHelper.showLoader();
+        Enrollment.getCustomerById({
             id: $stateParams.pageId
-        }).then(function(data) {
-            irfProgressMessage.clear();
-            $scope.ekycdashboard = 
-            PageHelper.hideLoader();
+        }).$promise.then(function(data) {
+            $scope.customer = data;
+
+            if (!data.aadhaarNo) {
+                PageHelper.setError({
+                    "error": "Customer does not have aadhar number. eKYC is not possible"
+                });
+                return;
+            }
+            document.getElementById("Aadhar").value = data.aadhaarNo;
+            document.getElementById("kyc_data").value = (data.panNo ? data.panNo : "PANEXEMPT") + "|" + data.mobilePhone + "|" + (data.email || '');
+
+            document.getElementById('ekycForm').action = irf.CAMS_EKYC_INTEG_URL;
+            document.getElementById('ekycForm').submit();
+
         }, function(err) {
-            irfProgressMessage.clear();
-            PageHelper.hideLoader();
             PageHelper.showProgress('EKYC', 'EKYC login failed.', 5000);
             PageHelper.showErrors(err);
-        });
-
-
+        }).finally(PageHelper.hideLoader);
     }
 ]);
