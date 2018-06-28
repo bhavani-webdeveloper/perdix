7c9b6b97d3ad721feea461ccae7735e27bad83ab
vasanthi.shanmugam <vasanthi.shanmugam@dvara.com>
2018-06-28 07:37:07 +0000

    Mutual fund dashboard
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/trunk@9892 15cf4ce8-1186-411e-815e-a2b7fc64482c

---
+++ source/dev-www/process/pages/definitions/MutualFund/MutualFundDashboard.js
@@ -0,0 +1,22 @@
+irf.pageCollection.controller(irf.controller("MutualFund.MutualFundDashboard"), ['$log', '$scope', "formHelper", "$state", "$q", "Utils", 'PagesDefinition', 'SessionStore', "entityManager", "IndividualLoan", "LoanBookingCommons", "Messaging",
+    function($log, $scope, formHelper, $state, $q, Utils, PagesDefinition, SessionStore, entityManager, IndividualLoan, LoanBookingCommons, Messaging) {
+        $log.info("MutualFundDashboard loaded");
+        $scope.$templateUrl = "process/pages/templates/Page.Dashboard.html";
+        var currentBranch = SessionStore.getCurrentBranch();
+
+        var fullDefinition = {
+            "title": "Mutual Fund Dashboard",
+            "iconClass": "fa fa-money",
+            "items": [
+                "Page/Engine/MutualFund.MutualFundCustomerSearch"
+            ]
+        };
+
+        PagesDefinition.getUserAllowedDefinition(fullDefinition).then(function(resp) {
+            $scope.dashboardDefinition = resp;
+            var branchId = SessionStore.getBranchId();
+            var branchName = SessionStore.getBranch();
+            var centres = SessionStore.getCentres();
+        });
+    }
+]);
