1069aa1dc70ef8066afd9df7288276a763a9cda4
shahal.tharique <shahal.tharique@dvara.com>
2018-07-02 08:40:27 +0000

    # Changed
    1. IRF Env changes reverted
    
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/trunk@9954 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- source/dev-www/js/irf-env.js
+++ source/dev-www/js/irf-env.js
@@ -1,57 +1,9 @@
-// var irf = irf || {};
-// irf.BASE_URL = 'http://devkinara.perdix.in:8080/perdix-server';
-// irf.MANAGEMENT_BASE_URL = 'http://devkinara.perdix.in:8081/management';
-// irf.FORM_DOWNLOAD_URL = 'http://devkinara.perdix.in:8080/forms/formPrint.jsp';
-// irf.BI_BASE_URL = "http://devkinara.perdix.in:8081/bi-dev";
-// irf.PSYCHOMETRIC_BASE_URL = "http://devkinara.perdix.in:8080/psychometric-8.2";
-// irf.AUDIT_API_URL = 'http://devkinara.perdix.in:84';
-// irf.ANALYTICS_WEB_URL = 'http://34.202.113.237:12345';
-// irf.ANALYTICS_API_URL = 'http://34.202.113.237:1337';
-
-
-
-//Kinara management_sit
-// var irf = irf || {};
-// irf.BASE_URL = 'http://kinarasit.perdix.co.in:8080/perdix-server';
-// irf.MANAGEMENT_BASE_URL = 'http://kinarasit.perdix.co.in:8081/management';
-// irf.FORM_DOWNLOAD_URL = 'http://kinarasit.perdix.co.in:8080/forms/formPrint.jsp';
-// irf.BI_BASE_URL = "http://kinarasit.perdix.co.in:8081/bi";
-// irf.PSYCHOMETRIC_BASE_URL = "http://kinarasit.perdix.co.in:8080/psychometric";
-// irf.AUDIT_API_URL = "http://kinarasit.perdix.co.in:84";
-// irf.ANALYTICS_WEB_URL = 'http://34.202.113.237:12345';
-// irf.ANALYTICS_API_URL = 'http://34.202.113.237:1337';
-
 var irf = irf || {};
-irf.BASE_URL = 'http://52.55.177.70:82/perdix-server_p2uat';
-irf.MANAGEMENT_BASE_URL = 'http://52.55.177.70:8081/management_p2uat';
-irf.FORM_DOWNLOAD_URL = 'http://52.55.177.70:82/forms_p2uat/formPrint.jsp';
-irf.BI_BASE_URL = "http://52.55.177.70:8081/bi-p2uat";
-irf.PSYCHOMETRIC_BASE_URL = "http://52.55.177.70:82/psychometric_p2uat";
-irf.AUDIT_API_URL = "http://52.55.177.70:84";
-
-
-// var irf = irf || {};
-// irf.BASE_URL = 'https://sit.witfin.perdix.co.in:8081/perdix-server';
-// irf.MANAGEMENT_BASE_URL = 'https://sit.witfin.perdix.co.in:8081/management';
-// irf.FORM_DOWNLOAD_URL = 'https://sit.witfin.perdix.co.in:8081/forms/formPrint.jsp';
-// irf.BI_BASE_URL = "https://sit.witfin.perdix.co.in:8081/bi";
-// irf.PSYCHOMETRIC_BASE_URL = "http://sit.witfin.perdix.co.in:8080/psychometric";
-
-
-/*//KGFS UAT2
-var irf = irf || {};
-irf.BASE_URL = 'http://34.202.145.163:8080/perdix-server';
-irf.MANAGEMENT_BASE_URL = 'http://34.202.145.163:8081/management/';
-irf.FORM_DOWNLOAD_URL = 'http://34.202.145.163:8080/forms/formsPrint.php';
-irf.BI_BASE_URL = "http://34.202.145.163:8081/bi";*/
-
-// var irf = irf || {}; 
-// irf.BASE_URL = 'http://devkinara.perdix.in:8080/perdix-server'; 
-// irf.MANAGEMENT_BASE_URL = 'http://devkinara.perdix.in:8081/management'; 
-// irf.FORM_DOWNLOAD_URL = 'http://devkinara.perdix.in:8080/forms/formPrint.jsp'; 
-// irf.BI_BASE_URL = "http://devkinara.perdix.in:8081/bi-dev"; 
-// irf.PSYCHOMETRIC_BASE_URL = "http://devkinara.perdix.in:8080/psychometric-8.2";
-// irf.AUDIT_API_URL = 'http://52.202.57.36:71';
-// irf.ANALYTICS_WEB_URL = 'http://34.202.113.237:12345';
-// irf.ANALYTICS_API_URL = 'http://34.202.113.237:1337';
-
+irf.BASE_URL = 'http://devkinara.perdix.in:8080/perdix-server';
+irf.MANAGEMENT_BASE_URL = 'http://devkinara.perdix.in:8081/management';
+irf.FORM_DOWNLOAD_URL = 'http://devkinara.perdix.in:8080/forms/formPrint.jsp';
+irf.BI_BASE_URL = "http://devkinara.perdix.in:8081/bi-dev";
+irf.PSYCHOMETRIC_BASE_URL = "http://devkinara.perdix.in:8080/psychometric-8.2";
+irf.AUDIT_API_URL = 'http://devkinara.perdix.in:84';
+irf.ANALYTICS_WEB_URL = 'http://34.202.113.237:12345';
+irf.ANALYTICS_API_URL = 'http://34.202.113.237:1337';
