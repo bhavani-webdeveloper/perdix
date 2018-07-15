78dc93bc8db752c5868e81383b3df0dffa77771a
stalin.solomon <stalin.solomon@dvara.com>
2018-07-15 10:12:11 +0000

    new age build
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/trunk@10279 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- source/dev-www/js/irf-env.js
+++ source/dev-www/js/irf-env.js
@@ -1,10 +1,10 @@
 var irf = irf || {};
-irf.BASE_URL = 'http://devkinara.perdix.in:8080/perdix-server';
-irf.MANAGEMENT_BASE_URL = 'http://devkinara.perdix.in:8081/management';
-irf.FORM_DOWNLOAD_URL = 'http://devkinara.perdix.in:8080/forms/formPrint.jsp';
-irf.BI_BASE_URL = "http://devkinara.perdix.in:8081/bi-dev";
-irf.PSYCHOMETRIC_BASE_URL = "http://devkinara.perdix.in:8080/psychometric-8.2";
-irf.AUDIT_API_URL = 'http://devkinara.perdix.in:84';
-irf.ANALYTICS_WEB_URL = 'http://34.202.113.237:12345';
-irf.ANALYTICS_API_URL = 'http://34.202.113.237:1337';
-
+irf.BASE_URL = "{{perdix.api}}";
+irf.MANAGEMENT_BASE_URL = "{{management.base}}";
+irf.FORM_DOWNLOAD_URL = "{{forms.jsp}}";
+irf.BI_BASE_URL = "{{bi.base}}";
+irf.PSYCHOMETRIC_BASE_URL = "{{psychometric.api}}";
+irf.AUDIT_API_URL = "{{audit.api}}";
+irf.ANALYTICS_WEB_URL = "{{analytics.web}}";
+irf.ANALYTICS_API_URL = "{{analytics.api}}";
+irf.CAMS_EKYC_INTEG_URL = "{{cams.ekyc-integ-web}}";
