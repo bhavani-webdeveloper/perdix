03558db21943ec184a469f7b19fda941b4820a13
preethi.magesh <preethi.magesh@dvara.com>
2018-07-19 06:28:13 +0000

    Removed extra comma from BranchDepositDetail.js
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/trunk@10362 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- source/dev-www/process/pages/definitions/loans/individual/collections/BranchDepositDetail.js
+++ source/dev-www/process/pages/definitions/loans/individual/collections/BranchDepositDetail.js
@@ -20,7 +20,7 @@
                         }
                         if(model.loanCollectionDetail.totalCashCollected){
                             var roundingPrecision = SessionStore.getGlobalSetting("currencyPrecision");
-                            model.loanCollectionDetail.totalCashCollected = _.round(model.loanCollectionDetail.totalCashCollected, roundingPrecision),
+                            model.loanCollectionDetail.totalCashCollected = _.round(model.loanCollectionDetail.totalCashCollected, roundingPrecision)
                         }
                         if(model.branchCollectionDetail && model.branchCollectionDetail[0]['instrumentType']=='CHQ'){
                             model.loanCollectionDetail = {
