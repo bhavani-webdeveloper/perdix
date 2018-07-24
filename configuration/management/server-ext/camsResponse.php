e9393d82aca0f1ee20e22a7d86d592c76776bf65
akshay.yadav <akshay.yadav@dvara.com>
2018-07-24 11:36:30 +0000

    creating camsResponse.php for cams integration
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/trunk@10466 15cf4ce8-1186-411e-815e-a2b7fc64482c

---
+++ configuration/management/server-ext/camsResponse.php
@@ -0,0 +1,28 @@
+<style type="text/css">
+	.successCallBack{
+		font-size: 30px;
+		color: red;
+		text-align: center;
+	}
+</style>
+<!-- <script type="text/javascript">	
+	$http.get('https://eiscuat1.camsonline.com/ekycuat3/eKYCVal_Aadhar.aspx').
+		success(function(data, status, headers, config) {
+		var results = JSON.parse(data);
+		PageHelper.setError(results);
+		return;
+	    // this callback will be called asynchronously
+	    // when the response is available
+	    alert(data)
+	})
+	.error(function(data, status, headers, config) {
+		// called asynchronously if an error occurs
+		// or server returns response with an error status.
+		alert(data)
+	});
+</script> -->
+
+<div class="successCallBack">
+   EKYC done Successfully.
+</div>
+
