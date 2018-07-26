09d250dbadea9c3d3b6f4a63056580455f9c349c
shahal.tharique <shahal.tharique@dvara.com>
2018-07-26 09:36:25 +0000

    # Fixed
    1. Removed witfin site code
    
    
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/trunk@10517 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- source/dev-www/process/pages/definitions/loans/individual/disbursement/Disbursement.js
+++ source/dev-www/process/pages/definitions/loans/individual/disbursement/Disbursement.js
@@ -212,7 +212,7 @@ irf.pageCollection.factory(irf.page("loans.individual.disbursement.Disbursement"
                     {
                         "key": "loanAccountDisbursementSchedule.referenceNumber",
                         "title": "CHEQUE_NO",
-                        "condition":"model.siteCode=='witfin'&& model.loanAccountDisbursementSchedule.modeOfDisbursement.toLowerCase()=='cheque'"
+                        "condition":"model.loanAccountDisbursementSchedule.modeOfDisbursement.toLowerCase()=='cheque'"
                     },
                     {
                         key: "loanAccountDisbursementSchedule.disbursementFromBankAccountNumber",
@@ -457,7 +457,7 @@ irf.pageCollection.factory(irf.page("loans.individual.disbursement.Disbursement"
                     }
                 ]
             }
-            
+
 
 
             ],
