6544b2d94e9aed1666bb2b104690cf10b8a88b93
stalin.solomon <stalin.solomon@dvara.com>
2018-08-09 12:20:22 +0000

    scroll enhancement
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/trunk@10882 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- source/dev-www/js/irf-config.IREPDhan.js
+++ source/dev-www/js/irf-config.IREPDhan.js
@@ -2,3 +2,20 @@ irf.appConfig.CONNECT_PERDIX7 = false;
 irf.appConfig.CORPORATE_LOGO = "img/corporate_logo.IREPDhan.png";
 irf.appConfig.FORCE_COLOR = "";
 irf.appConfig.AMS_ENABLED = false;
+
+
+var blurActiveNumber = function(e) {
+    if (document.activeElement.type === "number") {
+        document.activeElement.blur();
+        e.preventDefault();
+    }
+    return true;
+}
+var keyBlurActive = function(e) {
+    if (e.which == 38 || e.which == 40) {
+        blurActiveNumber(e);
+    }
+    return true;
+}
+document.addEventListener("wheel", blurActiveNumber);
+document.addEventListener("keydown", keyBlurActive);
