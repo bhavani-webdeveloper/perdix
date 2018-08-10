2de82d83f3aa766f0030e6943d0b2c1153670e43
rajkumar.v <rajkumar.v@dvara.com>
2018-08-10 14:23:13 +0000

    Scheme_master_upload for pahal
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/trunk@10922 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- source/dev-www/process/pages/definitions/excelUpload/ExcelUpload.js
+++ source/dev-www/process/pages/definitions/excelUpload/ExcelUpload.js
@@ -1,10 +1,10 @@
 define({
     pageUID: "excelUpload.ExcelUpload",
     pageType: "Engine",
-    dependencies: ["$log", "SessionStore", "$state", "$stateParams", "DocumentTracking", "ExcelUpload", "$timeout"],
-
-    $pageFn: function($log, SessionStore, $state, $stateParams, DocumentTracking, ExcelUpload, $timeout) {
-
+    dependencies: ["$log", "SessionStore", "$state", "$stateParams", "DocumentTracking", "ExcelUpload"],
+ 
+    $pageFn: function($log, SessionStore, $state, $stateParams, DocumentTracking, ExcelUpload) {
+ 
         var branch = SessionStore.getBranch();
         return {
             "type": "schema-form",
@@ -13,47 +13,34 @@ define({
             initialize: function(model, form, formCtrl) {
                 model.upload = model.upload || {};
                 $log.info("DocumentTrackingBulkUpload  Page got initialized");
-                var self = this;
-
-                var formData = function() {
-                    self.form = [{
-                        "type": "box",
-                        "title": "GENERIC_EXCEL_UPLOAD",
-                        "colClass": "col-sm-6",
-                        "items": [{
-                            key: "upload.excel_type",
-                            title: "EXCEL_TYPE",
-                            type: "select",
-                            enumCode: "excel_type",
-                            "required": true
-                        }, {
-                            "key": "upload.upload",
-                            "title": "EXCEL_UPLOAD",
-                            "category": "ACH",
-                            "subCategory": "cat2",
-                            "type": "file",
-                            "required": true,
-                            "fileType": "application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-                            customHandle: function(file, progress, modelValue, form, model) {
-                                ExcelUpload.ExcelFileUpload(file, progress, model.upload.excel_type).then(function(resp) {
-                                    formData();
-                                    // model.upload = {};
-                                });
-                            }
-                        }]
-                    }];
-                }
-
-                function formData() {
-                    $timeout(function() {
-                        self.form = [];
-                    });
-                };
             },
-
             offline: false,
             getOfflineDisplayItem: function(item, index) {},
-            form: [],
+            form: [{
+                "type": "box",
+                "title": "GENERIC_EXCEL_UPLOAD",
+                "colClass": "col-sm-6",
+                "items": [{
+                    key: "upload.excel_type",
+                    title: "EXCEL_TYPE",
+                    type: "select",
+                    enumCode: "excel_type",
+                    "required": true
+                }, {
+                    "key": "upload.upload",
+                    "title": "EXCEL_UPLOAD",
+                    "category": "ACH",
+                    "subCategory": "cat2",
+                    "type": "file",
+                    "required": true,
+                    "fileType": "application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                    customHandle: function(file, progress, modelValue, form, model) {
+                        ExcelUpload.ExcelFileUpload(file, progress, model.upload.excel_type).then(function(resp) {
+                            model.upload = {};
+                        });
+                    }
+                }]
+            }],
             schema: {
                 "type": "object",
                 "properties": {
@@ -64,7 +51,7 @@ define({
             },
             actions: {
                 submit: function(model, form, formName) {
-
+ 
                 },
                 proceed: function(model, formCtrl, form, $event) {}
             }
