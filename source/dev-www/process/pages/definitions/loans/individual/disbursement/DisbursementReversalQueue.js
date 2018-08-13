c2791918018d3f550213f6618f0d3093cfaedc17
kishan.yadav <kishan.yadav@dvara.com>
2018-08-13 10:15:42 +0000

    disbursementReversal queue
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/trunk@10931 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- source/dev-www/process/pages/definitions/loans/individual/disbursement/DisbursementReversalQueue.js
+++ source/dev-www/process/pages/definitions/loans/individual/disbursement/DisbursementReversalQueue.js
@@ -1,139 +1,175 @@
 define({
     pageUID: "loans.individual.disbursement.DisbursementReversalQueue",
     pageType: "Engine",
-    dependencies: ["$log", "formHelper", "$state", "SessionStore", "irfNavigator","$q", "IndividualLoan","entityManager"],
-    $pageFn: function($log, formHelper, $state, SessionStore, irfNavigator,$q, IndividualLoan,entityManager) {
+    dependencies: ["$log", "formHelper", "SessionStore","$q", "IndividualLoan","PageHelper","Queries"],
+    $pageFn: function($log, formHelper, SessionStore,$q, IndividualLoan,PageHelper,Queries) {
         return {
-            "type": "search-list",
+            "type": "schema-form",
             "title": "DISBURSEMENT_REVERSAL_QUEUE",
             "subTitle": "",
             "uri":"",
             initialize: function (model, form, formCtrl) {
-
+                model.DisbursementReverse = model.DisbursementReverse || {};
                 model.branchName = SessionStore.getBranch();
                 model.branch = SessionStore.getCurrentBranch().branchId;
 
-                model.stage = 'Completed';
+                model.currentStage = 'Completed';
                 console.log(model);
             },
-            offline: false,
-            definition: {
-                title: "ReadyForReversal",
-                autoSearch: true,
-                sorting:true,
-                sortByColumns:{
-                    "scheduledDisbursementDate":"Scheduled Disbursement Date"
-                },
-                searchForm: [
-                    "*"
-                ],
-                searchSchema: {
-                    "type": 'object',
-                    "title": "VIEW_LOANS",
-                    "required":[],
-                    "properties": {
-
-                        // "customerSignatureDate": {
-                        //     "title": "CUSTOMER_SIGNATURE_DATE",
-                        //     "type": "string",
-                        //     "x-schema-form": {
-                        //         "type": "date"
-
-                        //     }
-                        // },
-                        'branch': {
-                            'title': "BRANCH",
-                            "type": ["string", "null"],
-                            "x-schema-form": {
-                                "type": "userbranch",
-                                "screenFilter": true
-                            }
+            form: [
+                {
+                    "type": "box",
+                    "title": "LAST_DISBURSEMENT_DATA",
+                    "items": [
+                        {
+                            "key": "DisbursementReverse.branchId",
+                            "title": "BRANCH",
+                            "enumCode": "branch_id",
+                            "type": "userbranch",
+                            "readonly": true
                         },
-                        "centre": {
-                            "title": "CENTRE",
-                            "type": ["integer", "null"],
-                            "x-schema-form": {
-                                "type": "select",
-                                "enumCode": "centre",
-                                "parentEnumCode": "branch",
-                                "parentValueExpr": "model.branch",
-                                "screenFilter": true
-                            }
-                        },
-
-                        "scheduledDisbursementDate": {
-                            "title": "SCHEDULED_DISBURSEMENT_DATE",
-                            "type": "string",
-                            "x-schema-form": {
-                                "type": "date"
-                            }
-                        }
-                    }
-                },
-                getSearchFormHelper: function() {
-                    return formHelper;
-                },
-                getResultsPromise: function(searchOptions, pageOpts){
-                    return IndividualLoan.searchDisbursement({
-                        'currentStage': 'Completed',
-                        'branchId':searchOptions.branch,
-                        'centreId': searchOptions.centre,
-                        'customerSignatureDate': searchOptions.customerSignatureDate,
-                        'scheduledDisbursementDate': searchOptions.scheduledDisbursementDate,
-                        'page': pageOpts.pageNo,
-                        'per_page': pageOpts.itemsPerPage,
-                        'sortBy':searchOptions.sortBy,
-                        
-                    }).$promise;
-
-                },
-                paginationOptions: {
-                    "viewMode": "page",
-                    "getItemsPerPage": function(response, headers){
-                        return 20;
-                    },
-                    "getTotalItemsCount": function(response, headers){
-                        return headers['x-total-count']
-                    }
-                },
-                listOptions: {
-                    itemCallback: function(item, index) {
-                        $log.info(item);
-
-                    },
-                    getItems: function(response, headers){
-                        if (response!=null && response.length && response.length!=0){
-                            return response;
-                        }
-                        return [];
-                    },
-                    getListItem: function(item){
-                        return [
-                            item.customerName + " ( Account #: "+item.accountNumber+")",
-                            "<em>Disbursed Amount:  &#8377;"+(_.isEmpty(item.disbursedAmount)?0:item.disbursedAmount)+", Disbursement Amount :  &#8377;"+item.disbursementAmount+"</em>",
-                            "Customer Signature Date  : " + (_.isEmpty(item.customerSignatureDate)?" NA ":item.customerSignatureDate)+", Scheduled Disbursement Date :"+(_.isEmpty(item.scheduledDisbursementDate)?" NA ":item.scheduledDisbursementDate)
-                        ]
-                    },
-                    getActions: function(){
-                        return [
-                            {
-                                name: "Confirm Reversal",
-                                desc: "",
-                                fn: function(item, index){
-                                    //entityManager.setModel('loans.individual.disbursement.DisbursementReversal', {_disbursementConfirmation:item});
-                                    irfNavigator.go({
-                                        'state': 'Page.Engine',
-                                        'pageName': 'loans.individual.disbursement.DisbursementReversal',
-                                        'pageData': item
-                                    });
-
-
-                                },
-                                isApplicable: function(item, index){
-                                    return true;
+                        {
+                            "key": "DisbursementReverse.accountNumber",
+                            "title": "ACCOUNT_NUMBER",
+                            "type": "lov",
+                            "autolov": true,
+                            "inputMap": {
+                                "accountNumber":{
+                                    "key":"DisbursementReverse.accountNumber",
+                                    "title":"ACCOUNT_NUMBER"
                                 }
+                            },
+                            "outputMap": {
+                                "accountNumber": "DisbursementReverse.accountNumber",
+                                "branchId": "DisbursementReverse.branchId",
+                                "customerName":"DisbursementReverse.customerName",
+                                "transactionId":"DisbursementReverse.transactionId",
+                                "transactionName":"DisbursementReverse.transactionName",
+                                "actualDisbursementDate":"DisbursementReverse.actualDisbursementDate",
+                                "disbursedAmount":"DisbursementReverse.disbursedAmount"
+                            },
+                            "searchHelper": formHelper,
+                            initialize: function(inputModel) {
+                            },
+                            search: function(inputModel, form, model) {
+                                var deferred = $q.defer();
+                                IndividualLoan.searchDisbursement({
+                                    'currentStage': 'Completed',
+                                    'accountNumber':  (inputModel.accountNumber || model.DisbursementReverse.accountNumber)
+                                    
+                                }).$promise
+                                  .then(function(resp){
+                                      deferred.resolve({
+                                          "headers": {
+                                              "x-total-count": 1
+                                          },
+                                          body: [resp.body[0]]
+                                      });
+                                  },deferred.reject);
+                                return deferred.promise;
+                            },
+                            getListDisplayItem: function(item, index) {
+                                $log.info(item);
+                                return [
+                                    item.body[0].accountNumber,
+                                    item.body[0].branchName,
+                                    item.body[0].customerName,
+                                    item.body[0].transactionId,
+                                    item.body[0].transactionName,
+                                    item.body[0].actualDisbursementDate,
+                                    item.body[0].disbursedAmount
+                                ];
+                            },
+                            onSelect: function(result, model, context) {
+    
                             }
-                        ];
+                        },
+                        {
+                            "key": "DisbursementReverse.customerName",
+                            "title": "CUSTOMER_NAME",
+                            "condition": "model.DisbursementReverse.customerType == 'Individual'",
+                            "readonly": true
+                        },
+                        {
+                            "key": "DisbursementReverse.customerName",
+                            "title": "ENTERPRISE_NAME",
+                            "condition": "model.DisbursementReverse.customerType == 'Enterprise'",
+                            "readonly": true
+                        },
+                        {
+                            "key": "DisbursementReverse.transactionId",
+                            "title": "TRANSACTION_ID",
+                            "readonly": true
+                        },
+                        {
+                            "key": "DisbursementReverse.actualDisbursementDate",
+                            "title": "DISBURSEMENT_DATE",
+                            "readonly": true,
+                            "type": "date"
+                        },
+                        {
+                            "key": "DisbursementReverse.disbursedAmount",
+                            "title": "AMOUNT",
+                            "readonly": true
+                        }
+                    ]
+                },
+                {
+                    "type": "actionbox",
+                    "items": [
+                        {
+                            "type": "submit",
+                            "title": "REVERSE_DISBURSEMENT"
+                        }
+                    ]
+                }
+            ],
+            schema: {
+                "$schema": "http://json-schema.org/draft-04/schema#",
+                "type": "object",
+                "properties": {
+                    "DisbursementReverse": {
+                        "type": "object",
+                        "properties": {
+                            "accountNumber": {
+                                "type": "string"
+                            }
+                        }
+                    }
+                }
+            },
+            actions: {
+                submit: function(model, form, formName) {
+                    if(PageHelper.isFormInvalid(form)) {
+                        return false;
+                    }
+                    if(window.confirm("Are you sure?")){
+                        PageHelper.showLoader();
+                        var reqData={};
+                            reqData.disbursementConfirmations =[];
+                            var targetStage='ReadyForDisbursement';
+                            reqData.disbursementConfirmations.push({
+                                                accountNumber: model.DisbursementReverse.accountNumber,
+                                                "currentStage": "Completed",
+                                                stage: targetStage,
+                                                status: "REJECT",
+                                                tranchNumber:model.DisbursementReverse.trancheNumber,
+                                                transactionId: model.DisbursementReverse.transactionId,
+                                                udf1:"Rejected",
+                                                udf4:model.DisbursementReverse.udf4,
+                                                udf5:model.DisbursementReverse.udf5
+                                            });
+                            delete reqData.DisbursementReverse;
+                            IndividualLoan.batchDisbursementConfirmation(reqData,function(resp,header){
+                                PageHelper.showProgress("upd-disb","Done.","5000");
+                                backToQueue();
+                            },function(resp){
+                                PageHelper.showProgress("upd-disb","Oops. An error occurred","5000");
+                                PageHelper.showErrors(resp);
+
+                            }).$promise.finally(function(){
+                                PageHelper.hideLoader();
+                            });  
                     }
                 }
             }
