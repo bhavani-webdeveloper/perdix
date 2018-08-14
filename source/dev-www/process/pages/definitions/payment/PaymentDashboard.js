322138fcbe2c75204acbe0ef4423ce6db6f51348
prakash.d <prakash.d@dvara.com>
2018-08-14 05:33:33 +0000

    # Changed
    payment dashboard page is changed as adhoc page
    
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/trunk@10963 15cf4ce8-1186-411e-815e-a2b7fc64482c

---
+++ source/dev-www/process/pages/definitions/payment/PaymentDashboard.js
@@ -0,0 +1,61 @@
+irf.pageCollection.controller(irf.controller("payment.PaymentDashboard"), ['$log', '$scope', "formHelper", "$state", "$q", "Utils", 'PagesDefinition', 'SessionStore',"Payment",
+    function($log, $scope, formHelper, $state, $q, Utils, PagesDefinition, SessionStore, Payment) {
+        $log.info("Page.PaymentDashboard.html loaded");
+        $scope.$templateUrl = "process/pages/templates/Page.Dashboard.html";
+        var fullDefinition = {
+            "title": "PAYMENT_DASHBOARD",
+            "iconClass": "fa fa-inr",
+            "items": [
+                "Page/Engine/payment.PaymentInitiation",
+                "Page/Engine/payment.paymentInitiationSearch",
+                "Page/Engine/payment.PaymentApprovalSearch", 
+                "Page/Engine/payment.PaymentCreateBatch",
+                "Page/Engine/payment.PaymentConfirmation", 
+                "Page/Engine/payment.paymentRejected"
+            ]
+        };
+
+        PagesDefinition.getUserAllowedDefinition(fullDefinition).then(function(resp) {
+            $scope.dashboardDefinition = resp;
+
+          var pisMenu = $scope.dashboardDefinition.$menuMap["Page/Engine/payment.paymentInitiationSearch"];
+
+            if (pisMenu) {
+                pisMenu.data = 0;
+                    Payment.search({
+                        'currentStage': 'PaymentInitiation'
+                    }).$promise.then(function(response, headerGetter) {
+                        pisMenu.data = pisMenu.data + Number(response.headers['x-total-count']);
+                    }, function() {
+                        pisMenu.data = '-';
+                    });
+            }
+            var pasMenu = $scope.dashboardDefinition.$menuMap["Page/Engine/payment.PaymentApprovalSearch"];
+
+            if (pasMenu) {
+                pasMenu.data = 0;
+                    Payment.search({
+                        'currentStage': 'PaymentApproval'
+                    }).$promise.then(function(response, headerGetter) {
+                        pasMenu.data = pasMenu.data + Number(response.headers['x-total-count']);
+                    }, function() {
+                        pasMenu.data = '-';
+                    });
+            }
+            var prMenu = $scope.dashboardDefinition.$menuMap["Page/Engine/payment.paymentRejected"];
+
+            if (prMenu) {
+                prMenu.data = 0;
+                    Payment.search({
+                        'currentStage': 'PaymentRejected'
+                    }).$promise.then(function(response, headerGetter) {
+                        prMenu.data = prMenu.data + Number(response.headers['x-total-count']);
+                    }, function() {
+                        prMenu.data = '-';
+                    });
+            }
+           
+
+        });
+    }
+]);
