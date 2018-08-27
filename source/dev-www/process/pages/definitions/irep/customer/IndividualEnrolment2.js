df1c2d1cc54402bb93b5e884e8ec92f2d2db9df5
vasanthi.shanmugam <vasanthi.shanmugam@dvara.com>
2018-08-27 09:22:57 +0000

    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/trunk@11239 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- source/dev-www/process/pages/definitions/irep/customer/IndividualEnrolment2.js
+++ source/dev-www/process/pages/definitions/irep/customer/IndividualEnrolment2.js
@@ -1697,6 +1697,8 @@ define(['perdix/domain/model/customer/EnrolmentProcess', 'perdix/infra/api/Angul
                                 PageHelper.showProgress('enrolment', 'Done.', 5000);
                                 PageHelper.clearErrors();
                                 BundleManager.pushEvent(model.pageClass +"-updated", model._bundlePageObj, enrolmentProcess);
+                                BundleManager.pushEvent('new-enrolment', model._bundlePageObj, {customer: model.customer});
+
                             }, function (err) {
                                 PageHelper.showErrors(err);
                                 PageHelper.showProgress('enrolment', 'Oops. Some error.', 5000);
@@ -1719,6 +1721,8 @@ define(['perdix/domain/model/customer/EnrolmentProcess', 'perdix/infra/api/Angul
                                 PageHelper.showProgress('enrolment', 'Done.', 5000);
                                 PageHelper.clearErrors();
                                 BundleManager.pushEvent(model.pageClass +"-updated", model._bundlePageObj, enrolmentProcess);
+                                BundleManager.pushEvent('new-enrolment', model._bundlePageObj, {customer: model.customer});
+
                                 model.enrolmentProcess.proceed()
                                 .subscribe(function(enrolmentProcess) {
                                     PageHelper.showProgress('enrolment', 'Done.', 5000);
