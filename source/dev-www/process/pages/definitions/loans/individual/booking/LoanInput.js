50c2dde36eee07812821abef5f09b7a4b0e99eda
shahal.tharique <shahal.tharique@dvara.com>
2018-08-29 14:02:40 +0000

    # Fixed
    1. Computation of scheduleStartDate only if allowPreEmiIntereset is set to true at page config level.
    
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/trunk@11341 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- source/dev-www/process/pages/definitions/loans/individual/booking/LoanInput.js
+++ source/dev-www/process/pages/definitions/loans/individual/booking/LoanInput.js
@@ -404,7 +404,7 @@ irf.pageCollection.factory(irf.page("loans.individual.booking.LoanInput"),
                             model.additional.portfolioUrnSelector = "coapplicant";
                         }
 
-                        if (model.loanAccount.disbursementSchedules.length > 0 && model.loanAccount.disbursementSchedules[0] && model.loanAccount.disbursementSchedules[0].moratoriumPeriodInDays && _.isNumber(model.loanAccount.disbursementSchedules[0].moratoriumPeriodInDays)  && model.loanAccount.disbursementSchedules[0].scheduledDisbursementDate) {
+                        if (model.allowPreEmiInterest && model.loanAccount.disbursementSchedules.length > 0 && model.loanAccount.disbursementSchedules[0] && model.loanAccount.disbursementSchedules[0].moratoriumPeriodInDays && _.isNumber(model.loanAccount.disbursementSchedules[0].moratoriumPeriodInDays)  && model.loanAccount.disbursementSchedules[0].scheduledDisbursementDate) {
                             model._currentDisbursement = model.loanAccount.disbursementSchedules[0];
                             model.loanAccount.scheduleStartDate = moment(model.loanAccount.disbursementSchedules[0].scheduledDisbursementDate, "YYYY-MM-DD").add(model.loanAccount.disbursementSchedules[0].moratoriumPeriodInDays, 'days').format("YYYY-MM-DD");
                         }
