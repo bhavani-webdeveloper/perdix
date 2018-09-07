9bcdde2220db7eb70b7a1a513b99b772642a1793
rajkumar.v <rajkumar.v@dvara.com>
2018-09-07 11:32:10 +0000

    Customer view group
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/branches/v8.x-kgfs-production@11571 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- source/dev-www/process/models/v1/Queries.js
+++ source/dev-www/process/models/v1/Queries.js
@@ -713,6 +713,22 @@ irf.models.factory('Queries', [
             return deferred.promise;
         }
 
+        resource.getCustomerGroups = function(urnNo) {
+            var deferred = $q.defer();
+            resource.getResult("getCustomerGroups.list",{'urn_no':urnNo}).then(function(records) {
+                if (records && records.results) {
+                    var result = {
+                        headers: {
+                            "x-total-count": records.results.length
+                        },
+                        body: records.results
+                    };
+                    deferred.resolve(result);
+                }
+            }, deferred.reject);
+            return deferred.promise;
+        }
+
         return resource;
     }
 ]);
