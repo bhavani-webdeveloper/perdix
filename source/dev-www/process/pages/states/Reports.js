fe80e7b8c50747abcd86664d3f7191050477128a
kishan.yadav <kishan.yadav@dvara.com>
2018-09-17 06:27:56 +0000

    report dashboard changes ui side , using user branch and centre branch of irfElements
    git-svn-id: http://svn.perdix.co/svn/perdix/irf-perdix-client/trunk@11700 15cf4ce8-1186-411e-815e-a2b7fc64482c

--- source/dev-www/process/pages/states/Reports.js
+++ source/dev-www/process/pages/states/Reports.js
@@ -1,141 +1,197 @@
 irf.pages.controller("ReportsCtrl",
-["$log", "$scope", "SessionStore", "$state", "$stateParams", "$q", "BIReports", "PageHelper", "$timeout", "irfSimpleModal",
-	function($log, $scope, SessionStore, $state, $stateParams, $q, BIReports, PageHelper, $timeout, irfSimpleModal){
-	$log.info("ReportsCtrl loaded");
+    ["$log", "$scope", "SessionStore", "$state", "$stateParams", "$q", "BIReports", "PageHelper", "$timeout", "irfSimpleModal", "formHelper",
+        function ($log, $scope, SessionStore, $state, $stateParams, $q, BIReports, PageHelper, $timeout, irfSimpleModal, formHelper) {
+            $log.info("ReportsCtrl loaded");
 
-	var pageData = {};
-	var tabData = {"menu_name":$stateParams.pageId};
+            var pageData = {};
+            var tabData = {
+                "menu_name": $stateParams.pageId
+            };
 
-	var userName = SessionStore.getLoginname();
+            var userName = SessionStore.getLoginname();
 
-	var currentUserRole = SessionStore.getUserRole();
+            var currentBranch = SessionStore.getBranchId();
 
-	$scope.currentUserAccessLevel = currentUserRole.accessLevel;
+            var currentUserRole = SessionStore.getUserRole();
 
-	PageHelper.showLoader();
+            $scope.currentUserAccessLevel = currentUserRole.accessLevel;
 
-	var allAccessLevels = [10, 20, 30]; // filtering not available for 40
+            PageHelper.showLoader();
 
-	$scope.filterAccessLevels = allAccessLevels.slice(allAccessLevels.indexOf($scope.currentUserAccessLevel));
-	$scope.fSelect = Array($scope.filterAccessLevels.length);
-	$scope.fNodes = Array($scope.filterAccessLevels.length);
+            var initialize = function () {
+                if (_.isObject(pageData)) {
+                    if (pageData.filter) {
+                        tabData.filter = pageData.filter;
+                    }
+                }
+                $scope.ResultDataSet = [];
+                BIReports.reportTabList(tabData).$promise.then(function (response) {
+                    $scope.TabDefinition = response;
+                    $timeout(function () {
+                        $scope.active = 0;
+                    });
+                }).finally(function () {
+                    PageHelper.hideLoader();
+                });
+            };
 
-	$scope.listNextAccessLevelItems = function(accessLevel, selectedValue, index) {
-		for (i = index; i < $scope.filterAccessLevels.length; i++) $scope.fSelect[i] = [];
-		BIReports.reportFilterList({
-			"accessLevel": accessLevel,
-			"selectedValue": selectedValue,
-			"userId": userName,
-			"userAccessLevel": $scope.currentUserAccessLevel
-		}).$promise.then(function(response) {
-			$scope.fNodes[index] = response.items;
-		});
-	}
+			$scope.filterForm = [{
+                type: "section",
+                "condition":"model.currentUserAccessLevel < 40",
+                "htmlClass": "pull-right",
+                "items": [{
+                    "type": "userbranch",
+                    "key": "userbranch",
+                    "notitle": true
+                }, {
+                    "type": "select",
+                    "key": "usercentre",
+                    "notitle": true,
+                    "enumCode": "centre_code",
+                    "parentEnumCode": "userbranch",
+                    "parentValueExpr": "model.userbranch"
+                }, {
+                    type: "button",
+                    notitle: true,
+                    "title": "Apply Filter",
+                    "icon": "fa fa-filter",
+                    "style": "btn-default btn-xs",
+                    "onClick": "actions.applyFilter()"
+                }, {
+                    type: "button",
+                    notitle: true,
+                    "title": "Reset",
+                    "icon": "fa fa-rotate-left",
+                    "style": "btn-default btn-xs",
+                    "onClick": "actions.resetFilter()"
+                }]
+            }, {
+                type: "section",
+                "condition":"model.currentUserAccessLevel == 40",
+                "htmlClass": "pull-right",
+                "items": [{
+                    "type": "select",
+                    "key": "usercentre",
+                    "notitle": true,
+                    "enumCode": "usercentre",
+                }, {
+                    type: "button",
+                    notitle: true,
+                    "title": "Apply Filter",
+                    "icon": "fa fa-filter",
+                    "style": "btn-default btn-xs",
+                    "onClick": "actions.applyFilter()"
+                }, {
+                    type: "button",
+                    notitle: true,
+                    "title": "Reset",
+                    "icon": "fa fa-rotate-left",
+                    "style": "btn-default btn-xs",
+                    "onClick": "actions.resetFilter()"
+                }]
+            }];
+            $scope.filterSchema = {
+                "type": "object",
+                "properties": {
+                    "userbranch": {
+                        'title': "BRANCH"
+                    },
+                    "usercentre": {
+                        'title': "CENTRE"
+                    }
 
-	$scope.listNextAccessLevelItems($scope.filterAccessLevels[0], '', 0);
+                }
+            };
+            $scope.filterModel = {
+                "userbranch": null,
+				"usercentre": null,
+				"currentUserAccessLevel": $scope.currentUserAccessLevel
+			};
 
-	var initialize = function() {
-		if (_.isObject(pageData)) {
-			if (pageData.filter) {
-				tabData.filter = pageData.filter;
-			}
-			if (pageData.user && pageData.user.name) {
-				$scope.filterUsername = pageData.user.name;
-			}
-		}
-		$scope.ResultDataSet = [];
-		BIReports.reportTabList(tabData).$promise.then(function(response) {
-			$scope.TabDefinition = response;
-			$timeout(function() {
-				$scope.active = 0;
-			});
-		}).finally(function() {
-			PageHelper.hideLoader();
-		});
-	};
+            $scope.formHelper = formHelper;
+            $scope.filterFormName = "filterForm";
+            $scope.filterFormOptions = {};
+            $scope.filterActions = {
+                applyFilter: function () {
+                    pageData = {
+                        filter: [$scope.filterModel.userbranch, $scope.filterModel.usercentre].join(':')
+                    }
+                    initialize();
+                },
+                resetFilter: function () {
+                    pageData = {};
+                    initialize();
+                }
+            };
+			$scope.initFilterForm = function (model, form, formCtrl) {};
 
-	$scope.applyFilter = function() {
-		var user = $scope.fSelect[$scope.filterAccessLevels.length - 1];
-		var _f = [];
-		for (var i = 0; i < $scope.fSelect.length; i++) {
-			_f.push($scope.fSelect[i].id);
-		}
+            $scope.onTabLoad = function (menuId, activeindex) {
+                if (!$scope.ResultDataSet[activeindex]) {
+                    PageHelper.showLoader();
+                    var reportData = {
+                        "menu_id": menuId,
+                        "user_id": userName
+                    };
+                    if (pageData && pageData.filter) {
+                        reportData.filter = pageData.filter;
+                    }
+                    BIReports.reportDataList(reportData).$promise.then(function (response) {
+                        var dataset = [];
 
-		pageData = {
-			filter: _f.join(':'),
-			user: user
-		};
-		initialize();
-	}
+                        angular.forEach(response.ReportData, function (value, key) {
+                            dataset[key] = value;
+                            /*if(value.ReportType == 'CHART') {
+                            	dataset[key] = value;
+                            } else {
+                            	dataset[key] = value;
+                            }*/
+                        });
+                        $scope.ResultDataSet[activeindex] = dataset;
+                        PageHelper.hideLoader();
+                    });
+                }
+            };
 
-	$scope.resetFilter = function() {
-		for (i = 0; i < $scope.filterAccessLevels.length; i++) $scope.fSelect[i] = [];
+            var drilldownBodyHtml =
+                '<div ng-show="model.$showLoader" class="text-center">Loading...</div>' +
+                '<div ng-hide="model.$showLoader || model.error" style="overflow:auto">' +
+                '<table class="table table-striped table-responsive">' +
+                '<tr>' +
+                '<th ng-repeat="(key, value) in model.drilldownReport[0]" ng-hide="key.startsWith(\'__\')">{{key|translate}}</th>' +
+                '</tr>' +
+                '<tr ng-repeat="row in model.drilldownReport">' +
+                '<td ng-repeat="(key, value) in row" ng-hide="key.startsWith(\'__\')">{{value}}</td>' +
+                '</tr>' +
+                '</table>' +
+                '</div>' +
+                '<div ng-show="model.error">Error: {{model.error.error}}</div>';
 
-		pageData = {};
-		initialize();
-	}
+            $scope.reportDrilldown = function (report, record) {
+                var drilldownModel = {
+                    report: report,
+                    $showLoader: true
+                };
+                var drilldownRequest = {
+                    user: userName,
+                    reportId: report.unique_id,
+                    record: record
+                };
+                if (pageData && pageData.filter) {
+                    drilldownRequest.filter = pageData.filter;
+                }
+                BIReports.reportDrilldown(drilldownRequest).$promise.then(function (response) {
+                    drilldownModel.drilldownReport = response.drilldownReport;
+                }, function (error) {
+                    drilldownModel.error = error;
+                }).finally(function () {
+                    drilldownModel.$showLoader = false;
+                });
+                irfSimpleModal('{{model.report.TableTitle}}', drilldownBodyHtml, drilldownModel, {
+                    size: 'lg'
+                });
+            };
 
-	$scope.onTabLoad = function(menuId, activeindex) {
-		if(!$scope.ResultDataSet[activeindex]) {
-			PageHelper.showLoader();
-			var reportData = {"menu_id":menuId, "user_id": userName};
-			if (pageData && pageData.filter) {
-				reportData.filter = pageData.filter;
-			}
-			BIReports.reportDataList(reportData).$promise.then(function(response) {
-				var dataset = [];
-				
-				angular.forEach(response.ReportData, function(value, key) {
-					dataset[key] = value;
-					/*if(value.ReportType == 'CHART') {
-						dataset[key] = value;
-					} else {
-						dataset[key] = value;
-					}*/
-				});
-				$scope.ResultDataSet[activeindex] = dataset;
-				PageHelper.hideLoader();
-			});
-		}
-	};
+            initialize();
 
-	var drilldownBodyHtml =
-		'<div ng-show="model.$showLoader" class="text-center">Loading...</div>'+
-		'<div ng-hide="model.$showLoader || model.error" style="overflow:auto">'+
-			'<table class="table table-striped table-responsive">'+
-				'<tr>'+
-					'<th ng-repeat="(key, value) in model.drilldownReport[0]" ng-hide="key.startsWith(\'__\')">{{key|translate}}</th>'+
-				'</tr>'+
-				'<tr ng-repeat="row in model.drilldownReport">'+
-					'<td ng-repeat="(key, value) in row" ng-hide="key.startsWith(\'__\')">{{value}}</td>'+
-				'</tr>'+
-			'</table>'+
-		'</div>'+
-		'<div ng-show="model.error">Error: {{model.error.error}}</div>';
-
-	$scope.reportDrilldown = function(report, record) {
-		var drilldownModel = {
-			report: report,
-			$showLoader: true
-		};
-		var drilldownRequest = {
-			user: userName,
-			reportId: report.unique_id,
-			record: record
-		};
-		if (pageData && pageData.filter) {
-			drilldownRequest.filter = pageData.filter;
-		}
-		BIReports.reportDrilldown(drilldownRequest).$promise.then(function(response) {
-			drilldownModel.drilldownReport = response.drilldownReport;
-		}, function(error) {
-			drilldownModel.error = error;
-		}).finally(function() {
-			drilldownModel.$showLoader = false;
-		});
-		irfSimpleModal('{{model.report.TableTitle}}', drilldownBodyHtml, drilldownModel, {size: 'lg'});
-	};
-
-	initialize();
-
-}]);
+        }
+    ]);
