62b5783631bd78cc311867ce32b86aac2922f109
Harish.D <harish.d@dvara.com>
2018-09-25 20:00:21 +0530

    kgfs loan booking

--- source/dev-www/js/irf-env.js
+++ source/dev-www/js/irf-env.js
@@ -1,14 +1,14 @@
 
 
-// var irf = irf || {};
-// irf.BASE_URL = 'http://devkinara.perdix.in:8080/perdix-server';
-// irf.MANAGEMENT_BASE_URL = 'http://devkinara.perdix.in:8081/management';
-// irf.FORM_DOWNLOAD_URL = 'http://devkinara.perdix.in:8080/forms/formPrint.jsp';
-// irf.BI_BASE_URL = "http://devkinara.perdix.in:8081/bi-dev";
-// irf.PSYCHOMETRIC_BASE_URL = "http://devkinara.perdix.in:8080/psychometric-8.2";
-// irf.AUDIT_API_URL = 'http://devkinara.perdix.in:84';
-// irf.ANALYTICS_WEB_URL = 'http://34.202.113.237:12345';
-// irf.ANALYTICS_API_URL = 'http://34.202.113.237:1337';
+var irf = irf || {};
+irf.BASE_URL = 'http://devkinara.perdix.in:8080/perdix-server';
+irf.MANAGEMENT_BASE_URL = 'http://devkinara.perdix.in:8081/management';
+irf.FORM_DOWNLOAD_URL = 'http://devkinara.perdix.in:8080/forms/formPrint.jsp';
+irf.BI_BASE_URL = "http://devkinara.perdix.in:8081/bi-dev";
+irf.PSYCHOMETRIC_BASE_URL = "http://devkinara.perdix.in:8080/psychometric-8.2";
+irf.AUDIT_API_URL = 'http://devkinara.perdix.in:84';
+irf.ANALYTICS_WEB_URL = 'http://34.202.113.237:12345';
+irf.ANALYTICS_API_URL = 'http://34.202.113.237:1337';
 
 // var irf = irf || {};
 // irf.BASE_URL = 'http://52.55.177.70:82/perdix-server_p2uat';
@@ -231,10 +231,10 @@ irf.BI_BASE_URL = "http://34.202.145.163:8081/bi";*/
 // irf.CAMS_EKYC_INTEG_URL = "{{cams.ekyc-integ-web}}";
 
 
-var irf = irf || {};
-irf.BASE_URL = 'https://uat.pahal.perdix.co.in:8081/perdix-server';
-irf.MANAGEMENT_BASE_URL = 'https://uat.pahal.perdix.co.in:8081/management';
-irf.FORM_DOWNLOAD_URL = 'https://uat.pahal.perdix.co.in:8081/forms/formPrint.jsp';
-irf.BI_BASE_URL = "https://uat.pahal.perdix.co.in:8081/bi";
-irf.PSYCHOMETRIC_BASE_URL = "http://uat.pahal.perdix.co.in:8080/psychometric";
+// var irf = irf || {};
+// irf.BASE_URL = 'https://uat.pahal.perdix.co.in:8081/perdix-server';
+// irf.MANAGEMENT_BASE_URL = 'https://uat.pahal.perdix.co.in:8081/management';
+// irf.FORM_DOWNLOAD_URL = 'https://uat.pahal.perdix.co.in:8081/forms/formPrint.jsp';
+// irf.BI_BASE_URL = "https://uat.pahal.perdix.co.in:8081/bi";
+// irf.PSYCHOMETRIC_BASE_URL = "http://uat.pahal.perdix.co.in:8080/psychometric";
 
