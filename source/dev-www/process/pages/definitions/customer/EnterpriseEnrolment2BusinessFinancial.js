3d9da7b6783b02268f1a03ad00a06aaaaa2ba970
Harish.D <harish.d@dvara.com>
2018-10-08 16:34:37 +0530

    #Fixed Datatable minor errors

--- source/dev-www/process/pages/definitions/customer/EnterpriseEnrolment2BusinessFinancial.js
+++ source/dev-www/process/pages/definitions/customer/EnterpriseEnrolment2BusinessFinancial.js
@@ -581,7 +581,7 @@ irf.pageCollection.factory(irf.page("customer.EnterpriseEnrolment2BusinessFinanc
                         var mandatoryExpenses = null;
                         var mandatoryBankStatements = null;
                         var mandatoryIncomeThroughSales = null;
-                        if (model.customer.customerBankAccounts.length > 0 && isError == false) {
+                        if (model.customer.customerBankAccounts.length > 0 && isError == false && typeof model.customer.customerBankAccounts !="undefined") {
                             mandatoryBankStatements = _.findIndex(model.customer.customerBankAccounts, function (arr) {
                                 if (arr.bankStatements.length < 6) {
                                     isError = true;
@@ -608,7 +608,7 @@ irf.pageCollection.factory(irf.page("customer.EnterpriseEnrolment2BusinessFinanc
                                 })
                             }
                         }
-                        if (model.customer.incomeThroughSales.length > 0 && isError == false) {
+                        if (model.customer.incomeThroughSales.length > 0 && isError == false && model.customer.incomeThroughSales !="undefined") {
                             mandatoryIncomeThroughSales = _.findIndex(model.customer.incomeThroughSales, function (arr) {
                                 if (arr.buyerName != null && arr.incomeType != null && arr.invoiceType != null) {
                                     if (arr.incomeType === "Invoice") {
@@ -626,7 +626,7 @@ irf.pageCollection.factory(irf.page("customer.EnterpriseEnrolment2BusinessFinanc
                                 }
                             })
                         };
-                        if (model.customer.rawMaterialExpenses.length > 0 && isError == false) {
+                        if (model.customer.rawMaterialExpenses.length > 0 && isError == false && model.customer.rawMaterialExpenses !="undefined") {
                             mandatoryPurchases = _.findIndex(model.customer.rawMaterialExpenses, function (arr) {
                                 if (arr.vendorName != null && arr.rawMaterialType != null && arr.amount != null) {
                                     if (arr.rawMaterialType === "Invoice") {
