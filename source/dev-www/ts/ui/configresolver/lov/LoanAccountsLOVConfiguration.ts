019e6e15f9b55422b4beda9412ecb5a84d22b95a
preethi.mahesh <preethi.magesh@dvara.com>
2018-10-15 18:37:37 +0530

    LoanAccountLovConfiguration

--- source/dev-www/ts/ui/configresolver/lov/LoanAccountsLOVConfiguration.ts
+++ source/dev-www/ts/ui/configresolver/lov/LoanAccountsLOVConfiguration.ts
@@ -21,6 +21,7 @@ export class LoanAccountsLOVConfiguration extends LOVElementConfiguration {
     };
 
     getListDisplayItem: Function =  function(data, index) {
+        console.log(data);
         return [
             'Account Number : ' + data.accountNumber,
             'Loan Amount : ' + data.loanAmount
@@ -40,7 +41,7 @@ export class LoanAccountsLOVConfiguration extends LOVElementConfiguration {
                 if(model.payment.paymentPurpose == 'Loan Disbursement') {
                     model.payment.amount = res.loanAmount;
                 }
-                if(res.disbursementSchedules.length > 0) {                    
+                if(model.pageConfig.DefaultBeneficaryParty == 'NONE' && res.disbursementSchedules.length > 0) {                    
                     model.payment.beneficiaryAccountNumber = res.disbursementSchedules[0].customerAccountNumber;
                     model.payment.beneficiaryIfsc = res.disbursementSchedules[0].ifscCode;
                     model.payment.beneficiaryBankName = res.disbursementSchedules[0].customerBankName;
