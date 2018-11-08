2e28a52934e3517979d59fea240c3ec1db38ad76
prakash.d@dvara.com <prakash.d@dvara.com>
2018-11-08 18:43:08 +0530

    # Added
    LTV value calculation changes are added

--- source/dev-www/ts/domain/model/loan/policy/CollateralFieldPolicy.ts
+++ source/dev-www/ts/domain/model/loan/policy/CollateralFieldPolicy.ts
@@ -9,11 +9,13 @@ import {RepositoryIdentifiers} from "../../../shared/RepositoryIdentifiers";
 import EnrolmentProcessFactory = require("../../customer/EnrolmentProcessFactory");
 import {CustomerTypes} from "../../customer/Customer";
 import {LoanCustomerRelationTypes} from "../LoanCustomerRelation";
+import AngularResourceService = require("../../../../infra/api/AngularResourceService");
 import Collateral = require("../Collateral");
 
 
 export interface CollateralFieldPolicyArgs {
     postStage: string;
+    ltv: boolean;
 }
 export class CollateralFieldPolicy extends IPolicy<LoanProcess> {
 
@@ -23,26 +25,77 @@ export class CollateralFieldPolicy extends IPolicy<LoanProcess> {
     }
 
     run(loanProcess: LoanProcess): Observable<LoanProcess> {
-        console.log(loanProcess);
-        let col;
-        if(_.hasIn(loanProcess.loanAccount, "collateral") && loanProcess.loanAccount.collateral.length == 0) {
-            col = new Collateral();
-            let vehicleDetails = loanProcess.loanAccount.vehicleLoanDetails;
-            col.collateralType = 'Vehicle';
-            col.collateralCategory = 'Vehicle';
-            col.quantity = 1;
-            col.electricityAvailable = vehicleDetails.segment;
-            col.collateralDescription = vehicleDetails.category;
-            col.udf1 = vehicleDetails.yearOfManufacture;
-
-            col.manufacturer = vehicleDetails.make;
-            col.modelNo = vehicleDetails.vehicleModel;
-            col.serialNo = vehicleDetails.registrationNumber;
-            // col.collateralValue = vehicleDetails.registrationNumber;
-            col.machineOld = (vehicleDetails.vehicleType == 'Used') ? 'true': 'false';
-            loanProcess.loanAccount.collateral.push(col); 
-        } 
+        let Queries = AngularResourceService.getInstance().getNGService("Queries");
+        let ltv = null;
         
-       return Observable.of(loanProcess);
+        if(this.args && this.args.ltv && this.args.ltv == true) {
+            return Observable.fromPromise(Queries.getLTVValue({
+                "make":loanProcess.loanAccount.vehicleLoanDetails.make,
+                "vehicle_model":loanProcess.loanAccount.vehicleLoanDetails.vehicleModel,
+                "registration_number":loanProcess.loanAccount.vehicleLoanDetails.registrationNumber,
+                "year_of_manufacture":loanProcess.loanAccount.vehicleLoanDetails.yearOfManufacture
+            })).map((res:any)=>{
+                let vintageValue = null;
+                if(res.body.length>0 && res.body[0].value) {
+                    vintageValue = res.body[0].value;
+                }
+
+                if(loanProcess.loanAccount.vehicleLoanDetails.vehicleType == 'New') {
+                    ltv = loanProcess.loanAccount.vehicleLoanDetails.price;
+                } else if(loanProcess.loanAccount.vehicleLoanDetails.vehicleType == 'Used') {
+                    ltv = _.min([
+                            loanProcess.loanAccount.vehicleLoanDetails.insuredDeclaredValue, 
+                            loanProcess.loanAccount.vehicleLoanDetails.currentMarketValue, 
+                            loanProcess.loanAccount.vehicleLoanDetails.price, 
+                            vintageValue
+                        ]);
+                } else {
+                    ltv = _.min([
+                            loanProcess.loanAccount.vehicleLoanDetails.insuredDeclaredValue, 
+                            loanProcess.loanAccount.vehicleLoanDetails.currentMarketValue, 
+                            vintageValue
+                        ]);
+                }
+
+                let col;               
+                if(_.hasIn(loanProcess.loanAccount, "collateral") && loanProcess.loanAccount.collateral.length == 0) {
+                    col = new Collateral();
+                    let vehicleDetails = loanProcess.loanAccount.vehicleLoanDetails;
+                    col.collateralType = 'Vehicle';
+                    col.collateralCategory = 'Vehicle';
+                    col.quantity = 1;
+                    col.electricityAvailable = vehicleDetails.segment;
+                    col.collateralDescription = vehicleDetails.category;
+                    col.udf1 = vehicleDetails.yearOfManufacture;
+                    col.manufacturer = vehicleDetails.make;
+                    col.modelNo = vehicleDetails.vehicleModel;
+                    col.serialNo = vehicleDetails.registrationNumber;
+                    col.collateralValue = ltv;
+                    col.machineOld = (vehicleDetails.vehicleType == 'Used') ? 'true': 'false';
+                    loanProcess.loanAccount.collateral.push(col); 
+                }
+                return loanProcess;
+           });
+        } else {
+            let col;
+            if(_.hasIn(loanProcess.loanAccount, "collateral") && loanProcess.loanAccount.collateral.length == 0) {
+                col = new Collateral();
+                let vehicleDetails = loanProcess.loanAccount.vehicleLoanDetails;
+                col.collateralType = 'Vehicle';
+                col.collateralCategory = 'Vehicle';
+                col.quantity = 1;
+                col.electricityAvailable = vehicleDetails.segment;
+                col.collateralDescription = vehicleDetails.category;
+                col.udf1 = vehicleDetails.yearOfManufacture;
+
+                col.manufacturer = vehicleDetails.make;
+                col.modelNo = vehicleDetails.vehicleModel;
+                col.serialNo = vehicleDetails.registrationNumber;
+                col.machineOld = (vehicleDetails.vehicleType == 'Used') ? 'true': 'false';
+                loanProcess.loanAccount.collateral.push(col); 
+            }
+            
+            return Observable.of(loanProcess);
+       }
     }
 }
