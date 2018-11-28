f33bc6a50297f885342bb487ca1e25abd10c566c
Stalin Solomon <stalin.solomon@dvara.com>
2018-11-28 18:56:08 +0530

    portfolio analytics tab added to all kinara pages

--- source/dev-www/process/pages/definitions/loans/individual/screening/CreditCommitteeReview.js
+++ source/dev-www/process/pages/definitions/loans/individual/screening/CreditCommitteeReview.js
@@ -73,17 +73,21 @@ irf.pageCollection.factory(irf.page('loans.individual.screening.CreditCommitteeR
                 minimum: 1,
                 maximum: 1,
                 order: 80
+            }, {
+                pageName: 'loans.individual.screening.detail.SummaryView',
+                title: 'SummaryView',
+                pageClass: 'summaryView',
+                minimum: 1,
+                maximum: 1,
+                order: 5
+            }, {
+                pageName: 'loans.individual.screening.detail.PortfolioAnalyticsView',
+                title: 'Portfolio Analytics',
+                pageClass: 'portfolio-analytics',
+                minimum: 1,
+                maximum: 1,
+                order: 90
             }];
-            if (SessionStore.getGlobalSetting('siteCode') != 'IREPDhan' || SessionStore.getGlobalSetting('siteCode') == 'IREPDhan') {
-                definition.push({
-                    pageName: 'loans.individual.screening.detail.SummaryView',
-                    title: 'SummaryView',
-                    pageClass: 'summaryView',
-                    minimum: 1,
-                    maximum: 1,
-                    order: 5
-                });
-            }
             return definition;
         };
         return {
@@ -289,6 +293,12 @@ irf.pageCollection.factory(irf.page('loans.individual.screening.CreditCommitteeR
                                     }
                                 });
 
+                                $this.bundlePages.push({
+                                    pageClass: 'portfolio-analytics',
+                                    model: {
+                                        loanId: bundleModel.loanId
+                                    }
+                                });
 
 
                                 deferred.resolve();
