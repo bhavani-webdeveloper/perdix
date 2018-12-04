7234fb951c252909e093e4dfedf41814ee8020b3
Dvara_Arpit <arpit.agarwall@dvara.com>
2018-12-04 18:54:49 +0530

    repay issues

--- source/dev-www/process/pages/definitions/loans/individual/collections/TransactionAuthorization.js
+++ source/dev-www/process/pages/definitions/loans/individual/collections/TransactionAuthorization.js
@@ -529,7 +529,7 @@ irf.pageCollection.factory(irf.page("loans.individual.collections.TransactionAut
                         }
 
                         if(model._transAuth.transactionName == 'PenalInterestPayment'){
-                            model._transAuth.bookedNotDuePenalInterest = model.loanAccount.repaymentAmount || 0;
+                            model._transAuth.bookedNotDuePenalInterest = model._transAuth.repaymentAmount || 0;
                         }
 
                         if(model._transAuth.transactionName == 'Fee Payment'){
