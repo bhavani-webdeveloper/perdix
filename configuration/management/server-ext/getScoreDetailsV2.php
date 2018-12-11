b58b34053feaed23aa3db5fb200f6e8063cfe8bd
najam safar <najam.s@gmail.com>
2018-12-11 23:01:28 +0530

    added bearer prefix to token

--- configuration/management/server-ext/getScoreDetailsV2.php
+++ configuration/management/server-ext/getScoreDetailsV2.php
@@ -32,7 +32,7 @@ if (isset($_GET)) {
 
     $error_log = "";
     $CustomerLoanId = $_GET['LoanId'];
-    $authInfo = $_GET['auth_token'];
+    $authInfo = 'Bearer '.$_GET['auth_token'];
     $SessionUserName = "admin";
     $userInfo = $perdixService->accountInfo($authInfo);
     $partnerCode = $userInfo['partnerCode'];
