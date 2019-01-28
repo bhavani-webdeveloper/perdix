20620c449e65fd0e1ad94328c65a466ad6f0dde6
Akshay Yadav <akshayyadav@Akshays-MacBook-Air.local>
2019-01-28 12:49:27 +0530

    Revert "Modifying given groovy file under kgfs folder"
    This reverts commit 15e8028a0e93700057f614099f80d97ae07abd80.

--- configuration/perdix-server-scripts/future generali.groovy
+++ configuration/perdix-server-scripts/future generali.groovy
@@ -2,35 +2,14 @@ import java.time.*;
 import java.util.Date;
 
 			policyNo =null;
-			certificateNo=null;
 			firstPartSequenceNo=null;
-			String secondPartSequenceNo =null;
-			String insurer="Future Generali India";
 			bankMaster = bankMasterRepository.findById(customer.getCustomerBankId().longValue());
 			Date date = Date.from(insurancePolicyDetailsDTO.getPurchaseDate().atStartOfDay(ZoneId.systemDefault()).toInstant());
 			master = pAISequenceMasterRepository.findById(bankMaster.getId());
 			int days = (int)((date.getTime() - master.getResetDate().getTime()) / (1000 * 60 * 60 * 24));
-			
-insuranceSequenceList = insuranceSequenceDetailsRepository.findByBankIdAndDateAndInsurer(bankMaster.getId().intValue(),insurancePolicyDetailsDTO.getPurchaseDate(),"PAI-"+insurer);
-
-            if(!insuranceSequenceList.isEmpty()){
-                insuranceSequenceDetails = insuranceSequenceList.get(0);
-                insuranceSequenceDetails.setCounter(insuranceSequenceList.get(0).getCounter()+1);
-            }else{
-                insuranceSequenceDetails = sequenceDetailsCreateObj;
-                insuranceSequenceDetails.setBankId(bankMaster.getId().intValue());
-                insuranceSequenceDetails.setDate(insurancePolicyDetailsDTO.getPurchaseDate());
-                insuranceSequenceDetails.setInsuranceType("PAI");
-                insuranceSequenceDetails.setInsurer("PAI-"+insurer);
-                insuranceSequenceDetails.setCounter(1);
-            }
-			insuranceSequenceDetailsRepository.saveAndFlush(insuranceSequenceDetails);
-			
 			int value = 8-bankMaster.getPaiCoiCode().length();
 			firstPartSequenceNo = stringLPad(master.getSequenceStartValue()+days+"", value, '0');
-			secondPartSequenceNo = stringLPad(insuranceSequenceDetails.getCounter()+"", 5, '0');
 			policyNo = new Date().getYear()+1900+"/"+bankMaster.getPaiCoiCode()+firstPartSequenceNo+"/"+"FGP";
-			certificateNo = bankMaster.getPaiCoiCode()+firstPartSequenceNo+"/"+secondPartSequenceNo;
 
 
 
