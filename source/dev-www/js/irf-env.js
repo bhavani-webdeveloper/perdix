3a8f500a842147f6517d678fe57103f3612cef73
manjunath <manjunathappi68@gmail.com>
2019-02-20 16:36:06 +0530

    env

--- source/dev-www/js/irf-env.js
+++ source/dev-www/js/irf-env.js
@@ -1,13 +1,13 @@
-// var irf = irf || {};
-// irf.BASE_URL = 'http://devkinara.perdix.in:8080/perdix-server';
-// irf.MANAGEMENT_BASE_URL = 'http://devkinara.perdix.in:8081/management';
-// irf.FORM_DOWNLOAD_URL = 'http://devkinara.perdix.in:8080/forms/formPrint.jsp';
-// irf.BI_BASE_URL = "http://devkinara.perdix.in:8081/bi-dev";
-// irf.PSYCHOMETRIC_BASE_URL = "http://devkinara.perdix.in:8080/psychometric-8.2";
-// irf.AUDIT_API_URL = 'http://devkinara.perdix.in:84';
-// irf.ANALYTICS_WEB_URL = 'http://34.202.113.237:12345';
-// irf.ANALYTICS_API_URL = 'http://34.202.113.237:1337';
-// irf.FWR_BASE_URL = 'http://perdix.kgfs.co.in/fwr_dev';
+var irf = irf || {};
+irf.BASE_URL = 'http://devkinara.perdix.in:8080/perdix-server';
+irf.MANAGEMENT_BASE_URL = 'http://devkinara.perdix.in:8081/management';
+irf.FORM_DOWNLOAD_URL = 'http://devkinara.perdix.in:8080/forms/formPrint.jsp';
+irf.BI_BASE_URL = "http://devkinara.perdix.in:8081/bi-dev";
+irf.PSYCHOMETRIC_BASE_URL = "http://devkinara.perdix.in:8080/psychometric-8.2";
+irf.AUDIT_API_URL = 'http://devkinara.perdix.in:84';
+irf.ANALYTICS_WEB_URL = 'http://34.202.113.237:12345';
+irf.ANALYTICS_API_URL = 'http://34.202.113.237:1337';
+irf.FWR_BASE_URL = 'http://perdix.kgfs.co.in/fwr_dev';
 
 // var irf = irf || {};
 // irf.BASE_URL = 'http://52.55.177.70:82/perdix-server_p2uat';
@@ -256,9 +256,9 @@
 
 
 // ------------------------------ Shramsarathi sit--------------------------------------
-var irf = irf || {};
-irf.BASE_URL = 'http://sit.perdix.co.in:8080/sit_shramsarathi_perdix-server';
-irf.MANAGEMENT_BASE_URL = 'http://sit.perdix.co.in:8081/sit_shramsarathi_management';
-irf.FORM_DOWNLOAD_URL = 'http://sit.perdix.co.in:8080/sit_shramsarathi_forms/formPrint.jsp';
-irf.BI_BASE_URL = "http://sit.perdix.co.in:8081/sit_shramsarathi_bi";
-irf.PSYCHOMETRIC_BASE_URL = "http://sit.perdix.co.in:8080/sit_shramsarathi_psychometric";
+// var irf = irf || {};
+// irf.BASE_URL = 'http://sit.perdix.co.in:8080/sit_shramsarathi_perdix-server';
+// irf.MANAGEMENT_BASE_URL = 'http://sit.perdix.co.in:8081/sit_shramsarathi_management';
+// irf.FORM_DOWNLOAD_URL = 'http://sit.perdix.co.in:8080/sit_shramsarathi_forms/formPrint.jsp';
+// irf.BI_BASE_URL = "http://sit.perdix.co.in:8081/sit_shramsarathi_bi";
+// irf.PSYCHOMETRIC_BASE_URL = "http://sit.perdix.co.in:8080/sit_shramsarathi_psychometric";
