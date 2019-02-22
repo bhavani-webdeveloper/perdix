b00daf3cd44bf5639d14963764fc1168eaa2a2f5
raveena.sindhu <raveena.sindhu@dvara.com>
2019-02-22 13:25:42 +0530

    maitreya change type of business loaction photo

--- source/dev-www/process/pages/definitions/base/dashboard/loans/individual/customer/EnterpriseEnrolment2.js
+++ source/dev-www/process/pages/definitions/base/dashboard/loans/individual/customer/EnterpriseEnrolment2.js
@@ -42,9 +42,13 @@ define(['perdix/domain/model/customer/EnrolmentProcess'], function(EnrolmentProc
                         "condition": "model.customer.id",
                     },
                     "EnterpriseInformation.photoImageId":{
+                        "key": "customer.photoImageId",
+                        "title": "BUSINESS_LOCATION_PHOTO",
                         "type": "file",
                         "fileType": "application/pdf",
+                        "category": "CustomerEnrollment",
                         "using": "scanner"
+
                     },
                     "EnterpriseInformation.referredName":{
                         "condition": "model.customer.enterprise.referredBy == 'Channel Partner'||model.customer.enterprise.referredBy =='Peer Referral'||model.customer.enterprise.referredBy =='Known supply chain'",
