80c1be633a23c14664c0e5b4869e7fcb7ecf81b8
Stalin Solomon <stalin.solomon@dvara.com>
2019-02-27 20:52:22 +0530

    encore log

--- configuration/management/server-ext/serverError.php
+++ configuration/management/server-ext/serverError.php
@@ -4,27 +4,68 @@
     use Illuminate\Database\Capsule\Manager as DB;
 
     $base = '//'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']);
-    $id = $_GET['id'];
-    if (!isset($id) &&empty($id)) {
+    if (isset($_GET['encore'])) {
+        if (empty($_GET['encore'])) {
+            header("Location: $base/serverError.php?encore=100");
+            die();
+        }
+        $encore = $_GET['encore'];
+        // CREATE VIEW `encoresite` AS select '/opt/mount_point/encoresite' AS `path`
+        $encoresite = DB::table('encoresite')->first();
+        $encore_log = shell_exec('tail -n '.$encore.' '.$encoresite->path.'/logs/encore.log');
+        header("Content-Type: text/html");
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <title>Encore Server Log</title>
+    <style>
+        body {
+            font-family: 'Open Sans', sans-serif;
+            padding: 15px;
+        }
+        .subject {
+            padding-top: 12px;
+        }
+        .request-time {
+            font-size: 30px;
+        }
+        .content {
+            padding-top: 6px;
+        }
+        span.pull-right>span {
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body>
+    <div class="content"><pre><?= $encore_log ?></pre></div>
+</body>
+</html><?php
+
+    } else if (!isset($_GET['id']) && empty($_GET['id'])) {
         $max_id = DB::table('email_request')->max('id');
         header("Location: $base/serverError.php?id=$max_id");
         die();
-    }
-    header("Content-Type: text/html");
-    $error = DB::table('email_request')->where('id', $id)->first();
-    // $file = DB::table('file_info')->where('file_id', $error->path)->first();
-    $url = getenv(PERDIX_V8_BASE_URL);
-    if (empty($error->path))
-    {   
-        
-        echo("<h4><center>OOPS..SOMETHING WENT WRONG ".json_decode('"\uD83D\uDE3F"')."</center><h4>");
-        
-    }
-    else{
-    $content = file_get_contents(getenv(PERDIX_V8_BASE_URL).'/api/stream/'.$error->path);
-    }
-    $previous = $base.'/serverError.php?id='.($id-1);
-    $next = $base.'/serverError.php?id='.($id+1);
+    } else {
+        $id = $_GET['id'];
+        header("Content-Type: text/html");
+        $error = DB::table('email_request')->where('id', $id)->first();
+        // $file = DB::table('file_info')->where('file_id', $error->path)->first();
+        $url = getenv(PERDIX_V8_BASE_URL);
+        if (empty($error->path)) {
+            echo("<h4><center>OOPS..SOMETHING WENT WRONG ".json_decode('"\uD83D\uDE3F"')."</center><h4>");
+        } else {
+            $content = file_get_contents(getenv(PERDIX_V8_BASE_URL).'/api/stream/'.$error->path);
+        }
+        $previous = $base.'/serverError.php?id='.($id-1);
+        $next = $base.'/serverError.php?id='.($id+1);
+
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
@@ -65,4 +106,5 @@
     <div class="subject"><?= $error->subject ?><span class="pull-right">Number of failures: <span><?= $error->number_of_failures ?></span></span></div>
     <div class="content"><?= $content ?></div>
 </body>
-</html>
+</html><?php
+    }
