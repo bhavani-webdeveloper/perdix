b3bb8f891bd69b04ea136c35dc10692ff5f333ff
Hemanth <manjunathappi68@gmail.com>
2019-03-04 13:06:02 +0530

    update in lead generation gender and migrante type

--- source/dev-www/process/pages/definitions/shramsarathi/dashboard/lead/LeadGeneration.js
+++ source/dev-www/process/pages/definitions/shramsarathi/dashboard/lead/LeadGeneration.js
@@ -60,7 +60,7 @@ function(LeadProcess, AngularResourceService) {
                     "leadProfile.leadDetails": {
                         "orderNo" : 15,
                     },
-                    "leadProfile.leadDetails.individualDetails.gender": {
+                    "leadProfile.individualDetails.gender": {
                         "required": true
                     },
                     "leadProfile.individualDetails.existingApplicant":{
@@ -581,6 +581,12 @@ function(LeadProcess, AngularResourceService) {
                     submit: function(model, form, formName) {
                         $log.info("Inside submit()");
                         PageHelper.showLoader();
+                        
+                        formCtrl.scope.$broadcast('schemaFormValidate');
+					    if(!formCtrl.$valid){
+                            PageHelper.showProgress('form-error', 'Your form have errors. Please fix them.',5000);
+                            return
+                        }
 
                         var reqData = _.cloneDeep(model);
                         var centres = formHelper.enum('centre').data;
