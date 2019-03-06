4c831d8c48617cd9c073bf42340f9fa582a13b67
bibhuti <manjunathappi68@gmail.com>
2019-03-06 17:28:11 +0530

    Updated

--- source/dev-www/process/pages/definitions/shramsarathi/dashboard/loans/individual/screening/detail/IndividualEnrollmentView.js
+++ source/dev-www/process/pages/definitions/shramsarathi/dashboard/loans/individual/screening/detail/IndividualEnrollmentView.js
@@ -301,7 +301,7 @@ define({
     <div class="col-sm-6">
     <i class="fa fa-check-circle text-green" style="font-size:x-large">&nbsp;</i><em class="text-darkgray">{{model.existingCustomerStr}}</em><br>&nbsp;
     </div>
-    <div class="col-sm-3">{{'BRANCH'|translate}}: <strong>{{model.customer.customerBranchName}}</strong></div>
+    <div class="col-sm-3">{{'BRANCH'|translate}}: <strong>{{model.customer.customerBranchId}}</strong></div>
     <div class="col-sm-3">{{'ZONE'|translate}}: <strong>{{model.customer.centreId}}</strong></div>
     `
                     }, {
