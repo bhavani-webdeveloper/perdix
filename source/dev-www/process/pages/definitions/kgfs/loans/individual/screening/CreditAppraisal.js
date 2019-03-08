8b071b32bb431bd7672786ae9c70512e95612219
preethi.mahesh <preethi.magesh@dvara.com>
2019-03-08 13:16:53 +0530

    mel fix

--- source/dev-www/process/pages/definitions/kgfs/loans/individual/screening/CreditAppraisal.js
+++ source/dev-www/process/pages/definitions/kgfs/loans/individual/screening/CreditAppraisal.js
@@ -47,7 +47,7 @@ define(["perdix/domain/model/loan/LoanProcess",
                             title: 'BUSINESS',
                             pageClass: 'business',
                             minimum: 1,
-                            maximum: 0,
+                        maximum: 0,
                             order:40
                         },
                         {
