a9c67bf89a362e1112774cf9e6580a17cd76a131
Harish <harish.d@dvara.com>
2019-03-14 11:32:00 +0530

    <kgfs-sit> enrolled as readonly in customer 360

--- source/dev-www/process/pages/definitions/customer360/EnrollmentProfile.js
+++ source/dev-www/process/pages/definitions/customer360/EnrollmentProfile.js
@@ -1565,7 +1565,7 @@ function($log, Enrollment,Queries, EnrollmentHelper,PagesDefinition, SessionStor
                         {
                             key:"customer.enrolledAs",
                             type:"radios",
-                            //readonly: true
+                            readonly: true
                         },
                         {
                             key:"customer.gender",
