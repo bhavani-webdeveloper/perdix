b854de3ba37d4242708e75cba4dddde8d1ac59d2
Harish <harish.d@dvara.com>
2019-04-10 15:32:26 +0530

    removed irep dhan condition

--- source/dev-www/process/pages/definitions/customer/CustomerSearch.js
+++ source/dev-www/process/pages/definitions/customer/CustomerSearch.js
@@ -80,7 +80,7 @@ function($log, formHelper,filterFilter, Enrollment,Queries,$q,$state, SessionSto
 	                    key: "branch",
 	                    enumCode: "userbranches",
 	                    "type":"select",
-	                    condition: "model.siteCode=='sambandh' || model.siteCode =='IREPDhan'"
+	                    condition: "model.siteCode=='sambandh'"
 	                }, {
 	                    key: "centre",
 	                },{
