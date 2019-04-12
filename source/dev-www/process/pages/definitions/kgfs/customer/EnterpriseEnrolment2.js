f7a71862e1a46d46b3db25613693d6c52c249ff9
Iyyappan Pachaiyappan <iyyappan@irssoft.com>
2019-04-12 11:21:13 +0530

    Enhancements:GST to be made as non-mandatory:KGFS:MEL

--- source/dev-www/process/pages/definitions/kgfs/customer/EnterpriseEnrolment2.js
+++ source/dev-www/process/pages/definitions/kgfs/customer/EnterpriseEnrolment2.js
@@ -603,7 +603,6 @@ define(['perdix/domain/model/customer/EnrolmentProcess', "perdix/domain/model/lo
                                 "key": "customer.enterprise.serviceTaxNumber",
                                 "type": "text",
                                 "title": "GST_NUMBER",
-                                "required":true,
                                 schema: {
                                     "pattern": "^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9]{1}Z[a-zA-Z0-9]{1}$",
                                     "type": ["string", "null"],
