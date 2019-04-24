864805b3c53fa45a4218372b9828e2bd8ac8fc57
Harish <harish.d@dvara.com>
2019-04-24 13:21:56 +0530

    Intellicash telecalling api update

--- source/dev-www/process/models/v1/loans/Telecalling.js
+++ source/dev-www/process/models/v1/loans/Telecalling.js
@@ -2,8 +2,7 @@ irf.models.factory('Telecalling',[
     "$resource","$httpParamSerializer","BASE_URL","searchResource","Upload","$q","PageHelper",
     function($resource,$httpParamSerializer,BASE_URL,searchResource,Upload,$q,PageHelper){
         var endpoint = BASE_URL + '/api/telecallingdetails'; 
-      
-    
+        var endpoint2= BASE_URL = '/api/telecallingdetailsBulkUpdate'; 
         var resource = $resource(endpoint, null, {
             save:{
                 method:'POST',
@@ -12,6 +11,27 @@ irf.models.factory('Telecalling',[
             update:{
                 method:'PUT',
                 url:endpoint
+            },
+            find:{
+              method:'GET',
+              url:endpoint+'/:processType/:processId',  
+            },
+            getByCustomerId:{
+                method:'GET',
+                url: endpoint+'/:id/:customerId'
+            },
+            getById:{
+                method:'GET',
+                url:endpoint+'/:id',
+
+            },
+            bulkUpdate:{
+                method:'PUT',
+                url:endpoint2
+            },
+            deleteById:{
+                method:'PUT',
+                url:endpoint+'/:id'
             }
         });
             return resource;
