1806cd4b320aa93bc0891dfff1e9492b5f6b3053
Harish <harish.d@dvara.com>
2019-04-26 12:59:49 +0530

    removed as it seems repeated

--- source/dev-www/modules/irfcommons/irfFormToggler.js
+++ source/dev-www/modules/irfcommons/irfFormToggler.js
@@ -5,51 +5,51 @@ irf.commons.factory("irfFormToggler", ['$log', '$filter', 'Enrollment', "Enrollm
             var toggleKey = '__FT__' + Utils.generateUUID();
             model[toggleKey] = false;
             var finalform=[];
-            finalform.push({
+            // finalform.push({
 
-                "type": "section",
-                    //"title": forms[i].title,
-                    //"items": forms[i].form,
-                "items": [
-                            {
-                                "title": forms[0].title,
-                                "default":forms[0].default,
-                                "onClick": function(model, form, formName){
-                                    model[toggleKey]=forms[0].title;
-                                }
-                            },
-                            {
-                                "title": forms[1].title,
-                                "default":forms[1].default,
-                                "onClick": function(model, form, formName){
-                                    model[toggleKey]=forms[1].title;
-                                }
-                            }
-                        ],
-                    //"condition": 'model["' + toggleKey + '"] == "' + forms[i].title + '"',
-                "html": '<div class="btn-group btn-group-toggle" style="padding-left: 20px" data-toggle="buttons"><label ng-repeat="tab in form.items" class="btn btn-xs btn-default {{tab.default ? \'active\':\'\'}}" style="margin-bottom:2px; margin-right:2px" ng-click="tab.onClick(model, form, formName)"><input type="radio" autocomplete="off" checked > {{tab.title}}</label></div>'
+            //     "type": "section",
+            //         //"title": forms[i].title,
+            //         //"items": forms[i].form,
+            //     "items": [
+            //                 {
+            //                     "title": forms[0].title,
+            //                     "default":forms[0].default,
+            //                     "onClick": function(model, form, formName){
+            //                         model[toggleKey]=forms[0].title;
+            //                     }
+            //                 },
+            //                 {
+            //                     "title": forms[1].title,
+            //                     "default":forms[1].default,
+            //                     "onClick": function(model, form, formName){
+            //                         model[toggleKey]=forms[1].title;
+            //                     }
+            //                 }
+            //             ],
+            //         //"condition": 'model["' + toggleKey + '"] == "' + forms[i].title + '"',
+            //     "html": '<div class="btn-group btn-group-toggle" style="padding-left: 20px" data-toggle="buttons"><label ng-repeat="tab in form.items" class="btn btn-xs btn-default {{tab.default ? \'active\':\'\'}}" style="margin-bottom:2px; margin-right:2px" ng-click="tab.onClick(model, form, formName)"><input type="radio" autocomplete="off" checked > {{tab.title}}</label></div>'
                 
-                    // "key": toggleKey,
-                    // "type": "radiobutton",
-                    // //"title": "Gender",
-                    // "notitle": true,
-                    // "fieldHtmlClass": "btn-xs",
-                    // "titleMap": [{
-                    //     "name": "Business",
-                    //     "value": "Business",
-                    // }, {
-                    //     "name": "Financials",
-                    //     "value": "Business Financials"
-                    // }],
-                    // "onChange": function(modelValue, form, model){
-                    //     model[toggleKey]=modelValue;
-                    // }
-                    //"enumCode": "businessTabs",
-                    // "schema": {
-                    //     "default": "Business"
-                    // }
+            //         // "key": toggleKey,
+            //         // "type": "radiobutton",
+            //         // //"title": "Gender",
+            //         // "notitle": true,
+            //         // "fieldHtmlClass": "btn-xs",
+            //         // "titleMap": [{
+            //         //     "name": "Business",
+            //         //     "value": "Business",
+            //         // }, {
+            //         //     "name": "Financials",
+            //         //     "value": "Business Financials"
+            //         // }],
+            //         // "onChange": function(modelValue, form, model){
+            //         //     model[toggleKey]=modelValue;
+            //         // }
+            //         //"enumCode": "businessTabs",
+            //         // "schema": {
+            //         //     "default": "Business"
+            //         // }
                 
-                })
+            //     })
             // finalform.push({
             //     "type":"button-tabs",
             //     "tabs": [
