48d33ee67c9cdb4c804d57179c96a5856498ce33
Shahal Tharique <shahalpk@gmail.com>
2019-05-05 02:18:40 +0530

    # Fixed
    1. Upgrade application error coming on opening the app for the first time.

--- source/dev-www/js/irf-config.witfin.js
+++ source/dev-www/js/irf-config.witfin.js
@@ -3,3 +3,4 @@ irf.appConfig.CORPORATE_LOGO = "img/corporate_logo.witfin.png";
 irf.appConfig.FORCE_COLOR = "";
 irf.appConfig.AMS_ENABLED = false;
 irf.appConfig.OFFLINE_USERS = "all"; // ["all"|"mobile"|false]
+irf.appConfig.FILESYSTEM_ENABLED = false;
