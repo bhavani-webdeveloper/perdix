e84dd33615de45df5c815ac723fbf5f1a0b0cf78
Harish <harish.d@dvara.com>
2019-05-27 18:35:07 +0530

    handled promise

--- source/dev-www/process/pages/definitions/workflow/CustomerInfoUpdateInit.js
+++ source/dev-www/process/pages/definitions/workflow/CustomerInfoUpdateInit.js
@@ -25,14 +25,14 @@ define(["perdixConfig/CustomerApprovalConfig"], function (customerApproveConfig)
             }
             var update = function (model, workflowId) {
                 irfProgressMessage.pop("cust-load", "Loading Customer Data...");
-                Workflow.getByID({ id: workflowId }, function (resp, header) {
+                Workflow.getByID({ id: workflowId }).$promise.then((resp)=>{
                     model.workflow = _.cloneDeep(resp);
                     model.UpdatedWorkflow = JSON.parse(model.workflow.temporary);
                     model.old = model.workflow.customer;
                     model.old.biometricCaptured = "Captured";
                     model.old.biometricNotCaptured = "Not Captured";
                     irfProgressMessage.pop("cust-load", "Load Complete", 2000);
-                }, function (resp) {
+                },(err) => {
                     irfProgressMessage.pop("cust-load", "An Error Occurred. Failed to fetch Data", 5000);
                 }).finally(function() {
                     irfProgressMessage.pop("cust-load", "Loading Customer Data...", 10);
