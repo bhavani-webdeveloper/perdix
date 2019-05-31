9e760a6dcae98fe70c85ecc3e2ac1c169a142737
Raveena Sindhu <raveena.sindhu@Raveenas-MacBook-Air.local>
2019-05-31 16:28:18 +0530

    #Upated irf-env.ja

--- source/dev-www/js/irf-env.js
+++ source/dev-www/js/irf-env.js
@@ -1,8 +1,8 @@
 var irf = irf || {};
-irf.BASE_URL = 'http://13.126.44.79:8080/perdix-server';
-irf.MANAGEMENT_BASE_URL = 'http://13.126.44.79:8081/management';
-irf.FORM_DOWNLOAD_URL = 'http://13.126.44.79:8080/forms/formPrint.jsp';
-irf.BI_BASE_URL = "http://13.126.44.79:8081/bi";
-irf.PSYCHOMETRIC_BASE_URL = "http://13.126.44.79:8080/psychometric";
+irf.BASE_URL = 'http://uat.witfin.perdix.co.in:8080/uat_witfin_perdix-server';
+irf.MANAGEMENT_BASE_URL = 'http://uat.witfin.perdix.co.in:8081/uat_witfin_management';
+irf.FORM_DOWNLOAD_URL = 'http://uat.witfin.perdix.co.in:8080/uat_witfin_forms/formPrint.jsp';
+irf.BI_BASE_URL = "http://uat.witfin.perdix.co.in:8081/uat_witfin_bi";
+irf.PSYCHOMETRIC_BASE_URL = "http://uat.witfin.perdix.co.in:8080/uat_witfin_psychometric";
 irf.ANALYTICS_WEB_URL = 'http://34.202.113.237:12345';
 irf.ANALYTICS_API_URL = 'http://34.202.113.237:1337';
