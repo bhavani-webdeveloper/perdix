fedca66570947dc456b888f0a1085859ea774009
Raveena Sindhu <raveena.sindhu@Raveenas-MacBook-Air.local>
2019-05-31 11:17:27 +0530

    #Updated 1.18110 : updated

--- configuration/encore-server-scripts/witfin/FCR5.groovy
+++ configuration/encore-server-scripts/witfin/FCR5.groovy
@@ -3,12 +3,15 @@ import java.time.*
 String[] strTenure = tenure.toString().split(" ");
 int tenureVal = Integer.parseInt(strTenure[0]);
 String tenureUnit = strTenure[1];
+holders = accountRepository.findAccountHoldersByCustomerId(customerId);
+accountProfile = accountRepository.findAccountProfile(holders.get(0).getAccountId());
+LocalDate openDate = accountProfile.getOpenedOnValueDate();
 LocalDate currentDate = bankRepository.findBank().getCurrentWorkingDate();
-Period p = Period.between(currentDate, LocalDate.now());
+Period p = Period.between(openDate, currentDate);
 int periodInMonths = p.getMonths();
 int periodInYears = p.getYears();
 if (periodInYears == 0 && periodInMonths <= 12) {
-	feeCharge = ((amount * new BigDecimal (4))/1000).setScale(2, BigDecimal.ROUND_HALF_UP);
+	feeCharge = ((amount * new BigDecimal (4))/100).setScale(2, BigDecimal.ROUND_HALF_UP);
 } else {
-	feeCharge = ((amount * new BigDecimal (3))/1000).setScale(2, BigDecimal.ROUND_HALF_UP);
+	feeCharge = ((amount * new BigDecimal (3))/100).setScale(2, BigDecimal.ROUND_HALF_UP);
 }
