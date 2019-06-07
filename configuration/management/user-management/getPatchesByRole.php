f1dec2c7537a017512b2148acd572443433ea5af
Harish <harish.d@dvara.com>
2019-06-07 17:37:14 +0530

    New files for Patch Changes

---
+++ configuration/management/user-management/getPatchesByRole.php
@@ -0,0 +1,41 @@
+<?php
+
+require '_init.php';
+require '_Pages.php';
+header("Access-Control-Request-Method: GET");
+
+$PM_QUERY = "
+SELECT  *
+FROM ".DB_SCHEMA.".role_patch_access rra where rra.role_id = ?";
+// "SELECT p.id, p.uri, rpa.id rpa_id, rpa.page_config
+// FROM ".DB_SCHEMA.".pages p
+// LEFT OUTER
+// JOIN ".DB_SCHEMA.".role_page_access rpa ON p.id = rpa.page_id AND rpa.role_id = 
+// ";
+
+$role_id = (!empty($_GET['roleId'])) ? mysqli_real_escape_string($connection,$_GET['roleId']) : $_GET['roleId'];
+if (empty($role_id)) {
+	die('{"error":"Role Id is mandatory"}');
+} else {
+	$stmt = $connection->prepare($PM_QUERY);
+	$stmt->bind_param("i", $role_id);
+	$stmt->execute();
+	$result = $stmt->get_result();
+}
+
+if (!$result) {
+	http_response_code(404);
+	echo 'Invalid query: ' . $connection->error;
+	return;
+} else {
+	$data = array();
+	while($output = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+		$data[] = $output;
+	}
+	$stmt->close();
+	http_response_code(200);
+	header('X-Total-Count: 100');
+	echo json_encode($data);
+}
+
+require '_end.php';
